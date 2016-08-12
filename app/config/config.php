615445f665f53dd2d391f047019e6bb8b539c194
Hanson Li <lihsca@gmail.com>
2016-08-12 08:54:28 -0400

    git unignore app/config/config.php

---
+++ app/config/config.php
@@ -0,0 +1,50 @@
+<?php
+
+use Phalcon\Config;
+use Phalcon\Logger;
+
+return new Config([
+    'database' => [
+        'adapter' => 'Mysql',
+        'host' => '127.0.0.1',
+        'username' => 'root',
+        'password' => '',
+        'dbname' => 'bte'
+    ],
+    'application' => [
+        'controllersDir' => APP_DIR . '/controllers/',
+        'modelsDir' => APP_DIR . '/models/',
+        'formsDir' => APP_DIR . '/forms/',
+        'viewsDir' => APP_DIR . '/views/',
+        'libraryDir' => APP_DIR . '/library/',
+        'pluginsDir' => APP_DIR . '/plugins/',
+        'cacheDir' => APP_DIR . '/cache/',
+        'baseUri' => '/',
+        'publicUrl' => 'http://btelocal',
+        'cryptSalt' => 'eEAfR|_&G&f,+vU]:jFr!!A&+71w1Ms9~8_4L!<@[N@DyaIP_2My|:+.u>/6m,$D'
+    ],
+    'mail' => [
+        'fromName' => 'BTE',
+        'fromEmail' => 'info@btecanada.com',
+        'smtp' => [
+            'server' => 'smtp.gmail.com',
+            'port' => 587,
+            'security' => 'tls',
+            'username' => '',
+            'password' => ''
+        ]
+    ],
+    'amazon' => [
+        'AWSAccessKeyId' => '',
+        'AWSSecretKey' => ''
+    ],
+    'logger' => [
+        'path'     => APP_DIR . '/logs/',
+        'format'   => '%date% [%type%] %message%',
+        'date'     => 'D j H:i:s',
+        'logLevel' => Logger::DEBUG,
+        'filename' => 'application.log',
+    ],
+
+    'xmlapi' => include 'xmlapi.php',
+]);
