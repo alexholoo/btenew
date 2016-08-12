1e577122c143b42ce41e0d8774a3a278ed209ae8
Hanson Li <lihsca@gmail.com>
2016-08-12 15:35:26 -0400

    new file ca_order_notes.php

---
+++ bin/scripts/ca_order_notes.php
@@ -0,0 +1,62 @@
+<?php
+
+require 'database.php';
+
+if (!($fh = @fopen('w:/out/purchasing/ca_order_notes.csv', 'rb'))) {
+    echo 'Failed to open file: ca_order_notes.csv';
+    exit;
+}
+
+echo "loading ca_order_notes.csv\n";
+
+fgetcsv($fh); // skip the first line
+
+$count = 0;
+while(($fields = fgetcsv($fh))) {
+
+    $date         = $fields[0];
+    $order_id     = $fields[1];
+    $stock_status = $fields[2];
+    $express      = $fields[3];
+    $qty          = $fields[4];
+    $supplier     = $fields[5];
+    $supplier_sku = $fields[6];
+    $mpn          = $fields[7];
+    $supplier_no  = $fields[8];
+    $notes        = $fields[9];
+    $related_sku  = $fields[10];
+    $dimension    = $fields[11];
+
+    try {
+        $success = $db->insertAsDict('ca_order_notes',
+            array(
+                'date'          => $date,
+                'order_id'      => $order_id,
+                'stock_status'  => $stock_status,
+                'stock_status'  => $stock_status,
+                'express'       => $express,
+                'qty'           => $qty,
+                'supplier'      => $supplier,
+                'supplier_sku'  => $supplier_sku,
+                'mpn'           => $mpn,
+                'supplier_no'   => $supplier_no,
+                'notes'         => $notes,
+                'related_sku'   => $related_sku,
+                'dimension'     => $dimension,
+            )
+        );
+
+        if (!$success) {
+            echo $order_id, EOL;
+        }
+
+        $count++;
+
+    } catch (Exception $e) {
+        echo $e->getMessage(), EOL;
+    }
+}
+
+fclose($fh);
+
+echo "$count DONE\n";
