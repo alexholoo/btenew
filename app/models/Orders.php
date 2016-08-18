9f9461c1b5a501fcbb36134d7d9832024bf66519
Hanson Li <lihsca@gmail.com>
2016-08-18 17:50:45 -0400

    add Orders model

---
+++ app/models/Orders.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\Models;
+
+use Phalcon\Mvc\Model;
+
+/**
+ * App\Models\Orders
+ */
+class Orders extends Model
+{
+    public $id;
+    public $channel;
+    public $date;
+    public $orderId;
+    public $mgnOrderId;
+    public $express;
+    public $buyer;
+    public $address;
+    public $city;
+    public $province;
+    public $postalcode;
+    public $country;
+    public $phone;
+    public $email;
+    public $sku;
+    public $price;
+    public $qty;
+    public $shipping;
+    public $mgnInvoiceId;
+
+    public function initialize()
+    {
+        $this->setSource('all_mgn_orders');
+    }
+
+    public function columnMap()
+    {
+        // Keys are the real names in the table and
+        // the values their names in the application
+
+        return array(
+            'id'             => 'id',
+            'channel'        => 'channel',
+            'date'           => 'date',
+            'order_id'       => 'orderId',
+            'mgn_order_id'   => 'mgnOrderId',
+            'express'        => 'express',
+            'buyer'          => 'buyer',
+            'address'        => 'address',
+            'city'           => 'city',
+            'province'       => 'province',
+            'postalcode'     => 'postalcode',
+            'country'        => 'country',
+            'phone'          => 'phone',
+            'email'          => 'email',
+            'skus_sold'      => 'sku',
+            'sku_price'      => 'price',
+            'skus_qty'       => 'qty',
+            'shipping'       => 'shipping',
+            'mgn_invoice_id' => 'mgnInvoiceId'
+        );
+    }
+}
