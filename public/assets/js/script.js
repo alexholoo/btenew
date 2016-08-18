c942d93e7ad1e9ac9189c35b320fe64f764eb0d8
Hanson Li <lihsca@gmail.com>
2016-08-18 11:58:00 -0400

    two js utils from dealtap

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,19 @@
 function showToast(msg) {
-  $('#toast').removeClass('error').text(msg).fadeIn(400).delay(3000).fadeOut(400);
+    $('#toast').removeClass('error').text(msg).fadeIn(400).delay(3000).fadeOut(400);
 }
 
 function showError(msg) {
-  $('#toast').addClass('error').text(msg).fadeIn(400).delay(3000).fadeOut(400);
+    $('#toast').addClass('error').text(msg).fadeIn(400).delay(3000).fadeOut(400);
 }
+
+window.$E = function(tag) {
+    return $(document.createElement(tag || 'div'));
+};
+
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+};
+
