0ff6a496ef59927f536ca9874eee35d3c20eed8b
Hanson Li <lihsca@gmail.com>
2016-08-22 16:49:39 -0400

    ftp settings

--- job/config/config.php
+++ job/config/config.php
@@ -9,4 +9,70 @@ return array(
         'name'      => 'bte',
         'port'      => 3306
     ),
+
+    'ftp' => array(
+        'DH' => array(
+            'Host' => 'ftp.dandh.com',
+            'User' => '8007120000',
+            'Pass' => 'hJcc7ngk',
+            'File' => 'ITEMLIST',
+            'Save' => 'DH-ITEMLIST'
+        ),
+
+        'DHU' => array(
+            'Host' => 'ftp.dandh.com',
+            'User' => '2548490000',
+            'Pass' => '98yznqWs',
+            'File' => 'ITEMLIST',
+            'Save' => 'DHU-ITEMLIST'
+        ),
+
+        'SYN' => array(
+            'Host' => 'ftp.synnex.ca',
+            'User' => 'c1150897',
+            'Pass' => '8aj2agkl',
+            'File' => 'c1150897.zip',
+            'Save' => 'syn-c1150897.zip'
+        ),
+
+        'SP' => array(
+            'Host' => 'pricefile.supercom.ca',
+            'User' => 'TB069700',
+            'Pass' => 'future',
+            'File' => 'PRICELIST.CSV',
+            'Save' => 'SP-ITEMLIST.CSV'
+        ),
+
+        'ASI' => array(
+            'Host' => '67.104.19.215',
+            'User' => '75692',
+            'Pass' => 'tB75692Tzi',
+            'File' => '75692.csv',
+            'Save' => '75692.csv'
+        ),
+
+        'ASU' => array(
+            'Host' => '67.91.114.176',
+            'User' => '88045',
+            'Pass' => 'ruoyu8977',
+            'File' => '88045.csv',
+            'Save' => '88045.csv'
+        ),
+
+        'TAK' => array(
+            'Host' => 'ftp.taknology.ca',
+            'User' => 'u72778471-ONB06004',
+            'Pass' => 'Bc53qt1',
+            'File' => 'tak.csv',
+            'Save' => 'atoz.csv'
+        ),
+
+        'ING' => array(
+            'Host' => 'ftpsecure.ingrammicro.com',
+            'User' => '300232',
+            'Pass' => 'T49h27',
+            'File' => '/FUSION/CA/BTECO/PRICE.ZIP',
+            'Save' => 'ing-price.zip'
+        ),
+    ),
 );
