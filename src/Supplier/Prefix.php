a48edbacc0af26bbfc6493df1c3bf4d28f8030fc
Hanson Li <lihsca@gmail.com>
2016-08-24 15:08:09 -0400

    add Supplier\Prefix

---
+++ src/Supplier/Prefix.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Supplier;
+
+class Prefix
+{
+    const DH       = 'DH-';
+    const ASI      = 'AS-';
+    const INGRAM   = 'ING-';
+    const SYNEX    = 'SYN-';
+    const TECHDATA = 'TD-';
+}
