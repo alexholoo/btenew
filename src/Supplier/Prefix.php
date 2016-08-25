8a2fe56843671c235591b4d7206df52b7799ec58
Hanson Li <lihsca@gmail.com>
2016-08-25 14:18:05 -0400

    Prefix::all()

--- src/Supplier/Prefix.php
+++ src/Supplier/Prefix.php
@@ -2,11 +2,31 @@
 
 namespace Supplier;
 
+/**
+ * $prefix = \Supplier\Prefix::DH;
+ * $prefix = \Supplier\Prefix::ASI;
+ * $prefix = \Supplier\Prefix::INGRAM;
+ * $prefix = \Supplier\Prefix::SYNNEX;
+ * $prefix = \Supplier\Prefix::TECHDATA;
+ *
+ * $all = \Supplier\Prefix::all();
+ */
 class Prefix
 {
-    const DH       = 'DH-';
-    const ASI      = 'AS-';
-    const INGRAM   = 'ING-';
-    const SYNEX    = 'SYN-';
-    const TECHDATA = 'TD-';
+    const DH       = 'DH';
+    const ASI      = 'AS';
+    const INGRAM   = 'ING';
+    const SYNEX    = 'SYN';
+    const TECHDATA = 'TD';
+
+    public static function all()
+    {
+        return [
+            self::DH,
+            self::ASI,
+            self::INGRAM,
+            self::SYNEX,
+            self::TECHDATA
+        ];
+    }
 }
