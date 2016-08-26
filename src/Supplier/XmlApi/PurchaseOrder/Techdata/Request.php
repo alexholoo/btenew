fd97e47fc9ebed01198b7bb8811239ce2ecc51fe
Hanson Li <lihsca@gmail.com>
2016-08-26 17:54:17 -0400

    PO of Ingram and Techdata - not done yet

--- src/Supplier/XmlApi/PurchaseOrder/Techdata/Request.php
+++ src/Supplier/XmlApi/PurchaseOrder/Techdata/Request.php
@@ -28,6 +28,11 @@ class Request
     {
         $lines = array();
 
+        $lines[] = "<XML_Order_Submit>";
+        $lines[] = $this->header();
+        $lines[] = $this->detail();
+        $lines[] = "</XML_Order_Submit>";
+
         return implode("\n", $lines);
     }
 
@@ -38,23 +43,53 @@ class Request
         $userid = $this->config['username'];
         $passwd = $this->config['password'];
 
+        $orderNo = $this->order['orderNo'];
+        $address = $this->order['address'];
+        $city = $this->order['city'];
+        $state = $this->order['state'];
+        $postalCode = $this->order['zipcode'];
+        $country = $this->order['country'];
+        $contact = $this->order['contact'];
+
+        $lines[] = "<Header>";
+        $lines[] = "  <UserName>$userid</UserName>";
+        $lines[] = "  <Password>$password</Password>";
+        $lines[] = "  <ResponseVersion>1.6</ResponseVersion>";
+        $lines[] = "  <OrderTypeCode>BS</OrderTypeCode>"; // ??
+        $lines[] = "  <PONbr>$orderNo</PONbr>";
+        $lines[] = "  <SalesRequirementCode>BK</SalesRequirementCode>"; // ??
+        $lines[] = "  <Name>eCOMMERCE</Name>"; // ??
+        $lines[] = "  <AddrInfo>";
+        $lines[] = "    <Addr>$address</Addr>";
+        $lines[] = "  </AddrInfo>";
+        $lines[] = "  <CityName>$city</CityName>";
+        $lines[] = "  <StateProvinceCode>$state</StateProvinceCode>";
+        $lines[] = "  <PostalCode>$postalCode</PostalCode>";
+        $lines[] = "  <ContactName>$contact</ContactName>";
+        $lines[] = "</Header>";
+
         return implode("\n", $lines);
     }
 
-    public function orderHeader()
+    public function detail()
     {
         $lines = array();
 
-        return implode("\n", $lines);
-    }
-
-    public function orderItems()
-    {
-        $lines = array();
+        $sku = $this->order['sku'];
+        $qty = $this->order['qty'];
 
         if (substr($sku, 0, 3) == 'TD-') {
             $sku = substr($sku, 3);
         }
+
+        $lines[] = "<Detail>";
+        $lines[] = "  <LineInfo>";
+        $lines[] = "    <QtyOrdered>$qty</QtyOrdered>";
+        $lines[] = "    <ProductIDQual>VP</ProductIDQual>"; // ??
+        $lines[] = "    <ProductID>$sku</ProductID>";
+        $lines[] = "    <IDCode>01</IDCode>"; // ??
+        $lines[] = "  </LineInfo>";
+        $lines[] = "</Detail>";
 
         return implode("\n", $lines);
     }
