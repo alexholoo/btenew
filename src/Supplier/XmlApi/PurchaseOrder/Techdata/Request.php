b571b9223077dc64b4eb6e740fa9226a8ca6ea0e
Hanson Li <lihsca@gmail.com>
2016-08-29 13:18:50 -0400

    fix PO of TD

--- src/Supplier/XmlApi/PurchaseOrder/Techdata/Request.php
+++ src/Supplier/XmlApi/PurchaseOrder/Techdata/Request.php
@@ -50,22 +50,31 @@ class Request
         $zipcode = $this->order['zipcode'];
         $country = $this->order['country'];
         $contact = $this->order['contact'];
+        $phone   = $this->order['phone'];
+        $email   = $this->order['email'];
 
         $lines[] = "<Header>";
         $lines[] = "  <UserName>$userid</UserName>";
         $lines[] = "  <Password>$passwd</Password>";
         $lines[] = "  <ResponseVersion>1.6</ResponseVersion>";
-        $lines[] = "  <OrderTypeCode>BS</OrderTypeCode>"; // ??
         $lines[] = "  <PONbr>$orderNo</PONbr>";
-        $lines[] = "  <SalesRequirementCode>BK</SalesRequirementCode>"; // ??
-        $lines[] = "  <Name>eCOMMERCE</Name>"; // ??
-        $lines[] = "  <AddrInfo>";
-        $lines[] = "    <Addr>$address</Addr>";
-        $lines[] = "  </AddrInfo>";
-        $lines[] = "  <CityName>$city</CityName>";
-        $lines[] = "  <StateProvinceCode>$state</StateProvinceCode>";
-        $lines[] = "  <PostalCode>$zipcode</PostalCode>";
-        $lines[] = "  <ContactName>$contact</ContactName>";
+        $lines[] = "  <EndUserInfo>";
+        $lines[] = "    <EuiContactName>$contact</EuiContactName>";
+        $lines[] = "    <EuiPhoneNbr>$phone</EuiPhoneNbr>";
+        $lines[] = "    <EuiName>$contact</EuiName>";
+        $lines[] = "    <EuiAddr1>$address</EuiAddr1>";
+        $lines[] = "    <EuiAddr2></EuiAddr2>";
+        $lines[] = "    <EuiAddr3></EuiAddr3>";
+        $lines[] = "    <EuiCityName>$city</EuiCityName>";
+        $lines[] = "    <EuiStateProvinceCode>$state</EuiStateProvinceCode>";
+        $lines[] = "    <EuiPostalCode>$zipcode</EuiPostalCode>";
+        $lines[] = "    <EuiCountryCode>$country</EuiCountryCode>";
+        $lines[] = "    <EuiDropShipType>D</EuiDropShipType>";
+        $lines[] = "    <EuiContactEmailAddr1>$email</EuiContactEmailAddr1>";
+        $lines[] = "  </EndUserInfo>";
+        $lines[] = "  <MyOrderTracker>";
+        $lines[] = "    <EndUserEmail>$email</EndUserEmail>";
+        $lines[] = "  </MyOrderTracker>";
         $lines[] = "</Header>";
 
         return implode("\n", $lines);
@@ -77,6 +86,8 @@ class Request
 
         $sku = $this->order['sku'];
         $qty = $this->order['qty'];
+        $branch = $this->order['branch'];
+        $comment = $this->order['comment'];
 
         if (substr($sku, 0, 3) == 'TD-') {
             $sku = substr($sku, 3);
@@ -87,7 +98,9 @@ class Request
         $lines[] = "    <QtyOrdered>$qty</QtyOrdered>";
         $lines[] = "    <ProductIDQual>VP</ProductIDQual>"; // ??
         $lines[] = "    <ProductID>$sku</ProductID>";
+        $lines[] = "    <WhseCode>$branch</WhseCode>"; // ??
         $lines[] = "    <IDCode>01</IDCode>"; // ??
+        $lines[] = "    <OrderMessageLine>$comment</OrderMessageLine>"; // ??
         $lines[] = "  </LineInfo>";
         $lines[] = "</Detail>";
 
