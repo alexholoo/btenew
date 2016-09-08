3dfc9675eb7646503764f577b8e06d324312e225
Hanson Li <lihsca@gmail.com>
2016-09-08 14:43:00 -0400

    endUserPO in Order

--- src/Supplier/Model/Order.php
+++ src/Supplier/Model/Order.php
@@ -24,30 +24,32 @@ class Order
     public $shipping;   // shipping fee
 
     public $customerPO;
+    public $endUserPO;
     public $branch;     // the warehouse ship from
     public $comment;
 
     public function __construct($order)
     {
-        $this->channel  = Arr::get($order, 'channel');
-        $this->date     = Arr::get($order, 'date');
-        $this->orderId  = Arr::get($order, 'orderId');
-        $this->express  = Arr::get($order, 'express');
-        $this->contact  = Arr::get($order, 'buyer'); // !
-        $this->address  = Arr::get($order, 'address');
-        $this->city     = Arr::get($order, 'city');
-        $this->province = Arr::get($order, 'province');
-        $this->zipcode  = Arr::get($order, 'postalcode'); // !
-        $this->country  = Arr::get($order, 'country');
-        $this->phone    = Arr::get($order, 'phone');
-        $this->email    = Arr::get($order, 'email');
-        $this->sku      = Arr::get($order, 'sku');
-        $this->price    = Arr::get($order, 'price');
-        $this->qty      = Arr::get($order, 'qty');
-        $this->shipping = Arr::get($order, 'shipping');
+        $this->channel    = Arr::get($order, 'channel');
+        $this->date       = Arr::get($order, 'date');
+        $this->orderId    = Arr::get($order, 'orderId');
+        $this->express    = Arr::get($order, 'express');
+        $this->contact    = Arr::get($order, 'buyer'); // !
+        $this->address    = Arr::get($order, 'address');
+        $this->city       = Arr::get($order, 'city');
+        $this->province   = Arr::get($order, 'province');
+        $this->zipcode    = Arr::get($order, 'postalcode'); // !
+        $this->country    = Arr::get($order, 'country');
+        $this->phone      = Arr::get($order, 'phone');
+        $this->email      = Arr::get($order, 'email');
+        $this->sku        = Arr::get($order, 'sku');
+        $this->price      = Arr::get($order, 'price');
+        $this->qty        = Arr::get($order, 'qty');
+        $this->shipping   = Arr::get($order, 'shipping');
 
         $this->branch     = Arr::get($order, 'branch');
         $this->comment    = Arr::get($order, 'comment');
         $this->customerPO = Arr::get($order, 'customerPO');
+        $this->endUserPO  = Arr::get($order, 'endUserPO');
     }
 }
