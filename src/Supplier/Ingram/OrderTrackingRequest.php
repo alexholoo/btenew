9926909f000a590f8e2740f282a4db4ec15cedb2
Hanson Li <lihsca@gmail.com>
2016-09-14 11:55:18 -0400

    Ingram OrderTrackingRequest

--- src/Supplier/Ingram/OrderTrackingRequest.php
+++ src/Supplier/Ingram/OrderTrackingRequest.php
@@ -12,8 +12,29 @@ class OrderTrackingRequest extends BaseRequest
      */
     public function toXml()
     {
+        $loginId  = $this->config['loginId'];
+        $password = $this->config['password'];
+        $orderId  = $this->orderId;
+
         $lines = array();
-        // ...
+        $lines[] = '<OrderTrackingRequest>';
+        $lines[] = '<Version>2.0</Version>';
+        $lines[] = '<TransactionHeader>';
+        $lines[] =    '<SenderID></SenderID>';
+        $lines[] =    '<ReceiverID></ReceiverID>';
+        $lines[] =    '<CountryCode>FT</CountryCode>';
+        $lines[] =    "<LoginID>$loginId</LoginID>";
+        $lines[] =    "<Password>$password</Password>";
+        $lines[] =    '<TransactionID></TransactionID>';
+        $lines[] = '</TransactionHeader>';
+        $lines[] = '<TrackingRequestHeader>';
+        #lines[] =    '<BranchOrderNumber></BranchOrderNumber>';
+        #lines[] =    '<OrderSuffix></OrderSuffix>';
+        $lines[] =    "<CustomerPO>$orderId</CustomerPO>";
+        $lines[] = '</TrackingrequestHeader>';
+        $lines[] = '<ShowDetail>2</ShowDetail>';
+        $lines[] = '</OrderTrackingRequest>';
+
         return Utils::formatXml(implode("\n", $lines));
     }
 }
