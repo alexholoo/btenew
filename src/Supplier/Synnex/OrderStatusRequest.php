1a740415b727445e8ec5d6779efcbf479ba56029
Hanson Li <lihsca@gmail.com>
2016-09-14 11:45:48 -0400

    Synnex OrderStatusRequest

--- src/Supplier/Synnex/OrderStatusRequest.php
+++ src/Supplier/Synnex/OrderStatusRequest.php
@@ -12,8 +12,24 @@ class OrderStatusRequest extends BaseRequest
      */
     public function toXml()
     {
+        $username   = $this->config['username'];
+        $password   = $this->config['password'];
+        $customerNo = $this->config['customerNo'];
+        $orderId    = $this->orderId;
+
         $lines = array();
-        // ...
+        $lines = '<?xml version="1.0" encoding="UTF-8" ?>';
+        $lines = '<SynnexB2B version="2.2">';
+        $lines =     '<Credential>';
+        $lines =        "<UserID>$username</UserID>";
+        $lines =        "<Password>$password</Password>";
+        $lines =     '</Credential>';
+        $lines =     '<OrderStatusRequest>';
+        $lines =        "<CustomerNumber>$customerNo</CustomerNumber>";
+        $lines =        "<PONumber>$orderId</PONumber>";
+        $lines =     '</OrderStatusRequest>';
+        $lines = '</SynnexB2B>';
+
         return Utils::formatXml(implode("\n", $lines));
     }
 }
