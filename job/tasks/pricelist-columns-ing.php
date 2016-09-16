65786d3fe2d0fa54f3cc8b2d8ca41c551eba6bf4
Hanson Li <lihsca@gmail.com>
2016-09-16 13:10:22 -0400

    pricelist-columns-ing

---
+++ job/tasks/pricelist-columns-ing.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+    'action_indicator',
+    'sku',
+    'vendor_number',
+    'vendor_name',
+    'part_desc_1',
+    'part_desc_2',
+    'retail_price',
+    'vendor_part_number',
+    'weight',
+    'upc',
+    'length',
+    'width',
+    'height',
+    'price_change_flag',
+    'customer_price',
+    'special_price_flag',
+    'availability_flag',
+    'sku_status',
+    'cpu_code',
+    'media_type',
+    'category',
+    'new_item_receipt_flag',
+    'substitute_part',
+    'instant_rebate_flag'
+);
