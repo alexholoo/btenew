3425ba915c9400526c3390f16f987b9d61fee5b9
Hanson Li <lihsca@gmail.com>
2016-09-19 11:47:17 -0400

    Job/PriceAvailJob

---
+++ job/PriceAvailJob.php
@@ -0,0 +1,60 @@
+<?php
+
+class PriceAvailJob
+{
+    public function __construct()
+    {
+        $this->di = \Phalcon\Di::getDefault();
+        $this->db = $this->di->get('db');
+        $this->queue = $this->di->get('queue');
+    }
+
+    public function run($arg = '')
+    {
+        global $argv;
+
+        if (empty($arg) && isset($argv[1])) {
+            $arg = $argv[1];
+        }
+
+        $skus = $this->getSkus($arg);
+
+        foreach ($skus as $sku) {
+            try {
+                echo 'Price & Availability for ', $sku, PHP_EOL;
+                $client = \Supplier\Supplier::createClient($sku);
+                if ($client) {
+                    $client->getPriceAvailability($sku);
+                }
+            } catch (Exception $e) {
+                echo $e->getMessage(), PHP_EOL;
+            }
+        }
+    }
+
+    protected function getSkus($arg)
+    {
+        if (empty($arg)) {
+            $arg = date('Y-m-d');
+        }
+
+        $sql = "SELECT * FROM ca_order_notes WHERE date = '$arg'";
+        $result = $this->db->query($sql);
+
+        $list = [];
+        while ($row = $result->fetch(\Phalcon\Db::FETCH_ASSOC)) {
+            $related_sku = explode('|', $row['related_sku']);
+            $related_sku = array_map('trim', $related_sku);
+            $related_sku = array_filter($related_sku);
+
+            $list = array_merge($list, $related_sku);
+        }
+
+        return array_unique($list);
+    }
+}
+
+include __DIR__ . '/../public/init.php';
+
+$job = new PriceAvailJob();
+$job->run();
