216cf3eb9ab6724fe9bd69016591d2caa4c8b59f
Hanson Li <lihsca@gmail.com>
2016-09-28 09:24:23 -0400

    AmericaState

---
+++ src/Utility/AmericaState.php
@@ -0,0 +1,75 @@
+<?php
+
+class AmericaState
+{
+    static $codeNames = [
+        'AL' => 'Alabama',
+        'AK' => 'Alaska',
+        'AZ' => 'Arizona',
+        'AR' => 'Arkansas',
+        'CA' => 'California',
+        'CO' => 'Colorado',
+        'CT' => 'Connecticut',
+        'DE' => 'Delaware',
+        'FL' => 'Florida',
+        'GA' => 'Georgia',
+        'HI' => 'Hawaii',
+        'ID' => 'Idaho',
+        'IL' => 'Illinois',
+        'IN' => 'Indiana',
+        'IA' => 'Iowa',
+        'KS' => 'Kansas',
+        'KY' => 'Kentucky',
+        'LA' => 'Louisiana',
+        'ME' => 'Maine',
+        'MD' => 'Maryland',
+        'MA' => 'Massachusetts',
+        'MI' => 'Michigan',
+        'MN' => 'Minnesota',
+        'MS' => 'Mississippi',
+        'MO' => 'Missouri',
+        'MT' => 'Montana',
+        'NE' => 'Nebraska',
+        'NV' => 'Nevada',
+        'NH' => 'New Hampshire',
+        'NJ' => 'New Jersey',
+        'NM' => 'New Mexico',
+        'NY' => 'New York',
+        'NC' => 'North Carolina',
+        'ND' => 'North Dakota',
+        'OH' => 'Ohio',
+        'OK' => 'Oklahoma',
+        'OR' => 'Oregon',
+        'PA' => 'Pennsylvania',
+        'RI' => 'Rhode Island',
+        'SC' => 'South Carolina',
+        'SD' => 'South Dakota',
+        'TN' => 'Tennessee',
+        'TX' => 'Texas',
+        'UT' => 'Utah',
+        'VT' => 'Vermont',
+        'VA' => 'Virginia',
+        'WA' => 'Washington',
+        'WV' => 'West Virginia',
+        'WI' => 'Wisconsin',
+        'WY' => 'Wyoming',
+    ];
+
+    public static function codeToName($code)
+    {
+        $map = self::$codeNames;
+        $key = strtoupper($code);
+        return isset($map[$key]) ? $map[$key] : $code;
+    }
+
+    public static function nameToCode($name)
+    {
+        $map = array_flip(self::$codeNames);
+        $key = preg_replace('/ {2,}|\s/', ' ', trim(ucwords(mb_strtolower($name))));
+        return isset($map[$key]) ? $map[$key] : $name;
+    }
+}
+
+//echo AmericaState::codeToName('NY'), PHP_EOL;
+//echo AmericaState::codeToName('xx'), PHP_EOL;
+//echo AmericaState::nameToCode(' new   york '), PHP_EOL;
