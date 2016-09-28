cefcce6c2440e12a92b0c6632d89ea95695f7840
Hanson Li <lihsca@gmail.com>
2016-09-28 15:17:33 -0400

    more ebay methods

--- src/Marketplace/eBay/Client.php
+++ src/Marketplace/eBay/Client.php
@@ -9,12 +9,12 @@ class Client
 {
     const COMPATABILITY_LEVEL = 949;    // eBay API version
 
-    private $requestToken;
+    private $userToken;
     private $devID;
     private $appID;
     private $certID;
     private $serverUrl;
-    private $compatLevel;
+    private $apiVersion;
     private $siteID;
     private $verb;
 
@@ -23,16 +23,16 @@ class Client
      */
     public function __construct($config)
     {
-        $this->requestToken = Arr::val($config, 'userToken');
-        $this->devID        = Arr::val($config, 'devID');
-        $this->appID        = Arr::val($config, 'appID');
-        $this->certID       = Arr::val($config, 'certID');
-        $this->serverUrl    = Arr::val($config, 'serverUrl');
+        $this->userToken = Arr::val($config, 'userToken');
+        $this->devID     = Arr::val($config, 'devID');
+        $this->appID     = Arr::val($config, 'appID');
+        $this->certID    = Arr::val($config, 'certID');
+        $this->serverUrl = Arr::val($config, 'serverUrl');
 
-        $this->compatLevel  = self::COMPATABILITY_LEVEL;
+        $this->apiVersion = self::COMPATABILITY_LEVEL;
 
-        $this->siteID       = Arr::val($config, 'siteID', Site::CA);
-       #$this->verb         = Arr::val($config, 'callName');
+        $this->siteID    = Arr::val($config, 'siteID', Site::CA);
+       #$this->verb      = Arr::val($config, 'callName');
     }
 
     /**
@@ -106,7 +106,7 @@ class Client
     {
         $headers = array (
             //Regulates versioning of the XML interface for the API
-            'X-EBAY-API-COMPATIBILITY-LEVEL: ' . $this->compatLevel,
+            'X-EBAY-API-COMPATIBILITY-LEVEL: ' . $this->apiVersion,
 
             //set the keys
             'X-EBAY-API-DEV-NAME: ' . $this->devID,
@@ -132,7 +132,7 @@ class Client
         $lines[] = '<?xml version="1.0" encoding="utf-8" ?>';
         $lines[] = '<GetMyeBaySellingRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
         $lines[] =   "<RequesterCredentials>";
-        $lines[] =     "<eBayAuthToken>{$this->requestToken}</eBayAuthToken>";
+        $lines[] =     "<eBayAuthToken>{$this->userToken}</eBayAuthToken>";
         $lines[] =   "</RequesterCredentials>";
         $lines[] =   "<ActiveList>";
         $lines[] =     "<Include>true</Include>";
@@ -149,9 +149,156 @@ class Client
         $this->setVerb('GetMyeBaySelling'); //ucfirst(__FUNCTION__)
         $response = $this->sendHttpRequest($request);
 
-        // TODO: prase response to result;
-        return $response;
-        //return $result;
+        // TODO: prase response to result, return $result;
+
+        return simplexml_load_string($response);
+    }
+
+    public function completeSale($order)
+    {
+        $orderID = $order['OrderID'];
+        $TransactionID = $order['TransactionID'];
+        $trackingNumber = $order['TrackingNumber'];
+        $carrier = $order['Carrier'];
+        $date = $order['Date'];
+
+        $lines = [];
+
+        $lines[] = '<?xml version="1.0" encoding="utf-8"?>';
+        $lines[] = '<CompleteSaleRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
+        $lines[] =   '<RequesterCredentials>';
+        $lines[] =     "<eBayAuthToken>{$this->userToken}</eBayAuthToken>";
+        $lines[] =   '</RequesterCredentials>';
+        $lines[] =   "<OrderID>$orderID</OrderID>";
+        $lines[] =   '<Shipment>';
+        $lines[] =     '<ShipmentTrackingDetails>';
+        $lines[] =       '<ShipmentLineItem>';
+        $lines[] =         "<TransactionID>$transactionID</TransactionID>";
+        $lines[] =       '</ShipmentLineItem>';
+        $lines[] =       "<ShipmentTrackingNumber>$trackingNumber</ShipmentTrackingNumber>";
+        $lines[] =       "<ShippingCarrierUsed>$carrier</ShippingCarrierUsed>";
+        $lines[] =     '</ShipmentTrackingDetails>';
+       #$lines[] =     "<ShippedTime>$date</ShippedTime>";
+        $lines[] =   '</Shipment>';
+        $lines[] =   "<TransactionID>$transactionID</TransactionID>";
+        $lines[] =   '<Shipped>true</Shipped>';
+        $lines[] = '</CompleteSaleRequest>';
+
+        $request = implode("\n", $lines);
+
+        $this->setVerb('CompleteSale'); //ucfirst(__FUNCTION__)
+        $response = $this->sendHttpRequest($request);
+
+        // TODO: prase response to result, return $result;
+
+        return simplexml_load_string($response);
+    }
+
+    public function getOrders($timeFrom, $timeTo)
+    {
+        $lines = [];
+
+        // If you want to hard code From and To timings, Follow the below format in "GMT".
+        // $CreateTimeFrom = YYYY-MM-DDTHH:MM:SS; //GMT
+        // $CreateTimeTo = YYYY-MM-DDTHH:MM:SS; //GMT
+
+        $lines[] = '<?xml version="1.0" encoding="utf-8" ?>';
+        $lines[] = '<GetOrdersRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
+        $lines[] =   '<RequesterCredentials>';
+        $lines[] =     "<eBayAuthToken>{$this->userToken}</eBayAuthToken>";
+        $lines[] =   '</RequesterCredentials>';
+        $lines[] =   '<DetailLevel>ReturnSummary</DetailLevel>';
+        $lines[] =   "<CreateTimeFrom>$timeFrom</CreateTimeFrom>";
+        $lines[] =   "<CreateTimeTo>$timeTo</CreateTimeTo>";
+        $lines[] =   '<OrderRole>Seller</OrderRole>';
+        $lines[] =   '<OrderStatus>All</OrderStatus>';
+        $lines[] = '</GetOrdersRequest>';
+
+        $request = implode("\n", $lines);
+
+        $this->setVerb('GetOrders'); //ucfirst(__FUNCTION__)
+        $response = $this->sendHttpRequest($request);
+
+        // TODO: prase response to result, return $result;
+
+        return simplexml_load_string($response);
+    }
+
+    public function endItem($itemID)
+    {
+        $lines = [];
+
+		$lines[] = '<?xml version="1.0" encoding="utf-8"?>';
+		$lines[] = '<EndItemRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
+        $lines[] =   '<RequesterCredentials>';
+        $lines[] =     "<eBayAuthToken>{$this->userToken}</eBayAuthToken>";
+        $lines[] =   '</RequesterCredentials>';
+		$lines[] =   "<ItemID>$itemID</ItemID>";
+		$lines[] =   "<EndingReason>NotAvailable</EndingReason>";
+		$lines[] = '</EndItemRequest>';
+
+        $request = implode("\n", $lines);
+
+        $this->setVerb('EndItem'); //ucfirst(__FUNCTION__)
+        $response = $this->sendHttpRequest($request);
+
+        // TODO: prase response to result, return $result;
+
+        return simplexml_load_string($response);
+    }
+
+    public function reviseInventoryStatus($info)
+    {
+        $itemID = $info['ItemID'];
+        $quantity = $info['Quantity'];
+        $price = $info['Price'];
+
+        $lines = [];
+
+		$lines[] = '<?xml version="1.0" encoding="utf-8"?>';
+		$lines[] = '<ReviseInventoryStatusRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
+        $lines[] =   '<RequesterCredentials>';
+        $lines[] =     "<eBayAuthToken>{$this->userToken}</eBayAuthToken>";
+        $lines[] =   '</RequesterCredentials>';
+		$lines[] =   '<InventoryStatus>';
+		$lines[] =     "<ItemID>$itemID</ItemID>";
+		$lines[] =     "<Quantity>$quantity</Quantity>";
+	   #$lines[] =     "<SKU>$SKU</SKU>";
+		$lines[] =     "<StartPrice>$price</StartPrice>";
+		$lines[] =   '</InventoryStatus>';
+		$lines[] = '</ReviseInventoryStatusRequest>';
+
+        $request = implode("\n", $lines);
+
+        $this->setVerb('ReviseInventoryStatus'); //ucfirst(__FUNCTION__)
+        $response = $this->sendHttpRequest($request);
+
+        // TODO: prase response to result, return $result;
+
+        return simplexml_load_string($response);
+    }
+
+    public function geteBayDetails()
+    {
+        $lines = [];
+
+        $lines[] = '<?xml version="1.0" encoding="utf-8"?>';
+        $lines[] = '<GeteBayDetailsRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
+        $lines[] =   '<RequesterCredentials>';
+        $lines[] =     "<eBayAuthToken>{$this->userToken}</eBayAuthToken>";
+        $lines[] =   '</RequesterCredentials>';
+        $lines[] =   '<DetailName>ShippingCarrierDetails</DetailName>';
+        $lines[] =   '<DetailName>ShippingServiceDetails</DetailName>';
+        $lines[] = '</GeteBayDetailsRequest>';
+
+        $request = implode("\n", $lines);
+
+        $this->setVerb('GeteBayDetails'); //ucfirst(__FUNCTION__)
+        $response = $this->sendHttpRequest($request);
+
+        // TODO: prase response to result, return $result;
+
+        return simplexml_load_string($response);
     }
 }
 
