944d9811b60c3761c0089bee8a6cc6d9012f0120
Hanson Li <lihsca@gmail.com>
2016-10-03 13:53:17 -0400

    Marketplace/MasterOrderList

--- src/Marketplace/MasterOrderList.php
+++ src/Marketplace/MasterOrderList.php
@@ -7,4 +7,48 @@ use Toolkit\CsvFileReader;
 
 class MasterOrderList
 {
+    protected $csvWriter;
+    protected $csvReader;
+
+    protected $csvTitle = array(
+        'channel',
+        'date',
+        'channel_order_id',
+        'mgn_order_id',
+        'express?',
+        'buyer',
+        'address',
+        'city',
+        'province',
+        'postalcode',
+        'country',
+        'phone',
+        'email',
+        'skus_sold',
+        'sku_price',
+        'skus_qty',
+        'shipping',
+        'mgn_invoice_id'
+    );
+
+    public function __construct()
+    {
+        $filename = 'E:/BTE/orders/all_mgn_orders.csv';
+
+        $csvWriter = new CsvFileWriter($filename, $this->csvTitle);
+        $csvWriter->setFilemode(CsvFileWriter::MODE_CREATE);
+
+        $csvReader = new CsvFileReader($filename, true);
+        $csvReader->setColumns($this->csvTitle);
+    }
+
+    public function read()
+    {
+        return $this->csvReader->read();
+    }
+
+    public function write($data)
+    {
+        return $this->csvWriter->write($data);
+    }
 }
