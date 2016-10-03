9bb232c48abfc147193753f82886448fb30a6218
Hanson Li <lihsca@gmail.com>
2016-10-03 18:08:17 -0400

    Rakuten/Client

--- src/Marketplace/Rakuten/Client.php
+++ src/Marketplace/Rakuten/Client.php
@@ -2,14 +2,68 @@
 
 namespace Marketplace\Rakuten;
 
-use Toolkit\Arr;
-use Toolkit\Utils;
+use Toolkit\FtpClient;
 
 class Client
 {
+    protected $di;
+    protected $db;
+    protected $site;
+    protected $config;
+
+    public function __construct($site, $config = [])
+    {
+        // TODO: di & db
+        $this->site = $site = strtoupper($site);
+
+        if ($config) {
+            $this->config = $config;
+        } else {
+            $config = require APP_DIR . '/config/rakuten.php';
+            $this->config = $config[strtoupper($site)];
+        }
+    }
+
+    public function getOrders()
+    {
+        $ftp = new FtpClient($this->config);
+
+        if (!$ftp->connect()) {
+            echo "Failed to login Rakuten {$this->site} FTP server", PHP_EOL;
+            return;
+        }
+
+        $files = $ftp->listFiles('/Orders/');
+
+        if ($this->site == 'CA') {
+            $localFolder = 'E:/BTE/orders/rakuten/order_ca/';
+        } else if ($this->site == 'US') {
+            $localFolder = 'E:/BTE/orders/rakuten/order_us/';
+        }
+
+        $rakutenPrefix ='23267604_';
+
+        foreach ($files as $file) {
+
+            $localFile  = $localFolder . basename($file);
+
+            if (preg_match("/$rakutenPrefix/i", $file) && !file_exists($localFile)) {
+                echo "Downloading $file", PHP_EOL;
+                $ftp->download("/Orders/$file", $localFile);
+                $this->importFile($localFile);
+            }
+        }
+    }
+
+    protected function importFile($filename)
+    {
+        // TODO
+    }
 }
 
 #include '../../../public/init.php';
 #
-#$config = include '../../../app/config/rakuten.php';
-#$client = new Client($config['US']);
+#//$config = include APP_DIR . '/config/rakuten.php';
+#$client = new Client('CA'); // not avail
+#$client = new Client('US');
+#$client->getOrders();
