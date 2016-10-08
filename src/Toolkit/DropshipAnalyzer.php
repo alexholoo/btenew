d195786e3ce1f2e8e93030c406b39b6d4278af3f
Hanson Li <lihsca@gmail.com>
2016-10-08 12:01:04 -0400

    refactor DropshipAnalyzer

--- src/Toolkit/DropshipAnalyzer.php
+++ src/Toolkit/DropshipAnalyzer.php
@@ -6,44 +6,46 @@ class DropshipAnalyzer
 {
     public static function get($sku, $price, $place, $express)
     {
-        $syn_dropship = [ "ON","AB","BC","NB","NL","NS","YT","NT","PE" ];
-        $dh_dropship  = [ "AB","BC","NB","NL","NS","YT","NT","PE" ]; // location better with drop ship for DH items
-        $td_dropship  = [ "ON","AB","BC","NB","NL","NS","YT","NT","PE" ];
-        $ing_dropship = [ "ON","AB","NB","NL","NS","YT","NT","PE" ]; // does ing drop ship to bc?
-
         $parts = explode('-', $sku);
-        $supplier = $parts[0];
+        $supplier = strtoupper($parts[0]);
 
-        if ($supplier == 'AS') {
-            // no drop ship for asi item
-            return false;
+        switch ($supplier) {
+        case 'SYN':
+            return Dropship_SYN::analyze($sku, $price, $place, $express);
+            break;
+        case 'TD':
+            return Dropship_TD::analyze($sku, $price, $place, $express);
+            break;
+        case 'ING':
+            return Dropship_ING::analyze($sku, $price, $place, $express);
+            break;
+        case 'DH':
+            return Dropship_DH::analyze($sku, $price, $place, $express);
+            break;
         }
 
-        if ($supplier == 'SYN') {
-            return in_array($place, $syn_dropship);
-        }
+        return false;  // no drop ship
+    }
+}
 
-        if ($supplier == 'TD') {
-            if (in_array($place, $td_dropship)) {
-                if ($express == '1') { // TODO: ??
-                    // use drop ship for express order
-                    return true;
-                }
-                else if ($price > 300) {
-                    // use drop ship for expensive item, can adjust the amount accordingly
-                    return true;
-                }
-            }
-            return false;
-        }
+class Dropship_SYN
+{
+    protected static $dropshipProvinces = [ "ON", "AB", "BC", "NB", "NL", "NS", "YT", "NT", "PE" ];
 
-        if ($supplier == 'ING') {
-            // use drop ship for all dropshipable orders
-            return in_array($place, $ing_dropship);
-        }
+    public static function analyze($sku, $price, $place, $express)
+    {
+        return in_array($place, self::$dropshipProvinces);
+    }
+}
 
-        if ($supplier == 'DH') {
-            if ($express == 'Yes') { // TODO: ??
+class Dropship_TD
+{
+    protected static $dropshipProvinces  = [ "ON", "AB", "BC", "NB", "NL", "NS", "YT", "NT", "PE" ];
+
+    public static function analyze($sku, $price, $place, $express)
+    {
+        if (in_array($place, self::$dropshipProvinces)) {
+            if ($express == '1') { // TODO: ??
                 // use drop ship for express order
                 return true;
             }
@@ -51,11 +53,45 @@ class DropshipAnalyzer
                 // use drop ship for expensive item, can adjust the amount accordingly
                 return true;
             }
-            return in_array ($place ,$dh_dropship);
         }
-
         return false;
     }
 }
 
+class Dropship_ING
+{
+    // does ing drop ship to bc?
+    protected static $dropshipProvinces = [ "ON", "AB", "NB", "NL", "NS", "YT", "NT", "PE" ];
+
+    public static function analyze($sku, $price, $place, $express)
+    {
+        // use drop ship for all dropshipable orders
+        return in_array($place, self::$dropshipProvinces);
+    }
+}
+
+class Dropship_DH
+{
+    // location better with drop ship for DH items
+    protected static $dropshipProvinces = [ "AB", "BC", "NB", "NL", "NS", "YT", "NT", "PE" ];
+
+    public static function analyze($sku, $price, $place, $express)
+    {
+        if ($express == 'Yes') { // TODO: ??
+            // use drop ship for express order
+            return true;
+        }
+        else if ($price > 300) {
+            // use drop ship for expensive item, can adjust the amount accordingly
+            return true;
+        }
+        return in_array($place, self::$dropshipProvinces);
+    }
+}
+
 //echo DropshipAnalyzer::get($sku, $price, $place, $express);
+//var_dump(DropshipAnalyzer::get('DH-123',  200, 'ON', 1));
+//var_dump(DropshipAnalyzer::get('SYN-123', 200, 'ON', 1));
+//var_dump(DropshipAnalyzer::get('ING-123', 200, 'ON', 1));
+//var_dump(DropshipAnalyzer::get('TD-123',  200, 'ON', 1));
+//var_dump(DropshipAnalyzer::get('AS-123',  200, 'ON', 1));
