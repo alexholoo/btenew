9216fd63af48c7b3bc3d2a731b1e3ce537ef4636
Hanson Li <lihsca@gmail.com>
2016-10-14 14:15:52 -0400

    Marketplace\Channel

---
+++ src/Marketplace/Channel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Marketplace;
+
+abstract class Channel // Store?
+{
+    const AMAZON_CA = 'Amazon-ACA';
+    const AMAZON_US = 'Amazon-US';
+    const NEWEGG_CA = 'NeweggCA';
+    const RAKUTEN   = 'Rakuten-BUY';
+    const EBAY      = 'ebay';
+    const EBAY_CA   = 'ebay-ca';
+    const EBAY_US   = 'ebay-us';
+}
