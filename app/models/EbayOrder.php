96292bd04eb1acc728c1e3299276e8e38b48acd5
Hanson Li <lihsca@gmail.com>
2016-10-17 11:33:11 -0400

    app/models/EbayOrder

---
+++ app/models/EbayOrder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Phalcon\Mvc\Model;
+
+class EbayOrder extends Model
+{
+    public $id;
+    public $orderID;
+    public $status;
+    public $buyerUsername;
+    public $datePaid;
+    public $amountPaid;
+    public $salesTaxAmount;
+    public $shippingService;
+    public $shippingServiceCost;
+    public $name;
+    public $address;
+    public $address2;
+    public $city;
+    public $province;
+    public $postalCode;
+    public $country;
+    public $phone;
+    public $quantityPurchased;
+    public $email;
+    public $sku;
+    public $transactionID;
+    public $transactionPrice;
+    public $tracking;
+    public $itemID;
+    public $recordNumber;
+    
+    public function getSource()
+    {
+        return 'ebay_order_report_bte';
+    }
+
+    public function initialize()
+    {
+    }
+}
