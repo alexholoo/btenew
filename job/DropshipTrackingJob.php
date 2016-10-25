0eb6d3cf954ba5a320d447851c475d2a008db090
Hanson Li <lihsca@gmail.com>
2016-10-25 15:57:04 -0400

    job/DropshipTrackingJob

---
+++ job/DropshipTrackingJob.php
@@ -0,0 +1,54 @@
+<?php
+
+use Supplier\Supplier;
+
+class DropshipTrackingJob
+{
+    public function __construct()
+    {
+        $this->di = \Phalcon\Di::getDefault();
+        $this->db = $this->di->get('db');
+        $this->queue = $this->di->get('queue');
+    }
+
+    public function run($argv = [])
+    {
+        $sql = 'SELECT * FROM purchase_order_log WHERE shipped=0';
+        $result = $this->db->query($sql);
+
+        while ($order = $result->fetch()) {
+            $sku     = $order['sku'];
+            $orderId = $order['orderid'];
+            $ponum   = $order['ponumber'];
+            $invoice = $order['invoice'];
+
+            echo $orderId, ' ', $sku, ' ';
+
+            try {
+
+                $client = Supplier::createClient($sku);
+                if (!$client) {
+                    echo 'Not support', EOL;
+                    continue;
+                }
+
+                $tracking = $client->getOrderStatus($orderId, $invoice);
+                if ($tracking->trackingNumber) {
+                    echo $tracking->trackingNumber;
+                } else {
+                    echo '<NotYet>';
+                }
+
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+            }
+
+            echo EOL;
+        }
+    }
+}
+
+include __DIR__ . '/../public/init.php';
+
+$job = new DropshipTrackingJob();
+$job->run($argv);
