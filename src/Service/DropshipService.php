1ea7655b45c8660a3b2fc3a0c8011f8f7505c84c
Hanson Li <lihsca@gmail.com>
2016-11-11 12:59:59 -0500

    DropshipService::getMultiItemOrders()

--- src/Service/DropshipService.php
+++ src/Service/DropshipService.php
@@ -6,4 +6,17 @@ use Phalcon\Di\Injectable;
 
 class DropshipService extends Injectable
 {
+    public function getMultiItemOrders()
+    {
+        $result = [];
+
+        $sql = 'SELECT order_id, count(*) as c FROM ca_order_notes GROUP BY order_id HAVING c>1';
+        $orders = $this->db->fetchAll($sql);
+
+        foreach ($orders as $order) {
+            $result[] = $order['order_id'];
+        }
+
+        return $result;
+    }
 }
