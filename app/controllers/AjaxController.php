d431b51f8fef6bd54c494506e061a6f4803f0f24
Hanson Li <lihsca@gmail.com>
2016-11-11 22:07:19 -0500

    Move code from controller to service

--- app/controllers/AjaxController.php
+++ app/controllers/AjaxController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controllers;
 
 use Supplier\Supplier;
@@ -93,7 +94,7 @@ class AjaxController extends ControllerBase
 
             try {
                 // Make a xmlapi call to get price and availability
-                $data = $this->getPriceAvailability($sku);
+                $data = $this->priceAvailService->getPriceAvailability($sku);
 
                 // $this->response->setJsonContent(['status' => 'ERROR', 'message' => 'Unknown supplier']);
                 $this->response->setJsonContent(['status' => 'OK', 'data' => $data]);
@@ -177,54 +178,4 @@ class AjaxController extends ControllerBase
     }
 
     /* ===== internal methods ===== */
-
-    protected function getPriceAvailability($items)
-    {
-        $data = [];
-
-        foreach ($items as $sku) {
-            $client = Supplier::createClient($sku);
-            if ($client) {
-                $result = $client->getPriceAvailability($sku);
-                $data[] = $result->getFirst()->toArray();
-            }
-
-            #$data[] = [
-            #    'sku' => $sku,
-            #    'price' => rand(30, 200),
-            #    'avail' => [
-            #        [ 'branch' => 'MISSISSAUGA', 'qty' => rand(10, 30) ],
-            #        [ 'branch' => 'RICHMOND',    'qty' => rand(10, 50) ],
-            #        [ 'branch' => 'MARKHAM',     'qty' => rand(10, 70) ],
-            #    ]
-            #];
-        }
-
-        return $this->sortPriceAvailability($data);
-    }
-
-    protected function sortPriceAvailability($data)
-    {
-        $lowPriceFirst = function($a, $b) {
-            if ($a['price'] == $b['price']) {
-                return 0;
-            }
-            return ($a['price'] < $b['price']) ? -1 : 1; // ASC
-        };
-
-        $maxQtyFirst = function($a, $b) {
-            if ($a['qty'] == $b['qty']) {
-                return 0;
-            }
-            return ($a['qty'] < $b['qty']) ? 1 : -1; // DESC
-        };
-
-        foreach ($data as &$item) {
-            usort($item['avail'], $maxQtyFirst);
-        }
-
-        usort($data, $lowPriceFirst);
-
-        return $data;
-    }
 }
--- src/Service/PriceAvailService.php
+++ src/Service/PriceAvailService.php
@@ -2,9 +2,60 @@
 
 namespace Service;
 
+use Supplier\Supplier;
 use Phalcon\Di\Injectable;
 
 class PriceAvailService extends Injectable
 {
     // see Supplier\XmlApi\PriceAvailability\Client;
+
+    public function getPriceAvailability($items)
+    {
+        $data = [];
+
+        foreach ($items as $sku) {
+            $client = Supplier::createClient($sku);
+            if ($client) {
+                $result = $client->getPriceAvailability($sku);
+                $data[] = $result->getFirst()->toArray();
+            }
+
+            #$data[] = [
+            #    'sku' => $sku,
+            #    'price' => rand(30, 200),
+            #    'avail' => [
+            #        [ 'branch' => 'MISSISSAUGA', 'qty' => rand(10, 30) ],
+            #        [ 'branch' => 'RICHMOND',    'qty' => rand(10, 50) ],
+            #        [ 'branch' => 'MARKHAM',     'qty' => rand(10, 70) ],
+            #    ]
+            #];
+        }
+
+        return $this->sortPriceAvailability($data);
+    }
+
+    protected function sortPriceAvailability($data)
+    {
+        $lowPriceFirst = function($a, $b) {
+            if ($a['price'] == $b['price']) {
+                return 0;
+            }
+            return ($a['price'] < $b['price']) ? -1 : 1; // ASC
+        };
+
+        $maxQtyFirst = function($a, $b) {
+            if ($a['qty'] == $b['qty']) {
+                return 0;
+            }
+            return ($a['qty'] < $b['qty']) ? 1 : -1; // DESC
+        };
+
+        foreach ($data as &$item) {
+            usort($item['avail'], $maxQtyFirst);
+        }
+
+        usort($data, $lowPriceFirst);
+
+        return $data;
+    }
 }
