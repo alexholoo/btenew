f42b29c2b079f796ecb7e49c5adcccd48b718fbf
Hanson Li <lihsca@gmail.com>
2016-11-21 18:00:55 -0500

    DH unit test

--- test/Supplier/DH/PriceAvailabilityRequestTest.php
+++ test/Supplier/DH/PriceAvailabilityRequestTest.php
@@ -3,12 +3,50 @@
 namespace Test\Supplier\DH;
 
 use PHPUnit\Framework\TestCase;
+
 use Supplier\DH\PriceAvailabilityRequest;
+use Supplier\DH\PriceAvailabilityResponse;
+
+use Supplier\DH\PurchaseOrderRequest;
+use Supplier\DH\PurchaseOrderResponse;
+
+use Supplier\DH\OrderStatusRequest;
+use Supplier\DH\OrderStatusResponse;
 
 class PriceAvailabilityRequestTest extends TestCase
 {
-    public function testRequest()
+    public function testPriceAvailabilityRequest()
     {
+        $config = $this->getConfig();
+
+        $sku = 'DH-SKU-12345';
+
         $request = new PriceAvailabilityRequest();
+        $request->setConfig($config);
+        $request->addPartnum($sku);
+
+        $this->assertEquals($request->getPartnum(), $sku);
+
+        $xml = simplexml_load_string($request->toXml(), 'SimpleXMLElement', LIBXML_NOCDATA);
+
+        $this->assertEquals($xml->REQUEST,          'price-availability');
+        $this->assertEquals($xml->LOGIN->USERID,    $config['username']);
+        $this->assertEquals($xml->LOGIN->PASSWORD,  $config['password']);
+        $this->assertEquals($xml->PARTNUM,          substr($sku, 3));
+    }
+
+    protected function getConfig()
+    {
+        return array(
+            'username'    => 'DHUSERNAME',
+            'password'    => 'DHPASSWORD',
+            'dropshippw'  => '',
+            'partship'    => 'N',
+            'backorder'   => 'N',
+            'shipcarrier' => 'Purolator',
+            'shipservice' => 'Ground',
+            'onlybranch'  => 'Toronto',
+            'branches'    => '', // 3, 1-7
+        );
     }
 }
