ac59c0bb49a6bf200787c2ad54c2eeeb256a36f7
Hanson Li <lihsca@gmail.com>
2016-11-21 11:09:46 -0500

    move order unit test to OrderTest

--- test/Supplier/Model/OrderTest.php
+++ test/Supplier/Model/OrderTest.php
@@ -14,13 +14,13 @@ class OrderTest extends TestCase
 
         $order = new Order($info);
 
-        $this->assertEquals($order->channel,     'Amazon-ACA');
-        $this->assertEquals($order->date,        '2016-08-29');
-        $this->assertEquals($order->orderId,     '701-3707503-5766610');
-        $this->assertEquals($order->express,     '0');
-        $this->assertEquals($order->shipMethod,  'UPX');
-        $this->assertEquals($order->branch,      '57');
-        $this->assertEquals($order->comment,     'Price match D&amp;H $335.55');
+        $this->assertEquals($order->channel,     $info['channel']);
+        $this->assertEquals($order->date,        $info['date']);
+        $this->assertEquals($order->orderId,     $info['orderId']);
+        $this->assertEquals($order->express,     $info['express']);
+        $this->assertEquals($order->shipMethod,  $info['shipMethod']);
+        $this->assertEquals($order->branch,      $info['branch']);
+        $this->assertEquals($order->comment,     htmlspecialchars($info['comment']));
         $this->assertEquals($order->notifyEmail, null);
 
         $this->assertInstanceOf('Supplier\\Model\\OrderAddress', $order->shippingAddress);
@@ -28,6 +28,36 @@ class OrderTest extends TestCase
         $this->assertTrue(is_array($order->items));
         $this->assertCount(1, $order->items);
         $this->assertInstanceOf('Supplier\\Model\\OrderItem', $order->items[0]);
+    }
+
+    public function testOrderItem()
+    {
+        $order = $this->getOrder();
+
+        $item = new OrderItem($order);
+
+        $this->assertEquals($item->orderId, $order['orderId']);
+        $this->assertEquals($item->sku,     $order['sku']);
+        $this->assertEquals($item->price,   $order['price']);
+        $this->assertEquals($item->qty,     $order['qty']);
+    }
+
+    public function testOrderAddress()
+    {
+        $order = $this->getOrder();
+
+        $address = new OrderAddress($order);
+
+        $this->assertEquals($address->contact,    $order['buyer']);
+        $this->assertEquals($address->address,    $order['address']);
+        $this->assertEquals($address->city,       $order['city']);
+        $this->assertEquals($address->province,   $order['province']);
+        $this->assertEquals($address->state,      $order['province']);
+        $this->assertEquals($address->zipcode,    $order['postalcode']);
+        $this->assertEquals($address->postalcode, $order['postalcode']);
+        $this->assertEquals($address->country,    $order['country']);
+        $this->assertEquals($address->phone,      $order['phone']);
+        $this->assertEquals($address->email,      $order['email']);
     }
 
     public function getOrder()
