139effcc14c36ddcc21a47236c01fdf5df9fe026
Hanson Li <lihsca@gmail.com>
2016-11-28 15:03:29 -0500

    migration fix

--- sql/20161125-111127_express_in_master_order.sql
+++
@@ -1,4 +0,0 @@
-USE bte;
-
-ALTER TABLE `master_order`
-	ADD COLUMN `express` TINYINT NOT NULL COMMENT 'boolean' AFTER `order_id`;
--- sql/20161128-130127_master_order.sql
+++ sql/20161128-130127_master_order.sql
@@ -1,6 +1,6 @@
 USE bte;
 
-CREATE TABLE IF NOT EXIST `master_order` (
+CREATE TABLE IF NOT EXISTS `master_order` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`channel` VARCHAR(20) NOT NULL,
 	`date` DATE NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXIST `master_order` (
 	UNIQUE INDEX `order_id` (`order_id`)
 ) COLLATE='utf8_general_ci' ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXIST `master_order_item` (
+CREATE TABLE IF NOT EXISTS `master_order_item` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`order_id` VARCHAR(40) NOT NULL,
 	`sku` VARCHAR(40) NOT NULL,
