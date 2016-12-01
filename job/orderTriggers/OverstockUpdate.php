017ddb611b09b1440e4923280a7cf90bfa7eac24
Hanson Li <lihsca@gmail.com>
2016-12-01 15:55:13 -0500

    orderTriggers/OverstockUpdate

---
+++ job/orderTriggers/OverstockUpdate.php
@@ -0,0 +1,113 @@
+<?php
+
+class OverstockUpdate
+{
+    protected $priority = 20;  // 0 to disable
+    protected $orders;
+
+    public function __construct()
+    {
+        $this->di = \Phalcon\Di::getDefault();
+        $this->db = $this->di->get('db');
+        $this->queue = $this->di->get('queue');
+    }
+
+    public function setOrders($orders)
+    {
+        $this->orders = $orders;
+    }
+
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    public function run($argv = [])
+    {
+        echo '>> ', __CLASS__, EOL;
+
+        $this->updateOverstock();
+    }
+
+    protected function updateOverstock()
+    {
+        echo count($this->orders), ' new orders', EOL;
+
+        if (count($this->orders) > 0) {
+
+            $accdb = $this->openAccessDB();
+
+            foreach ($this->orders as $order) {
+                $date    = $order['date'];
+                $channel = $order['channel'];
+                $orderId = $order['order_id'];
+                $sku     = $order['sku'];
+                $qty     = $order['qty'];
+
+                echo "$date $channel $orderId $sku $qty", EOL;
+
+                $sql = "SELECT [Actual Quantity] FROM [overstock-automated] WHERE [SKU Number]='$sku'";
+                $row = $accdb->query($sql)->fetch();
+
+                $qtyOnHand = 0;
+                if ($row) {
+                    $qtyOnHand = $row['Actual Quantity'];
+                }
+
+                $x = 0;
+                $change = 'No change';
+                if ($qtyOnHand > 0) {
+                    $change = "-$qty";
+                    $x = $qtyOnHand - $qty;
+                    if ($x < 0) {
+                        $x = 0;
+                        $note = "-$qtyOnHand";
+                    }
+                }
+
+                $sql = "UPDATE [overstock-automated] SET [Actual Quantity]=$x WHERE [SKU Number]='$sku'";
+
+                $ret = $accdb->exec($sql);
+                if (!$ret && $accdb->errorCode() != '00000') {
+                    print_r($accdb->errorInfo());
+                }
+
+                $sql = "INSERT INTO [overstock-change] (
+                            [OrderDate],
+                            [Channel],
+                            [OrderNo],
+                            [SKU],
+                            [Qty],
+                            [Change]
+                        )
+                        VALUES (
+                            '$date',
+                            '$channel',
+                            '$orderId',
+                            '$sku',
+                            $qty,
+                            '$change'
+                        )";
+
+                $ret = $accdb->exec($sql);
+                if (!$ret && $accdb->errorCode() != '00000') {
+                    print_r($accdb->errorInfo());
+                }
+            }
+        }
+    }
+
+    protected function openAccessDB()
+    {
+        $dbname = "z:/BTE-Price-List/bte-dataprocess-files.accdb";
+
+        if (gethostname() != 'BTELENOVO') {
+            $dbname = "C:/Users/BTE/Desktop/bte-dataprocess-files.accdb";
+        }
+
+        $dsn = "odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=$dbname;";
+        $db = new PDO($dsn);
+
+        return $db;
+    }
+}
