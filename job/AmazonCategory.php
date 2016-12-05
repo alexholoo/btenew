4647a9f14d869d0290d3b509bff9a3879e4aa661
Hanson Li <lihsca@gmail.com>
2016-12-05 15:08:10 -0500

    AmazonCategory job

---
+++ job/AmazonCategory.php
@@ -0,0 +1,196 @@
+<?php
+
+include 'classes/Job.php';
+
+class AmazonCategory extends Job
+{
+    public function run($argv = [])
+    {
+        $this->log('>> '. __CLASS__);
+
+        $this->saveCategories();
+    }
+
+    private function saveCategories()
+    {
+        $orders = $this->getOrders();
+
+        foreach ($orders as $order) {
+            $channel = $order['channel'];
+            $sku     = $order['sku'];
+
+            if (strtolower(substr($channel, 0, 6)) != 'amazon') {
+                continue; // go to next SKU
+            }
+
+            if ($this->skuCategoryExists($sku)) {
+                continue; // go to next SKU
+            }
+
+            $category = $this->getAmazonSkuCategory($channel, $sku);
+
+            $id   = $category['ProductCategoryId'];
+            $name = $category['ProductCategoryName'];
+
+            $this->log("$sku\t$id\t$name");
+
+            $names = [];
+            while ($category) {
+                $catId   = $category['ProductCategoryId'];
+                $catName = $category['ProductCategoryName'];
+
+                array_unshift($names, $catName);
+
+                if (isset($category['Parent'])) {
+                    $parentId = $category['Parent']['ProductCategoryId'];
+                    $category = $category['Parent'];
+                } else {
+                    $parentId = '';
+                    $category = null;
+                }
+
+                $this->saveAmazonCategory($catId, $catName, $parentId);
+            }
+
+            array_shift($names); // remove duplicated 'Categories'
+            $this->saveSkuCategory($sku, $id, implode(' > ', $names));
+
+            sleep(6); // Amazon throttling
+        }
+
+        $this->log(count($orders). ' SKUs processed.');
+    }
+
+    private function getAmazonSkuCategory($channel, $sku)
+    {
+        $this->log("getCategory: $channel $sku");
+
+        $store = 'bte-amazon-ca';
+
+        if ($channel = 'Amazon-US') {
+            $store = 'bte-amazon-us';
+        }
+
+        $api = new \AmazonProductInfo($store);
+
+        $api->setSKUs($sku);
+        $api->fetchCategories();
+
+        $products = $api->getProduct();
+        $product = $products[0];
+
+        $data = $product->getData();
+        $category = $data['Categories'][0];
+
+        return $category;
+    }
+
+    private function saveAmazonCategory($id, $name, $parent)
+    {
+        if ($this->amazonCategoryExists($id)) {
+            return;
+        }
+
+        try {
+            $this->db->insertAsDict('amazon_category', [
+                'category_id'   => $id,
+                'category_name' => $name,
+                'parent_id'     => $parent
+            ]);
+        } catch (\Exception $e) {
+            //echo $e->getMessage(), EOL;
+        }
+    }
+
+    private function saveSkuCategory($sku, $catId, $catName)
+    {
+        if ($this->skuCategoryExists($sku)) {
+            return;
+        }
+
+        try {
+            $this->db->insertAsDict('amazon_sku_category', [
+                'sku'           => $sku,
+                'category_id'   => $catId,
+                'category_name' => $catName
+            ]);
+        } catch (\Exception $e) {
+            //echo $e->getMessage(), EOL;
+        }
+    }
+
+    private function amazonCategoryExists($id)
+    {
+        $sql = "SELECT category_id FROM amazon_category WHERE category_id='$id'";
+        $result = $this->db->fetchOne($sql);
+        return ($result);
+    }
+
+    private function skuCategoryExists($sku)
+    {
+        $sql = "SELECT sku FROM amazon_sku_category WHERE sku='$sku'";
+        $result = $this->db->fetchOne($sql);
+        return $result;
+    }
+
+    protected function getOrders()
+    {
+        $filename = 'w:/out/shipping/all_mgn_orders.csv';
+        if (gethostname() == 'BTELENOVO') {
+            $filename = 'E:/BTE/import/all_mgn_orders.csv';
+        }
+
+        if (!($handle = @fopen($filename, 'rb'))) {
+            $this->log("Failed to open file: $filename");
+            return [];
+        }
+
+        $this->log("Loading $filename");
+
+        $title = fgetcsv($handle);
+
+        $columns = [
+             'channel',
+             'date',
+             'order_id',
+             'mgn_order_id',
+             'express',
+             'buyer',
+             'address',
+             'city',
+             'province',
+             'postalcode',
+             'country',
+             'phone',
+             'email',
+             'sku',
+             'price',
+             'qty',
+             'shipping',
+             'product_name',
+        ];
+
+        $orders = [];
+
+        while (($fields = fgetcsv($handle))) {
+            $orderId = $fields[2];
+            if (count($columns) != count($fields)) {
+                $this->log('Error: '. $orderId);
+            }
+            $order = array_combine($columns, $fields);
+            $orders[] = [
+                'channel' => $order['channel'],
+                'sku'     => $order['sku'],
+            ];
+        }
+
+        fclose($handle);
+
+        return $orders;
+    }
+}
+
+include __DIR__ . '/../public/init.php';
+
+$job = new AmazonCategory();
+$job->run($argv);
--- sql/20161205-130110_amazon_category.sql
+++ sql/20161205-130110_amazon_category.sql
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS `amazon_category` (
 CREATE TABLE IF NOT EXISTS `amazon_sku_category` (
 	`sku` VARCHAR(20) NOT NULL,
 	`category_id` VARCHAR(20) NOT NULL,
-	`category_name` VARCHAR(80) NOT NULL,
+	`category_name` VARCHAR(200) NOT NULL,
 	PRIMARY KEY (`sku`)
 ) COLLATE='utf8_general_ci' ENGINE=InnoDB;
