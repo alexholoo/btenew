ca7cea755f6c557a1a25e411f828b5ba24a8330a
Hanson Li <lihsca@gmail.com>
2016-12-06 13:43:25 -0500

    amazon endpoints and marketplaceids

---
+++ doc/amazon/endpoints.php
@@ -0,0 +1,8 @@
+Amazon_MWS_Endpoint
+===================
+
+North America (NA) 	https://mws.amazonservices.com
+Europe (EU) 	    https://mws-eu.amazonservices.com
+India (IN) 	        https://mws.amazonservices.in
+China (CN) 	        https://mws.amazonservices.com.cn
+Japan (JP) 	        https://mws.amazonservices.jp
---
+++ doc/amazon/marketplace.php
@@ -0,0 +1,28 @@
+Amazon_MWS_MarketplaceIds
+=========================
+
+Table 2. NA region
+------------------
+CA 	A2EUQ1WTGCTBG2
+MX 	A1AM78C64UM0Y8
+US 	ATVPDKIKX0DER
+
+Table 3. EU region
+------------------
+DE 	A1PA6795UKMFR9
+ES 	A1RKKUPIHCS9HS
+FR 	A13V1IB3VIYZZH
+IT 	APJ6JRA9NG5V4
+UK 	A1F83G8C2ARO7P
+
+Table 4. IN region
+------------------
+IN 	A21TJRUUN4KGV
+
+Table 5. FE region
+------------------
+JP 	A1VC38T7YXB528
+
+Table 6. CN region
+------------------
+CN 	AAHKV2X7AFYLW
