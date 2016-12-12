795b5959eba458c9b24284f02fde527e6a2ca4ea
Hanson Li <lihsca@gmail.com>
2016-12-12 16:03:26 -0500

    AmazonCaOrderImportToAccessJob

---
+++ job/AmazonCaOrderImportToAccessJob.php
@@ -0,0 +1,168 @@
+<?php
+
+include 'classes/Job.php';
+
+class AmazonCaOrderImportToAccessJob extends Job
+{
+    public function run($argv = [])
+    {
+        $this->log('>> '. __CLASS__);
+
+        $this->importAmazonCAOrders();
+    }
+
+    protected function importAmazonCAOrders()
+    {
+        $orders = $this->getOrders();
+        $accdb = $this->openAccessDB();
+
+        $today = date('Y-m-d', strtotime('Yesterday'));
+
+        foreach ($orders as $order) {
+            if (($date = $order['date']) < $today) {
+                continue;
+            }
+
+            $sku         = $this->getSku($order['sku']);
+            $orderid     = $order['order_id'];
+            $xpress      = $order['express'];
+            $stockStatus = $order['stock_status'];
+            $qty         = $order['qty'];
+            $supplier    = $this->getSupplier($order['sku']);
+            $ponum       = ' ';
+            $mfrpn       = $this->getMfrPartNum($order['sku']);
+
+            $sql = "SELECT * FROM Amazon_CA WHERE [order_id]='$orderid'";
+            $result = $accdb->query($sql)->fetch();
+            if ($result) {
+                continue;
+            }
+
+            $sql = "INSERT INTO Amazon_CA (
+                        [date],
+                        [order_id],
+                        [Stock Status],
+                        [Xpress],
+                        [qty],
+                        [supplier],
+                        [supplier_sku],
+                        [MPN],
+                        [Supplier#],
+                        [notes],
+                        [related_sku],
+                        [dimension]
+                    )
+                    VALUES (
+                        '$date',
+                        '$orderid',
+                        '$stockStatus',
+                        '$xpress',
+                        '$qty',
+                        '$supplier',
+                        '$sku',
+                        '$mfrpn',
+                        '$ponum',
+                        '',
+                        '',
+                        ''
+                    )";
+
+            $ret = $accdb->exec($sql);
+
+            if (!$ret) {
+                print_r($this->accdb->errorInfo());
+            }
+
+            echo $orderid, EOL;
+        }
+    }
+
+    protected function openAccessDB()
+    {
+        $dbname = "Z:/Purchasing/General Purchase.accdb";
+
+        if (gethostname() != 'BTELENOVO') {
+            $dbname = "C:/Users/BTE/Desktop/General Purchase.accdb";
+        }
+
+        $dsn = "odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=$dbname;";
+        $db = new PDO($dsn);
+
+        return $db;
+    }
+
+    protected function getOrders()
+    {
+        $file = 'E:/BTE/import/ca_order_notes.csv';
+
+        if (gethostname() != 'BTELENOVO') {
+            $file = 'w:/out/purchasing/ca_order_notes.csv';
+        }
+
+        if (($fh = @fopen($file, 'rb')) === false) {
+            echo "Failed to open file: $file\n";
+            return [];
+        }
+
+        echo "Loading $file\n";
+
+        fgetcsv($fh); // skip the first line
+
+        $orders = [];
+        while(($fields = fgetcsv($fh))) {
+            $orders[] = [
+                'date'         => $fields[0],
+                'order_id'     => $fields[1],
+                'stock_status' => $fields[2],
+                'express'      => $fields[3],
+                'qty'          => $fields[4],
+                'supplier'     => $fields[5],
+                'sku'          => $fields[6],
+                'mpn'          => $fields[7],
+                'supplier_no'  => $fields[8],
+                'notes'        => $fields[9],
+                'related_sku'  => $fields[10],
+                'dimension'    => $fields[11],
+            ];
+        }
+
+        fclose($fh);
+
+        return $orders;
+    }
+
+    protected function getSku($sku)
+    {
+        $parts = explode('-', $sku);
+        array_shift($parts);
+        return implode('-', $parts);
+    }
+
+    protected function getSupplier($sku)
+    {
+        $names = [
+            'AS'  => 'ASI',
+            'SYN' => 'Synnex',
+            'ING' => 'Ingram Micro',
+            'EP'  => 'Eprom',
+            'TD'  => 'Techdata',
+            'TAK' => 'Taknology',
+            'SP'  => 'Supercom',
+        ];
+
+        $parts = explode('-', $sku);
+        $prefix = $parts[0];
+
+        return isset($names[$prefix]) ? $names[$prefix] : $prefix;
+    }
+
+    protected function getMfrPartNum($sku)
+    {
+        return $this->di->get('productService')->getMpnFromSku($sku);
+    }
+}
+
+include __DIR__ . '/../public/init.php';
+
+$job = new AmazonCaOrderImportToAccessJob();
+$job->run($argv);
