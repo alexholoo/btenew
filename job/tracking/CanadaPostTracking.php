042f75b2d0ef7d08bad0810ba5da0b0a74d2dd07
Hanson Li <lihsca@gmail.com>
2016-12-15 12:47:36 -0500

    CanadaPost + DH tracking

---
+++ job/tracking/CanadaPostTracking.php
@@ -0,0 +1,110 @@
+<?php
+
+class CanadaPostTracking extends Job
+{
+    protected $masterShipment;
+    protected $amazonCAshipment;
+    protected $amazonUSshipment;
+
+    public function getStatus()
+    {
+        return 1; // 1-enabled, 2-disabled
+    }
+
+    public function setAmazonCAshipment($amazonCAshipment)
+    {
+        $this->amazonCAshipment = $amazonCAshipment;
+    }
+
+    public function setAmazonUSshipment($amazonUSshipment)
+    {
+        $this->amazonUSshipment = $amazonUSshipment;
+    }
+
+    public function setMasterShipment($masterShipment)
+    {
+        $this->masterShipment = $masterShipment;
+    }
+
+    public function merge()
+    {
+        $filename = 'w:/out/shipping/cpc.xml';
+
+        if (gethostname() == 'BTELENOVO') {
+            $filename = 'E:/BTE/shipping/cpc.xml';
+        }
+
+        $xml = simplexml_load_file($filename);
+
+        foreach ($xml->{'delivery-request'} as $request) {
+
+            $recipient = $request->{'delivery-spec'}->destination->recipient;
+            $reference = $request->{'delivery-spec'}->reference;
+
+            $orderId = $recipient->{'customer-client-id'};
+            if (!$orderId) {
+                $orderId = $reference->{'customer-ref1'};
+            }
+
+            $name    = $recipient->{'contact-name'};
+            $address = $recipient->{'address-line-1'};
+            $city    = $recipient->{'city'};
+            $state   = $recipient->{'prov-state'};
+            $zip     = $recipient->{'postal-zip-code'};
+            $country = $recipient->{'country-code'};
+
+            $fullAddress = "$name, $address, $city, $state, $zip, $country";
+
+            if ($orderId) {
+
+                $orderItemId = '';
+                $quantity = '';
+                $shipDate = $request->{'settlement-details'}->{'mailing-date'};
+                $carrierCode = 'Canada Post';
+                $carrierName = '';
+                $trackingNumber = $reference->{'item-id'};
+                $shipMethod = 'BTE';
+                $site= 'Canada';
+
+                //echo "$orderId $trackingNumber\n";
+
+                $row = [
+                    $orderId,
+                    $orderItemId,
+                    $quantity,
+                    $shipDate,
+                    $carrierCode,
+                    $carrierName,
+                    $trackingNumber,
+                    $shipMethod,
+                    $site
+                ];
+
+                if ($country == 'CA' && $this->amazonCAshipment) {
+                    $this->amazonCAshipment->write($row);
+                }
+
+                if ($country == 'MX' && $this->amazonUSshipment) {
+                    $this->amazonUSshipment->write($row);
+                }
+
+                $row = [
+                    $orderId,
+                    $orderItemId,
+                    $quantity,
+                    $shipDate,
+                    $carrierCode,
+                    $carrierName,
+                    $trackingNumber,
+                    $shipMethod,
+                    $fullAddress,
+                    $site
+                ];
+
+                if ($this->masterShipment) {
+                    $this->masterShipment->write($row);
+                }
+            }
+        }
+    }
+}
---
+++ job/tracking/DHTracking.php
@@ -0,0 +1,113 @@
+<?php
+
+class DHTracking extends Job
+{
+    protected $masterShipment;
+    protected $amazonCAshipment;
+    protected $amazonUSshipment;
+
+    public function getStatus()
+    {
+        return 1; // 1-enabled, 2-disabled
+    }
+
+    public function setAmazonCAshipment($amazonCAshipment)
+    {
+        $this->amazonCAshipment = $amazonCAshipment;
+    }
+
+    public function setAmazonUSshipment($amazonUSshipment)
+    {
+        $this->amazonUSshipment = $amazonUSshipment;
+    }
+
+    public function setMasterShipment($masterShipment)
+    {
+        $this->masterShipment = $masterShipment;
+    }
+
+    public function merge()
+    {
+        #$columns = [
+        #    // 0,    1,             2,           3,              4,          5,
+        #    [ 'H1', 'OrderNo',     'InvoiceNo', 'InvoiceTotal' ],
+        #    [ 'D1', 'TrackingNum', 'Carrier',   'ServiceLevel', 'ShipMode', 'DateShipped' ],
+        #    [ 'D2', 'ModelNo',     'Qty',       'SerialNo+',    'Price' ],
+        #];
+
+        $filename = 'w:/data/csv/DH-TRACKING';
+
+        if (gethostname() == 'BTELENOVO') {
+            $filename = 'E:/BTE/shipping/DH-TRACKING';
+        }
+
+        $fmtdate = function($str) {
+            return substr($str, 4).'-'.substr($str, 0, 2).'-'.substr($str, 2, 2);
+        };
+
+        // import to dropship_tracking
+        if (($fp = fopen($filename, 'r')) !== NULL) {
+            return;
+        }
+
+        while ($fields = fgetcsv($fp, 0, '|')) {
+            if ($fields[0] == 'H1') {
+                $fields = array_map('trim', $fields);
+
+                $orderId = $fields[1];
+
+                $fields = fgetcsv($fp, 0, '|');
+
+                if ($fields[0] == 'D1') {
+                    $fields = array_map('trim', $fields);
+
+                    $trackingNumber = $fields[1];
+                    $carrierCode = $fields[2]; //. ' ' .$fields[3];
+                    $carrierName = ''; //$fields[4];
+                    $shipDate = $fmtdate($fields[5]);
+                }
+
+                if ($trackingNumber) {
+                    $orderItemId = '';
+                    $quantity = '';
+                    $shipMethod = 'DH_DS';
+                    $fullAddress = '';
+                    $site = 'Canada';
+
+                    if ($this->amazonCAshipment) {
+                        $row = [
+                            $orderId,
+                            $orderItemId,
+                            $quantity,
+                            $shipDate,
+                            $carrierCode,
+                            $carrierName,
+                            $trackingNumber,
+                            $shipMethod,
+                            $site
+                        ];
+                        $this->amazonCAshipment->write($row);
+                    }
+
+                    if ($this->masterShipment) {
+                        $row = [
+                            $orderId,
+                            $orderItemId,
+                            $quantity,
+                            $shipDate,
+                            $carrierCode,
+                            $carrierName,
+                            $trackingNumber,
+                            $shipMethod,
+                            $fullAddress,
+                            $site
+                        ];
+                        $this->masterShipment->write($row);
+                    }
+                }
+            }
+        }
+
+        fclose($fp);
+    }
+}
