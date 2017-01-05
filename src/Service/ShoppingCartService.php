c86d0c2459cb3564b5cdb21ed998ab4a7c65a099
Hanson Li <lihsca@gmail.com>
2017-01-05 15:39:09 -0500

    change to ShoppingCartService

--- src/Service/ShoppingCartService.php
+++ src/Service/ShoppingCartService.php
@@ -6,6 +6,9 @@ use Phalcon\Di\Injectable;
 
 class ShoppingCartService extends Injectable
 {
+    /**
+     * Get orders in shopping cart (that created today, not checked out yet)
+     */
     public function getOrders($column = '')
     {
         // get all orders that are not dropshipped
@@ -14,7 +17,7 @@ class ShoppingCartService extends Injectable
         #     LEFT JOIN purchase_order_log po ON sc.order_id = po.orderid
         #         WHERE po.id IS NULL';
 
-        // get all orders that are not checked out 
+        // get all orders that are created today and not checked out
         $sql = 'SELECT order_id as orderId, sku, qty
                   FROM shopping_cart
                  WHERE checkedout=0 AND DATE(createdon)=DATE(NOW())';
@@ -22,36 +25,59 @@ class ShoppingCartService extends Injectable
         $orders = $this->db->fetchAll($sql);
 
         if ($column) {
-            // $column should be 'orderId'
+            // $column should be 'orderId' if specified
             return array_column($orders, $column);
         }
 
         return $orders;
     }
 
+    /**
+     * Delete order in shopping cart
+     *
+     * If orderId is not specified, delete all orders.
+     *
+     * It only affectes orders that created today and not checked out
+     */
     public function removeOrder($orderId)
     {
         $sql = 'DELETE FROM shopping_cart';
 
-        if ($orderId) {
-            $sql .= " WHERE order_id='$orderId' AND checkedout=0";
-        }
+        $where = $this->getWhere($orderId);
 
-        return $this->db->execute($sql);
+        $this->db->execute($sql.$where);
+
+        return $this->db->affectedRows();
     }
 
+    /**
+     * Mark order in shopping cart as checkedout
+     *
+     * If orderId is not specified, mark all orders.
+     *
+     * It only affectes orders that created today and not checked out
+     */
     public function markOrderAsCheckedout($orderId)
     {
         $sql = 'UPDATE shopping_cart SET checkedout=1 ';
 
+        $where = $this->getWhere($orderId);
+
+        $this->db->execute($sql.$where);
+
+        return $this->db->affectedRows();
+    }
+
+    protected function getWhere($orderId)
+    {
         $where = 'WHERE ';
+
         if ($orderId) {
             $where .= "order_id='$orderId' AND ";
         }
-        $where .= 'DATE(createdon)=DATE(NOW())';
 
-        $sql .= $where;
+        $where .= 'checkedout=0 AND DATE(createdon)=DATE(NOW())';
 
-        return $this->db->execute($sql);
+        return $where;
     }
 }
