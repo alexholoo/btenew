5555f568e58244f1a6f478989085f544836cb43c
Hanson Li <lihsca@gmail.com>
2017-01-05 15:43:22 -0500

    code has been moved to ShoppingCartService

--- src/Service/DropshipService.php
+++ src/Service/DropshipService.php
@@ -14,54 +14,6 @@ class DropshipService extends Injectable
         return array_column($orders, 'order_id');
     }
 
-    public function getOrdersInShoppingCart($column = 'orderId')
-    {
-        // get all orders that are not dropshipped
-        #$sql = 'SELECT sc.order_id as orderId, sc.sku, sc.qty
-        #          FROM shopping_cart sc
-        #     LEFT JOIN purchase_order_log po ON sc.order_id = po.orderid
-        #         WHERE po.id IS NULL';
-
-        // get all orders that are not checked out 
-        $sql = 'SELECT order_id as orderId, sku, qty
-                  FROM shopping_cart
-                 WHERE checkedout=0 AND DATE(createdon)=DATE(NOW())';
-
-        $orders = $this->db->fetchAll($sql);
-
-        if ($column) {
-            return array_column($orders, $column);
-        }
-
-        return $orders;
-    }
-
-    public function removeOrdersInShoppingCart($orderId = '')
-    {
-        $sql = 'DELETE FROM shopping_cart';
-
-        if ($orderId) {
-            $sql .= " WHERE order_id='$orderId' AND checkedout=0";
-        }
-
-        return $this->db->execute($sql);
-    }
-
-    public function markOrderAsCheckedout($orderId = '')
-    {
-        $sql = 'UPDATE shopping_cart SET checkedout=1 ';
-
-        $where = 'WHERE ';
-        if ($orderId) {
-            $where .= "order_id='$orderId' AND ";
-        }
-        $where .= 'DATE(createdon)=DATE(NOW())';
-
-        $sql .= $where;
-
-        return $this->db->execute($sql);
-    }
-
     public function isOrderPurchased($orderId)
     {
         $sql = "SELECT orderid, sku FROM purchase_order_log WHERE orderid='$orderId'";
