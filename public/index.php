1461e6f07918750a5fc9667c6c1fdfa69f1cac5d
Hanson Li <lihsca@gmail.com>
2017-01-06 16:00:25 -0500

    include init.php in index.php

--- public/index.php
+++ public/index.php
@@ -2,44 +2,9 @@
 
 error_reporting(E_ALL);
 
-date_default_timezone_set("America/Toronto");
-
-#mb_internal_encoding('UTF-8');
-#mb_http_output('UTF-8');
-#mb_http_input('UTF-8');
-#mb_language('uni');
-#mb_regex_encoding('UTF-8');
+include 'init.php';
 
 try {
-
-    /**
-     * Define some useful constants
-     */
-    define('BASE_DIR', dirname(__DIR__));
-    define('APP_DIR', BASE_DIR . '/app');
-
-    include 'trace.php';
-
-    /**
-     * include autoload earlier, so we can use class-constants in config.php
-     */
-    require_once __DIR__ . '/../vendor/autoload.php';
-
-	/**
-	 * Read the configuration
-	 */
-	$config = include APP_DIR . '/config/config.php';
-
-	/**
-	 * Read auto-loader
-	 */
-	include APP_DIR . '/config/loader.php';
-
-	/**
-	 * Read services
-	 */
-	include APP_DIR . '/config/services.php';
-
 	/**
 	 * Handle the request
 	 */
