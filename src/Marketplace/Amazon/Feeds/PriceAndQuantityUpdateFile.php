6aa2e39b0ee0760180d4360b80d527e650345946
Hanson Li <lihsca@gmail.com>
2017-01-20 16:14:26 -0500

    Amazon/Feed/FlatFile

--- src/Marketplace/Amazon/Feeds/PriceAndQuantityUpdateFile.php
+++ src/Marketplace/Amazon/Feeds/PriceAndQuantityUpdateFile.php
@@ -2,12 +2,11 @@
 
 namespace Marketplace\Amazon\Feeds;
 
-class PriceAndQuantityUpdateFile
+class PriceAndQuantityUpdateFile extends FlatFile
 {
-    protected $filename;
-    protected $handle;
-    protected $headline = ['sku', 'price', 'quantity'];
-    protected $headline2 = [
+    protected $columns  = ['sku', 'price', 'quantity'];
+
+    protected $columns2 = [
         'sku',
         'price',
         'minimum-seller-allowed-price',
@@ -15,46 +14,4 @@ class PriceAndQuantityUpdateFile
         'quantity',
         'leadtime-to-ship'
     ];
-
-    public function __construct($filename)
-    {
-        $this->filename = $filename;
-    }
-
-    public function __destruct()
-    {
-        $this->close();
-    }
-
-    public function getFilename()
-    {
-        return $this->filename;
-    }
-
-    public function close()
-    {
-        if (is_resource($this->handle)) {
-            fclose($this->handle);
-        }
-    }
-
-    public function write($data)
-    {
-        if (!$this->handle) {
-            if (file_exists($this->filename)) {
-                $this->handle = fopen($this->filename, 'a');
-            } else {
-                $this->handle = fopen($this->filename, 'w');
-                if ($this->headline) {
-                    fputcsv($this->handle, $this->headline, "\t");
-                }
-            }
-        }
-
-        if (count($data) != count($this->headline)) {
-            throw new \Exception('Wrong number of elements: '. var_export($data, true));
-        }
-
-        return fputcsv($this->handle, $data, "\t");
-    }
 }
--- src/Marketplace/Amazon/Feeds/ShippingTemplateFile.php
+++ src/Marketplace/Amazon/Feeds/ShippingTemplateFile.php
@@ -2,51 +2,7 @@
 
 namespace Marketplace\Amazon\Feeds;
 
-class ShippingTemplateFile
+class ShippingTemplateFile extends FlatFile
 {
-    protected $filename;
-    protected $handle;
-    protected $headline = ['sku', 'merchant_shipping_group_name'];
-
-    public function __construct($filename)
-    {
-        $this->filename = $filename;
-    }
-
-    public function __destruct()
-    {
-        $this->close();
-    }
-
-    public function getFilename()
-    {
-        return $this->filename;
-    }
-
-    public function close()
-    {
-        if (is_resource($this->handle)) {
-            fclose($this->handle);
-        }
-    }
-
-    public function write($data)
-    {
-        if (!$this->handle) {
-            if (file_exists($this->filename)) {
-                $this->handle = fopen($this->filename, 'a');
-            } else {
-                $this->handle = fopen($this->filename, 'w');
-                if ($this->headline) {
-                    fputcsv($this->handle, $this->headline, "\t");
-                }
-            }
-        }
-
-        if (count($data) != count($this->headline)) {
-            throw new \Exception('Wrong number of elements: '. var_export($data, true));
-        }
-
-        return fputcsv($this->handle, $data, "\t");
-    }
+    protected $columns = ['sku', 'merchant_shipping_group_name'];
 }
