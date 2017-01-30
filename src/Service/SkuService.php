d981c60cea915624d2718d7a411a828ee7fe8eb6
Hanson Li <lihsca@gmail.com>
2017-01-30 13:34:55 -0500

    getAltSkus in SkuService

--- src/Service/SkuService.php
+++ src/Service/SkuService.php
@@ -54,4 +54,43 @@ class SkuService extends Injectable
         // $info = $this->db->fetchOne($sql);
         // return $info;
     }
+
+    public function getAltSkus($sku)
+    {
+        $info = $this->getMasterSku($sku);
+
+        return array_filter([
+            $info['syn_pn'],
+            $info['td_pn'],
+            $info['ing_pn'],
+            $info['dh_pn'],
+            $info['asi_pn'],
+            $info['tak_pn'],
+            $info['ep_pn'],
+        ]);
+    }
+
+    public function getUpc($sku)
+    {
+        $info = $this->getMasterSku($sku);
+        return isset($info['UPC']) ? $info['UPC'] : '';
+    }
+
+    public function getMpn($sku)
+    {
+        $info = $this->getMasterSku($sku);
+        return isset($info['MPN']) ? $info['MPN'] : '';
+    }
+
+    public function getWeight($sku)
+    {
+        $info = $this->getMasterSku($sku);
+        return isset($info['Weight']) ? $info['Weight'] : '';
+    }
+
+    public function getManufacturer($sku)
+    {
+        $info = $this->getMasterSku($sku);
+        return isset($info['Manufacturer']) ? $info['Manufacturer'] : '';
+    }
 }
