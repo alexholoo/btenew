a9a3e11bbccc89708328a3c53c7c5f971f23b856
Hanson Li <lihsca@gmail.com>
2017-02-16 16:53:33 -0500

    TD and Ingram tracking

--- job/tracking/import/Ingram_Tracking.php
+++ job/tracking/import/Ingram_Tracking.php
@@ -4,15 +4,24 @@ class Ingram_Tracking extends TrackingImporter
 {
     public function import()
     {
-        /*
-                    $this->saveToDb([
-                        'orderId'        => $orderId,
-                        'shipDate'       => $shipDate,
-                        'carrier'        => $carrierCode,
-                        'shipMethod'     => '',
-                        'trackingNumber' => $trackingNumber,
-                        'sender'         => 'ING-DS',
-                    ]);
-        */
+        $filename = 'W:/out/shipping/ing_tracking.csv';
+
+        if (($fp = fopen($filename, 'r')) == false) {
+            $this->error("File not found: $filename");
+            return;
+        }
+
+        while ($fields = fgetcsv($fp)) {
+            $this->saveToDb([
+                'orderId'        => $fields[1],
+                'shipDate'       => $fields[0],
+                'carrier'        => $fields[2],
+                'shipMethod'     => $fields[2],
+                'trackingNumber' => $fields[3],
+                'sender'         => 'ING-DS',
+            ]);
+        }
+
+        fclose($fp);
     }
 }
--- job/tracking/import/Techdata_Tracking.php
+++ job/tracking/import/Techdata_Tracking.php
@@ -4,15 +4,36 @@ class Techdata_Tracking extends TrackingImporter
 {
     public function import()
     {
-        /*
-                    $this->saveToDb([
-                        'orderId'        => $orderId,
-                        'shipDate'       => $shipDate,
-                        'carrier'        => $carrierCode,
-                        'shipMethod'     => '',
-                        'trackingNumber' => $trackingNumber,
-                        'sender'         => 'TD-DS',
-                    ]);
-        */
+        $filename = 'W:/out/shipping/TD_tracking.csv';
+
+        if (($fp = fopen($filename, 'r')) == false) {
+            $this->error("File not found: $filename");
+            return;
+        }
+
+        $columns = fgetcsv($fp);
+
+        while ($fields = fgetcsv($fp)) {
+            if (count($columns) != count($fields)) {
+                $this->error(__METHOD__ . print_r($fields, true));
+                continue;
+            }
+
+            $data = array_combine($columns, $fields);
+
+            list($m, $d, $y) = explode('/', $data['Date']);
+            $data['Date'] = "20$y-$m-$d";
+
+            $this->saveToDb([
+                'orderId'        => $data['PO #'],
+                'shipDate'       => $data['Date'],
+                'carrier'        => $data['Ship Method'],
+                'shipMethod'     => $data['Ship Method'],
+                'trackingNumber' => $data['Tracking'],
+                'sender'         => 'TD-DS',
+            ]);
+        }
+
+        fclose($fp);
     }
 }
