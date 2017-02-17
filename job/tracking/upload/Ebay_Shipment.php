59f64542c975280f155a35a8b0a39b580563680f
Hanson Li <lihsca@gmail.com>
2017-02-17 16:25:42 -0500

    job/tracking/upload/Ebay_Shipment

--- job/tracking/upload/Ebay_Shipment.php
+++ job/tracking/upload/Ebay_Shipment.php
@@ -6,6 +6,38 @@ class Ebay_Shipment extends TrackingUploader
 {
     public function upload()
     {
-        //File::backup($file);
+        // BTE
+        $client = new Marketplace\eBay\Client('bte');
+        $filename = 'w:/out/ship/ebay_shipping_bte.csv';
+        $this->uploadTracking($client, $filename);
+
+        File::backup($filename);
+
+        // ODO
+        $client = new Marketplace\eBay\Client('odo');
+        $filename = 'w:/out/ship/ebay_shipping_odo.csv';
+        $this->uploadTracking($client, $filename);
+
+        File::backup($filename);
+    }
+
+    protected function uploadTracking($client, $filename)
+    {
+        if (($fp = fopen($filename, 'r')) == false) {
+            $this->error("File not found: $filename");
+            return;
+        }
+
+        while (($fields = fgetcsv($fp))) {
+            $client->completeSale([
+                'OrderID'        => $fields[0],
+                'TransactionID'  => $fields[0],
+                'TrackingNumber' => $fields[0],
+                'Carrier'        => $fields[0],
+                'Date'           => $fields[0],
+            ]);
+        }
+
+        fclose($fp);
     }
 }
