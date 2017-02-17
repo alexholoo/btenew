03590f5f4300830264648ff484ee129ae2a8a5e3
Hanson Li <lihsca@gmail.com>
2017-02-17 11:17:06 -0500

    TrackingDownloadJob and TrackingExportJob

---
+++ job/TrackingDownloadJob.php
@@ -0,0 +1,45 @@
+<?php
+
+include 'classes/Job.php';
+
+class TrackingDownloadJob extends Job
+{
+    public function run($argv = [])
+    {
+        $this->log('>> '. __CLASS__);
+
+        $jobs = $this->getDownloadJobs();
+
+        foreach ($jobs as $job) {
+            $this->log('=> ' . get_class($job));
+            $job->download();
+        }
+    }
+
+    protected function getDownloadJobs()
+    {
+        $jobs = [];
+
+        // base class for all tracking downloaders
+        include_once('tracking/download/Base.php');
+
+        foreach (glob("tracking/download/*.php") as $filename) {
+            include_once($filename);
+
+            $path = pathinfo($filename);
+            $class = $path['filename'];
+
+            if (class_exists($class)) {
+                $job = new $class;
+                $jobs[] = $job;
+            }
+        }
+
+        return $jobs;
+    }
+}
+
+include __DIR__ . '/../public/init.php';
+
+$job = new TrackingDownloadJob();
+$job->run($argv);
---
+++ job/TrackingExportJob.php
@@ -0,0 +1,45 @@
+<?php
+
+include 'classes/Job.php';
+
+class TrackingExportJob extends Job
+{
+    public function run($argv = [])
+    {
+        $this->log('>> '. __CLASS__);
+
+        $jobs = $this->getExportJobs();
+
+        foreach ($jobs as $job) {
+            $this->log('=> ' . get_class($job));
+            $job->export();
+        }
+    }
+
+    protected function getExportJobs()
+    {
+        $jobs = [];
+
+        // base class for all tracking exporters
+        include_once('tracking/export/Base.php');
+
+        foreach (glob("tracking/export/*.php") as $filename) {
+            include_once($filename);
+
+            $path = pathinfo($filename);
+            $class = $path['filename'];
+
+            if (class_exists($class)) {
+                $job = new $class;
+                $jobs[] = $job;
+            }
+        }
+
+        return $jobs;
+    }
+}
+
+include __DIR__ . '/../public/init.php';
+
+$job = new TrackingExportJob();
+$job->run($argv);
