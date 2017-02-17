03f328b0269c7586b9e23a3fdd51aa0ff8a4f0e5
Hanson Li <lihsca@gmail.com>
2017-02-17 15:22:14 -0500

    job/tracking/upload/Newegg_Shipment

--- job/tracking/upload/Newegg_Shipment.php
+++ job/tracking/upload/Newegg_Shipment.php
@@ -1,9 +1,17 @@
 <?php
 
+use Toolkit\File;
+use Marketplace\Newegg\Ftp;
+
 class Newegg_Shipment extends TrackingUploader
 {
     public function upload()
     {
-        //File::backup($file);
+        $localFile = 'w:/out/shipping/newegg_canada_tracking.csv';
+        $remoteFile = '/Inbound/Shipping/newegg_canada_tracking.csv';
+
+        Ftp::upload($localFile, $remoteFile);
+
+        File::backup($localFile);
     }
 }
