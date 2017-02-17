c1f3c6a477816bb9274d64b1b194874e89888cda
Hanson Li <lihsca@gmail.com>
2017-02-17 12:30:37 -0500

    tracking/download/Amazon_Tracking

--- job/tracking/download/Amazon_Tracking.php
+++ job/tracking/download/Amazon_Tracking.php
@@ -2,8 +2,80 @@
 
 class Amazon_Tracking extends TrackingDownloader
 {
+    protected $store;
+    protected $folder = 'E:/BTE/amazon/reports/';
+
     public function download()
     {
-        // move AmazonDropshipDownloadJob to here
+        // CA
+        $this->store = 'bte-amazon-ca';
+        $this->orderFile = 'amazon_ca_drop_ship.csv';
+        $this->trackingFile = 'amazon_ca_drop_ship_tracking.csv';
+        $list = $this->downloadTracking();
+
+        // US
+        $this->store = 'bte-amazon-us';
+        $this->orderFile = 'amazon_us_drop_ship.csv';
+        $this->trackingFile = 'amazon_us_drop_ship_tracking.csv';
+        $list = $this->downloadTracking();
+    }
+
+    protected function downloadTracking()
+    {
+        $this->log('Downloading Amazon Dropship Tracking: '.$this->store);
+
+        $api = new \AmazonFulfillmentOrderList($this->store);
+
+        $api->setStartTime('-30 days');
+        $api->setUseToken(true); // auto fetch next
+        $api->fetchOrderList();
+
+        $list = $api->getFullList();
+        if (empty($list)) {
+            return;
+        }
+
+        $orderFile = fopen($this->folder.$this->orderFile, 'w');
+       #fputcsv($orderFile, ['orderId', 'orderDate']);
+
+        $trackingFile = fopen($this->folder.$this->trackingFile, 'w');
+       #fputcsv($trackingFile, ['orderId', 'shipmentStatus', 'Carrier', 'TrackingNumber', 'shippingDate']);
+
+        foreach ($list as $fulfillment) {
+            $order     = $fulfillment->getOrder();
+
+            $details   = $order['Details'];
+            $items     = $order['Items'];
+            $shipments = isset($order['Shipments']) ? $order['Shipments'] : [];
+
+            $orderId   = $details['DisplayableOrderId'];
+            $orderDate = substr($details['DisplayableOrderDateTime'], 0, 10);
+
+            // This is not necessary
+            fputcsv($orderFile, [ $orderId, $orderDate ]);
+
+            // This is what we need
+            foreach ($shipments as $shipment) {
+                $shipmentStatus = $shipment['FulfillmentShipmentStatus'];
+                $shippingDate   = substr($shipment['ShippingDateTime'], 0, 10);
+
+                if ($shipmentStatus != 'SHIPPED') {
+                    continue;
+                }
+
+                foreach ($shipment['FulfillmentShipmentPackage'] as $package) {
+                    fputcsv($trackingFile, [
+                        $orderId,
+                        $shipmentStatus,
+                        $package['CarrierCode'],
+                        $package['TrackingNumber'],
+                        $shippingDate
+                    ]);
+                }
+            }
+        }
+
+        fclose($orderFile);
+        fclose($trackingFile);
     }
 }
