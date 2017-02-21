4a5e1b13c7e17fc11bd3c9e4240762078cbf6fe1
Hanson Li <lihsca@gmail.com>
2017-02-21 14:29:14 -0500

    TD + ING tracking download

--- job/tracking/download/Ingram_Tracking.php
+++ job/tracking/download/Ingram_Tracking.php
@@ -5,6 +5,8 @@ class Ingram_Tracking extends TrackingDownloader
     public function download()
     {
         // No idea how to do
+        $source = 'w:/out/shipping/ing_tracking.csv';
         $filename = Filenames::get('ingram.tracking');
+        copy($source, $filename);
     }
 }
--- job/tracking/download/Techdata_Tracking.php
+++ job/tracking/download/Techdata_Tracking.php
@@ -5,6 +5,8 @@ class Techdata_Tracking extends TrackingDownloader
     public function download()
     {
         // No idea how to do
+        $source = 'w:/out/shipping/TD_tracking.csv';
         $filename = Filenames::get('techdata.tracking');
+        copy($source, $filename);
     }
 }
