93fbfa331d1bef0100009adc21fcf74b50c53b01
Hanson Li <lihsca@gmail.com>
2017-02-24 14:24:13 -0500

    order importers

---
+++ job/order/import/Bestbuy_Order.php
@@ -0,0 +1,107 @@
+<?php
+
+class Bestbuy_Order extends OrderImporter
+{
+    public function import()
+    {
+        $this->skuService = $this->di->get('skuService');
+
+        $this->importBestbuyOrders();
+    }
+
+    protected function importBestbuyOrders()
+    {
+        $accdb = $this->openAccessDB();
+
+        $orders = $this->getBestbuyOrders();
+
+        foreach ($orders as $order) {
+            $workDate    = $order['date'];
+            $orderId     = $order['orderId'];
+            $sku         = $order['sku'];
+            $channel     = 'BestbuyCA';
+            $xpress      = $order['express'];
+            $qty         = $order['qty'];
+            $supplier    = $this->skuService->getSupplier($sku);
+            $ponum       = ' ';
+            $mfrpn       = $this->skuService->getMpn($sku);
+            $stockStatus = ' ';
+
+            // TODO: fix multi-items-order issue
+            $sql = "SELECT * FROM BestbuyCA WHERE [Order #]='$orderId'";
+            $result = $accdb->query($sql)->fetch();
+            if ($result) {
+                continue;
+            }
+
+            $data = [
+                'Work Date'     => $workDate,
+                'Channel'       => $channel,
+                'Order #'       => $orderId,
+                'Express'       => $xpress,
+                'Stock Status'  => $stockStatus,
+                'Qty'           => $qty,
+                'Supplier'      => $supplier,
+                'Supplier SKU'  => $sku,
+                'Mfr #'         => $mfrpn,
+                'Supplier #'    => ' ',
+                'Remarks'       => '',
+                'Xpress'        => $xpress,
+                'RelatedSKU'    => '',
+                'Dimension'     => '',
+            ];
+
+            $sql = $this->insertMssql('BestbuyCA', $data);
+
+            $ret = $accdb->exec($sql);
+
+            if (!$ret) {
+                $this->error(__METHOD__);
+                $this->error(print_r($accdb->errorInfo(), true));
+                $this->error($sql);
+            }
+
+            $this->log($orderId);
+        }
+    }
+
+    protected function getBestbuyOrders()
+    {
+        $client = new Marketplace\Bestbuy\Client();
+
+        // only today's orders
+        $start = date('Y-m-d\T00:00:00');
+        $end   = date('Y-m-d\T23:59:59');
+
+        $orders = $client->listOrders($start, $end);
+
+        // TODO: move this to Marketplace\Bestbuy\Client?
+        foreach ($orders as $key => $order) {
+            // order_state:
+            // - WAITING_ACCEPTANCE
+            // - WAITING_DEBIT_PAYMENT
+            // - CANCELED
+            // - RECEIVED
+            if ($order['state'] != 'RECEIVED') {
+                unset($orders[$key]);
+                $this->log($order['orderId'].' '.$order['state']);
+            }
+        }
+
+        return $orders;
+    }
+
+    protected function openAccessDB()
+    {
+        $dbname = "Z:/Purchasing/General Purchase.accdb";
+
+        if (!IS_PROD) {
+            $dbname = "C:/Users/BTE/Desktop/General Purchase.accdb";
+        }
+
+        $dsn = "odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=$dbname;";
+        $db = new PDO($dsn);
+
+        return $db;
+    }
+}
---
+++ job/order/import/Newegg_Order.php
@@ -0,0 +1,191 @@
+<?php
+
+class Newegg_Order extends OrderImporter
+{
+    public function import()
+    {
+        $client = new Marketplace\Newegg\Client('CA');
+        $client->getOrders();
+
+        $folder = $client->getOrderFolder();
+        $this->importOrders($folder);
+    }
+
+    private function importOrders($path)
+    {
+        $path = trim($path, '/');
+
+        $files = glob("$path/OrderList_*.*");
+        foreach ($files as $file) {
+            $this->importOrderFile($file);
+        }
+    }
+
+    private function importOrderFile($file)
+    {
+        if ($this->alreadyImported($file)) {
+            return; // the file already proccessed
+        }
+
+        try {
+            $success = $this->db->insertAsDict('newegg_order_file', [
+                'filename' => basename($file),
+            ]);
+        } catch (Exception $e) {
+            return; // this should never happens
+        }
+
+        if (!($fh = @fopen($file, 'rb'))) {
+            $this->log("Failed to open file: $file");
+            return;
+        }
+
+        fgetcsv($fh); // skip the first line
+
+        $this->log("Importing Newegg Order File: $file");
+
+        $count = 0;
+
+        $columns = $this->getColumns();
+
+        while(($fields = fgetcsv($fh))) {
+
+            if (count($columns) != count($fields)) {
+                $this->error(__METHOD__.' Error: '.$fields[0].' in file '.$file);
+                continue;
+            }
+
+            $order = array_combine($columns, $fields);
+
+            $this->log(basename($file).' - '.$order['OrderNumber']);
+
+            try {
+                $success = $this->db->insertAsDict('newegg_order_report', [
+                    'OrderNumber'         => $order['OrderNumber'],
+                    'OrderDateTime'       => date('Y-m-d H:i:s', strtotime($order['OrderDateTime'])),
+                    'SalesChannel'        => $order['SalesChannel'],
+                    'FulfillmentOption'   => $order['FulfillmentOption'],
+                    'OrderCustomerEmail'  => $order['OrderCustomerEmail'],
+                    'OrderShippingMethod' => $order['OrderShippingMethod'],
+                    'OrderShippingTotal'  => $order['OrderShippingTotal'],
+                    'GSTorHSTTotal'       => $order['GSTorHSTTotal'],
+                    'PSTorQSTTotal'       => $order['PSTorQSTTotal'],
+                    'OrderTotal'          => $order['OrderTotal'],
+                ]);
+
+                $count++;
+
+            } catch (Exception $e) {
+                //echo $e->getMessage(), EOL;
+            }
+
+            $this->saveOrderItem($order);
+            $this->saveShippingAddress($order);
+        }
+
+        fclose($fh);
+
+        echo EOL; // "$count orders imported\n";
+    }
+
+    private function saveOrderItem($order)
+    {
+        try {
+            $success = $this->db->insertAsDict('newegg_order_item', [
+                'OrderNumber'            => $order['OrderNumber'],
+                'ItemSellerPartNo'       => $order['ItemSellerPartNo'],
+                'ItemNeweggNo'           => $order['ItemNeweggNo'],
+                'ItemUnitPrice'          => $order['ItemUnitPrice'],
+                'ExtendUnitPrice'        => $order['ExtendUnitPrice'],
+                'ItemUnitShippingCharge' => $order['ItemUnitShippingCharge'],
+                'ExtendShippingCharge'   => $order['ExtendShippingCharge'],
+                'QuantityOrdered'        => $order['QuantityOrdered'],
+                'QuantityShipped'        => $order['QuantityShipped'],
+                'ShipDate'               => $order['ShipDate'],
+                'ActualShippingCarrier'  => $order['ActualShippingCarrier'],
+                'ActualShippingMethod'   => $order['ActualShippingMethod'],
+                'TrackingNumber'         => $order['TrackingNumber'],
+                'ShipFromAddress'        => $order['ShipFromAddress'],
+                'ShipFromCity'           => $order['ShipFromCity'],
+                'ShipFromState'          => $order['ShipFromState'],
+                'ShipFromZipcode'        => $order['ShipFromZipcode'],
+                'ShipFromName'           => $order['ShipFromName'],
+            ]);
+        } catch (Exception $e) {
+            //echo $e->getMessage(), EOL;
+        }
+    }
+
+    private function saveShippingAddress($order)
+    {
+        try {
+            $success = $this->db->insertAsDict('newegg_order_shipping_address', [
+                'OrderNumber'        => $order['OrderNumber'],
+                'ShipToAddressLine1' => $order['ShipToAddressLine1'],
+                'ShipToAddressLine2' => $order['ShipToAddressLine2'],
+                'ShipToCity'         => $order['ShipToCity'],
+                'ShipToState'        => $order['ShipToState'],
+                'ShipToZipCode'      => $order['ShipToZipCode'],
+                'ShipToCountry'      => $order['ShipToCountry'],
+                'ShipToFirstName'    => $order['ShipToFirstName'],
+                'ShipToLastName'     => $order['ShipToLastName'],
+                'ShipToCompany'      => $order['ShipToCompany'],
+                'ShipToPhoneNumber'  => $order['ShipToPhoneNumber'],
+            ]);
+        } catch (Exception $e) {
+            //echo $e->getMessage(), EOL;
+        }
+    }
+
+    private function alreadyImported($file)
+    {
+        $file = basename($file);
+        $sql = "SELECT filename FROM newegg_order_file WHERE filename='$file'";
+        $result = $this->db->fetchOne($sql);
+        return $result;
+    }
+
+    private function getColumns()
+    {
+        return [
+            'OrderNumber',
+            'OrderDateTime',
+            'SalesChannel',
+            'FulfillmentOption',
+            'ShipToAddressLine1',
+            'ShipToAddressLine2',
+            'ShipToCity',
+            'ShipToState',
+            'ShipToZipCode',
+            'ShipToCountry',
+            'ShipToFirstName',
+            'ShipToLastName',
+            'ShipToCompany',
+            'ShipToPhoneNumber',
+            'OrderCustomerEmail',
+            'OrderShippingMethod',
+            'ItemSellerPartNo',
+            'ItemNeweggNo',
+            'ItemUnitPrice',
+            'ExtendUnitPrice',
+            'ItemUnitShippingCharge',
+            'ExtendShippingCharge',
+            'OrderShippingTotal',
+            'OrderDiscountAmount',
+            'GSTorHSTTotal',
+            'PSTorQSTTotal',
+            'OrderTotal',
+            'QuantityOrdered',
+            'QuantityShipped',
+            'ShipDate',
+            'ActualShippingCarrier',
+            'ActualShippingMethod',
+            'TrackingNumber',
+            'ShipFromAddress',
+            'ShipFromCity',
+            'ShipFromState',
+            'ShipFromZipcode',
+            'ShipFromName',
+        ];
+    }
+}
