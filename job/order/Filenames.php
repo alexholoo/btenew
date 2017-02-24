4341bbe1b031ee906589621f5799e4c023402985
Hanson Li <lihsca@gmail.com>
2017-02-24 15:23:00 -0500

    Order filenames update

--- job/order/Filenames.php
+++ job/order/Filenames.php
@@ -4,21 +4,25 @@ class Filenames
 {
     protected static $filenames = [
 
-        'amazon.ca.order'          => 'E:/BTE/orders/amazon_ca_orders.csv',
-        'amazon.us.order'          => 'E:/BTE/orders/amazon_us_orders.csv',
+        'amazon.ca.order'          => 'E:/BTE/orders/amazon/amazon_ca_orders.csv',
+        'amazon.us.order'          => 'E:/BTE/orders/amazon/amazon_us_orders.csv',
 
-        'bestbuy.order'            => 'E:/BTE/orders/bestbuy_orders.csv',
+        'bestbuy.order'            => 'E:/BTE/orders/bestbuy/bestbuy_orders.csv',
 
-        'ebay.bte.order'           => 'E:/BTE/orders/ebay_orders_bte.csv',
-        'ebay.odo.order'           => 'E:/BTE/orders/ebay_orders_odo.csv',
+        'ebay.bte.order'           => 'E:/BTE/orders/ebay/ebay_orders_bte.csv',
+        'ebay.odo.order'           => 'E:/BTE/orders/ebay/ebay_orders_odo.csv',
 
-        'newegg.ca.order'          => 'E:/BTE/orders/newegg_ca_orders.csv',
-        'newegg.us.order'          => 'E:/BTE/orders/newegg_us_orders.csv',
-        'newegg.master.order'      => 'E:/BTE/orders/newegg_master_orders.csv',
+        'newegg.ca.order'          => 'E:/BTE/orders/newegg/orders_ca',  // folder
+        'newegg.us.order'          => 'E:/BTE/orders/newegg/orders_us',  // folder
 
-        'rakuten.ca.order'         => 'E:/BTE/orders/rakuten_ca_orders.txt',
-        'rakuten.us.order'         => 'E:/BTE/orders/rakuten_us_orders.txt',
-        'rakuten.master.order'     => 'E:/BTE/orders/rakuten_master_orders.txt',
+        'newegg.ca.master.order'   => 'E:/BTE/orders/newegg/newegg_ca_master_orders.csv',
+        'newegg.us.master.order'   => 'E:/BTE/orders/newegg/newegg_us_master_orders.csv',
+
+        'rakuten.ca.order'         => 'E:/BTE/orders/rakuten/orders_ca', // folder
+        'rakuten.us.order'         => 'E:/BTE/orders/rakuten/orders_us', // folder
+
+        'rakuten.ca.master.order'  => 'E:/BTE/orders/rakuten/rakuten_ca_master_orders.csv',
+        'rakuten.us.master.order'  => 'E:/BTE/orders/rakuten/rakuten_us_master_orders.csv',
     ];
 
     public static function get($key)
