5069c3f74ea3ec6071f31ee577565433bc87d7ff
Hanson Li <lihsca@gmail.com>
2017-02-24 22:30:25 -0500

    add methods to OrderImporter

--- job/order/import/Base.php
+++ job/order/import/Base.php
@@ -3,4 +3,103 @@
 class OrderImporter extends Job
 {
     public function import() { }
+
+    protected function insertMasterOrders($masterOrders)
+    {
+        foreach ($masterOrders as $orderId => $orders) {
+            if (!$this->orderExists($orderId)) {
+                $this->log("Importing order $orderId");
+                $this->insertOrder($orders[0]);
+                $this->insertOrderShippingAddress($orders[0]);
+                $this->insertOrderItems($orders);
+            }
+        }
+    }
+
+    protected function insertOrder($order)
+    {
+        try {
+            $this->db->insertAsDict('master_order', [
+                'channel'   => $order['channel'],
+                'date'      => $order['date'],
+                'order_id'  => $order['orderId'],
+                'express'   => $order['express'],
+                'shipping'  => $order['shipping'],
+            ]);
+        } catch (\Exception $e) {
+            //echo $e->getMessage(), EOL;
+        }
+    }
+
+    protected function insertOrderItems($orders)
+    {
+        foreach ($orders as $order) {
+            try {
+                $this->db->insertAsDict('master_order_item', [
+                    'order_id'      => $order['orderId'],
+                    'sku'           => $order['sku'],
+                    'price'         => $order['price'],
+                    'qty'           => $order['qty'],
+                    'product_name'  => $order['productName'],
+                ]);
+
+                $order['order_item_id'] = $this->db->lastInsertId();
+                $this->insertOrderStatus($order);
+            } catch (\Exception $e) {
+                //echo $e->getMessage(), EOL;
+            }
+        }
+    }
+
+    protected function insertOrderShippingAddress($order)
+    {
+        try {
+            $this->db->insertAsDict('master_order_shipping_address', [
+                'date'       => $order['date'],
+                'order_id'   => $order['orderId'],
+                'buyer'      => $order['buyer'],
+                'address'    => $order['address'],
+                'city'       => $order['city'],
+                'province'   => $order['province'],
+                'postalcode' => $order['postalcode'],
+                'country'    => $order['country'],
+                'phone'      => $order['phone'],
+                'email'      => $order['email'],
+            ]);
+        } catch (\Exception $e) {
+            //echo $e->getMessage(), EOL;
+        }
+    }
+
+    protected function insertOrderStatus($order)
+    {
+        try {
+            $this->db->insertAsDict('master_order_status', [
+                'order_item_id' => $order['orderItemId'],
+                'date'          => $order['date'],
+                'channel'       => $order['channel'],
+                'order_id'      => $order['orderId'],
+                'stock_status'  => '',
+                'supplier'      => '',
+                'supplier_sku'  => '',
+                'mfrpn'         => '',
+                'ponum'         => '',
+                'invoice'       => '',
+                'ship_method'   => '',
+                'trackingnum'   => '',
+                'remarks'       => '',
+                'flag'          => '',
+                'related_sku'   => '',
+                'dimension'     => '',
+            ]);
+        } catch (\Exception $e) {
+            //echo $e->getMessage(), EOL;
+        }
+    }
+
+    protected function orderExists($orderId)
+    {
+        $sql = "SELECT order_id FROM master_order WHERE order_id='$orderId'";
+        return $this->db->fetchOne($sql);
+    }
 }
