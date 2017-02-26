0da8c95f673137563e1e1db32228d747899d112a
Hongshuan Li <lihsca@gmail.com>
2017-02-25 19:48:46 -0500

    copy order-related scripts to job/retired/

---
+++ job/retired/Amazon_Order.php
@@ -0,0 +1,179 @@
+<?php
+
+class Amazon_Order extends OrderImporter
+{
+    public function import()
+    {
+        // Amazon CA
+        $client = new Marketplace\Amazon\Client('bte-amazon-ca');
+        $channel = 'Amazon-ACA';
+
+        $orders = $client->getOrderList();
+        foreach ($orders as $order) {
+            $this->saveOrder($order, $channel);
+        }
+
+        // Amazon US
+        $client = new Marketplace\Amazon\Client('bte-amazon-us');
+        $channel = 'Amazon-US';
+
+        $orders = $client->getOrderList();
+        foreach ($orders as $order) {
+            $this->saveOrder($order, $channel);
+        }
+    }
+
+    public function saveOrder($order, $channel)
+    {
+        $data = $order->getData();
+
+        if ($this->orderExists($data['AmazonOrderId'])) {
+            return;
+        }
+
+        if ($data['OrderStatus'] == 'Canceled') {
+            // TODO: delete order if exists?
+            return;
+        }
+
+        if (!isset($data['OrderTotal'])) {
+            $data['OrderTotal']['Amount'] = '0.00';
+            $data['OrderTotal']['CurrencyCode'] = 'CAD';
+        }
+        if (!isset($data['PaymentMethod'])) {
+            $data['PaymentMethod'] = '';
+        }
+        if (!isset($data['BuyerName'])) {
+            $data['BuyerName'] = '';
+        }
+        if (!isset($data['BuyerEmail'])) {
+            $data['BuyerEmail'] = '';
+        }
+        if (!isset($data['ShippedByAmazonTFM'])) {
+            $data['ShippedByAmazonTFM'] = 'false';
+        }
+        if (!isset($data['EarliestDeliveryDate'])) {
+            $data['EarliestDeliveryDate'] = null;
+        }
+        if (!isset($data['LatestDeliveryDate'])) {
+            $data['LatestDeliveryDate'] = null;
+        }
+
+        try {
+            $this->db->insertAsDict('amazon_order', [
+                'Channel'                      => $channel,
+                'OrderId'                      => $data['AmazonOrderId'],
+                'PurchaseDate'                 => $this->dtime($data['PurchaseDate']),
+                'LastUpdateDate'               => $this->dtime($data['LastUpdateDate']),
+                'OrderStatus'                  => $data['OrderStatus'],
+                'FulfillmentChannel'           => $data['FulfillmentChannel'],
+                'SalesChannel'                 => $data['SalesChannel'],
+                'ShipServiceLevel'             => $data['ShipServiceLevel'],
+                'CurrencyCode'                 => $data['OrderTotal']['CurrencyCode'],
+                'OrderTotalAmount'             => $data['OrderTotal']['Amount'],
+                'NumberOfItemsShipped'         => $data['NumberOfItemsShipped'],
+                'NumberOfItemsUnshipped'       => $data['NumberOfItemsUnshipped'],
+                'PaymentMethod'                => $data['PaymentMethod'],
+                'BuyerName'                    => $data['BuyerName'],
+                'BuyerEmail'                   => $data['BuyerEmail'],
+                'ShipmentServiceLevelCategory' => $data['ShipmentServiceLevelCategory'],
+                'ShippedByAmazonTFM'           => $this->yesNo($data['ShippedByAmazonTFM']),
+                'OrderType'                    => $data['OrderType'],
+                'EarliestShipDate'             => $this->dtime($data['EarliestShipDate']),
+                'LatestShipDate'               => $this->dtime($data['LatestShipDate']),
+                'EarliestDeliveryDate'         => $this->dtime($data['EarliestDeliveryDate']),
+                'LatestDeliveryDate'           => $this->dtime($data['LatestDeliveryDate']),
+                'IsBusinessOrder'              => $this->yesNo($data['IsBusinessOrder']),
+                'IsPrime'                      => $this->yesNo($data['IsPrime']),
+                'IsPremiumOrder'               => $this->yesNo($data['IsPremiumOrder']),
+            ]);
+
+            echo $data['AmazonOrderId'], ' ', $channel, EOL;
+
+            $this->saveOrderItem($order);
+            $this->saveShippingAddress($order);
+
+        } catch (\Exception $e) {
+            //echo $e->getMessage(), EOL;
+        }
+    }
+
+    private function saveOrderItem($order)
+    {
+        $items = $order->fetchItems();
+
+        foreach ($items->getItems() as $item) {
+            if (!isset($item['ConditionNote'])) {
+                $item['ConditionNote'] = '';
+            }
+
+            try {
+                $this->db->insertAsDict('amazon_order_item', [
+                    'OrderId'            => $order->getAmazonOrderId(),
+                    'ASIN'               => $item['ASIN'],
+                    'SellerSKU'          => $item['SellerSKU'],
+                    'OrderItemId'        => $item['OrderItemId'],
+                    'Title'              => $item['Title'],
+                    'QuantityOrdered'    => $item['QuantityOrdered'],
+                    'QuantityShipped'    => $item['QuantityShipped'],
+                    'CurrencyCode'       => $item['ItemPrice']['CurrencyCode'],
+                    'ItemPrice'          => $item['ItemPrice']['Amount'],
+                    'ShippingPrice'      => $item['ShippingPrice']['Amount'],
+                    'GiftWrapPrice'      => $item['GiftWrapPrice']['Amount'],
+                    'ItemTax'            => $item['ItemTax']['Amount'],
+                    'ShippingTax'        => $item['ShippingTax']['Amount'],
+                    'GiftWrapTax'        => $item['GiftWrapTax']['Amount'],
+                    'ShippingDiscount'   => $item['ShippingDiscount']['Amount'],
+                    'PromotionDiscount'  => $item['PromotionDiscount']['Amount'],
+                    'ConditionId'        => $item['ConditionId'],
+                    'ConditionSubtypeId' => $item['ConditionSubtypeId'],
+                    'ConditionNote'      => $item['ConditionNote'],
+                ]);
+            } catch (\Exception $e) {
+                //echo $e->getMessage(), EOL;
+            }
+        }
+    }
+
+    private function saveShippingAddress($order)
+    {
+        $address = $order->getShippingAddress();
+
+        try {
+            $this->db->insertAsDict('amazon_order_shipping_address', [
+                'OrderId'       => $order->getAmazonOrderId(),
+                'Name'          => $address['Name'],
+                'AddressLine1'  => $address['AddressLine1'],
+                'AddressLine2'  => $address['AddressLine2'],
+                'AddressLine3'  => $address['AddressLine3'],
+                'City'          => $address['City'],
+                'County'        => $address['County'],
+                'District'      => $address['District'],
+                'StateOrRegion' => $address['StateOrRegion'],
+                'PostalCode'    => $address['PostalCode'],
+                'CountryCode'   => $address['CountryCode'],
+                'Phone'         => $address['Phone'],
+            ]);
+        } catch (\Exception $e) {
+            //echo $e->getMessage(), EOL;
+        }
+    }
+
+    private function orderExists($orderId)
+    {
+        $sql = "SELECT OrderId FROM amazon_order WHERE OrderId='$orderId'";
+        return $this->db->fetchOne($sql);
+    }
+
+    private function yesNo($value)
+    {
+        if ($value == 'false') return 'N';
+        return 'Y';
+    }
+
+    private function dtime($value)
+    {
+        # "2016-09-22T06:59:59Z" => "2016-09-22 06:59:59"
+        return str_replace(['T', 'Z'], [' ', ''], $value);
+    }
+}
---
+++ job/retired/Ebay_Order.php
@@ -0,0 +1,147 @@
+<?php
+
+class Ebay_Order extends OrderImporter
+{
+    public function import()
+    {
+        // BTE - CAD
+        $client = new Marketplace\eBay\Client('bte');
+        $table = 'ebay_order_report_bte';
+
+        $res = $client->getOrders();
+        //print_r($res);
+        if ($res->Ack == 'Success') {
+            $orders = $res->OrderArray->Order;
+            foreach ($orders as $order) {
+                echo $order->OrderID, PHP_EOL;
+                $this->saveOrder($order, $table);
+            }
+        }
+
+        // ODO - USD
+        $client = new Marketplace\eBay\Client('odo');
+        $table = 'ebay_order_report_odo';
+
+        $res = $client->getOrders();
+        //print_r($res);
+        if ($res->Ack == 'Success') {
+            $orders = $res->OrderArray->Order;
+            foreach ($orders as $order) {
+                echo $order->OrderID, PHP_EOL;
+                $this->saveOrder($order, $table);
+            }
+        }
+    }
+
+    public function saveOrder($order, $table)
+    {
+        if ($order->OrderStatus == "Cancelled") {
+            return;
+        }
+
+        $OrderID             = (string)$order->OrderID;
+       #$ExtOrderID          = (string)$order->ExtendedOrderID;
+        $Status              = (string)$order->OrderStatus;
+        $BuyerUsername       = (string)$order->BuyerUserID;
+        $DatePaid            = substr($order->PaidTime, 0, 10);
+        $Currency            = (string)$order->AmountPaid['currencyID'];
+        $AmountPaid          = (string)$order->AmountPaid;
+        $SalesTaxAmount      = (string)$order->ShippingDetails->SalesTax->SalesTaxAmount;
+        $ShippingService     = (string)$order->ShippingServiceSelected->ShippingService;
+        $ShippingServiceCost = (string)$order->ShippingServiceSelected->ShippingCost;
+
+        try {
+            $success = $this->db->insertAsDict($table,
+                array(
+                    'OrderID'             => $OrderID,
+                    'Status'              => $Status,
+                    'BuyerUsername'       => $BuyerUsername,
+                    'DatePaid'            => $DatePaid,
+                    'Currency'            => $Currency,
+                    'AmountPaid'          => $AmountPaid,
+                    'SalesTaxAmount'      => $SalesTaxAmount,
+                    'ShippingService'     => $ShippingService,
+                    'ShippingServiceCost' => $ShippingServiceCost,
+                )
+            );
+
+            $this->saveOrderItem($order);
+            $this->saveShippingAddress($order);
+
+        } catch (Exception $e) {
+            // echo $e->getMessage(), EOL;
+        }
+    }
+
+    private function saveOrderItem($order)
+    {
+        $OrderID = (string)$order->OrderID;
+
+        $transactions = $order->TransactionArray;
+
+        foreach ($transactions->Transaction as $transaction) {
+            $SKU               = (string)$transaction->Item->SKU;
+            $QuantityPurchased = (string)$transaction->QuantityPurchased;
+            $TransactionID     = (string)$transaction->TransactionID;
+            $TransactionPrice  = (string)$transaction->TransactionPrice;
+            $Tracking          = (string)$transaction->ShippingDetails->ShipmentTrackingDetails->ShipmentTrackingNumber;
+            $ItemID            = (string)$transaction->Item->ItemID;
+            $Email             = (string)$transaction->Buyer->Email;
+            $RecordNumber      = (string)$transaction->ShippingDetails->SellingManagerSalesRecordNumber;
+
+            if (!$Tracking) {
+                $Tracking = 'NA';
+            }
+
+            try {
+                $success = $this->db->insertAsDict('ebay_order_item',
+                    array(
+                        'OrderID'           => $OrderID,
+                        'SKU'               => $SKU,
+                        'QuantityPurchased' => $QuantityPurchased,
+                        'TransactionID'     => $TransactionID,
+                        'TransactionPrice'  => $TransactionPrice,
+                        'Tracking'          => $Tracking,
+                        'ItemID'            => $ItemID,
+                        'Email'             => $Email,
+                        'RecordNumber'      => $RecordNumber,
+                    )
+                );
+            } catch (Exception $e) {
+                // echo $e->getMessage(), EOL;
+            }
+        }
+    }
+
+    private function saveShippingAddress($order)
+    {
+        $OrderID         = (string)$order->OrderID;
+        $shippingAddress = $order->ShippingAddress;
+        $Name            = (string)$shippingAddress->Name;
+        $Address         = (string)$shippingAddress->Street1;
+        $Address2        = (string)$shippingAddress->Street2;
+        $City            = (string)$shippingAddress->CityName;
+        $Province        = (string)$shippingAddress->StateOrProvince;
+        $PostalCode      = (string)$shippingAddress->PostalCode;
+        $Country         = (string)$shippingAddress->Country;
+        $Phone           = (string)$shippingAddress->Phone;
+
+        try {
+            $success = $this->db->insertAsDict('ebay_order_shipping_address',
+                array(
+                    'OrderID'    => $OrderID,
+                    'Name'       => $Name,
+                    'Address'    => $Address,
+                    'Address2'   => $Address2,
+                    'City'       => $City,
+                    'Province'   => $Province,
+                    'PostalCode' => $PostalCode,
+                    'Country'    => $Country,
+                    'Phone'      => $Phone,
+                )
+            );
+        } catch (Exception $e) {
+            // echo $e->getMessage(), EOL;
+        }
+    }
+}
