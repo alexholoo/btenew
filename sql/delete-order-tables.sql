5b7d79831196dc267c7cc092b67be8b4f0667ebb
Hanson Li <lihsca@gmail.com>
2017-02-25 19:32:42 -0500

    new file: delete-order-tables.sql

---
+++ sql/delete-order-tables.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS `amazon_order`;
+DROP TABLE IF EXISTS `amazon_order_item`;
+DROP TABLE IF EXISTS `amazon_order_shipping_address`;
+
+DROP TABLE IF EXISTS `amazon_ca_order_report`;
+DROP TABLE IF EXISTS `amazon_us_order_report`;
+
+DROP TABLE IF EXISTS `ebay_order_item`;
+DROP TABLE IF EXISTS `ebay_order_report_bte`;
+DROP TABLE IF EXISTS `ebay_order_report_odo`;
+DROP TABLE IF EXISTS `ebay_order_shipping_address`;
+
+DROP TABLE IF EXISTS `newegg_order_file`;
+DROP TABLE IF EXISTS `newegg_order_item`;
+DROP TABLE IF EXISTS `newegg_order_report`;
+DROP TABLE IF EXISTS `newegg_order_shipping_address`;
+
+DROP TABLE IF EXISTS `rakuten_order_file`;
+DROP TABLE IF EXISTS `rakuten_order_item`;
+DROP TABLE IF EXISTS `rakuten_order_report`;
+DROP TABLE IF EXISTS `rakuten_order_shipping_address`;
