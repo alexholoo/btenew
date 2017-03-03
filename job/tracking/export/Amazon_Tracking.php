e61b8e9fa710cd9cd012c06ece046d645bd0bfea
Hanson Li <lihsca@gmail.com>
2017-03-03 13:10:27 -0500

    copy amazon tracking code to tracking jobs, disabled for now

--- job/tracking/export/Amazon_Tracking.php
+++ job/tracking/export/Amazon_Tracking.php
@@ -7,5 +7,99 @@ class Amazon_Tracking extends TrackingExporter
         // Nothing to do, it's done in AmazonShippingUploadJob
         //
         // It's might be better to move AmazonShippingUploadJob here
+        return;
+
+        $store    = 'bte-amazon-ca';
+        $orders   = $this->getUnshippedOrders($store);
+        $filename = Filenames::get('amazon.ca.shipping');
+        $this->outputFeed($filename, $orders);
+
+        $store    = 'bte-amazon-us';
+        $orders   = $this->getUnshippedOrders($store);
+        $filename = Filenames::get('amazon.us.shipping');
+        $this->outputFeed($filename, $orders);
+    }
+
+    private function getUnshippedOrders($store)
+    {
+        $orders = [];
+
+        try {
+            $api = new AmazonOrderList($store);
+
+            $api->setFulfillmentChannelFilter("MFN");
+            $api->setLimits('Modified', "-7 days");
+            $api->setOrderStatusFilter(["Unshipped", "PartiallyShipped", "Canceled", "Unfulfillable"]);
+            $api->setUseToken();
+            $api->fetchOrders();
+
+            if (0) {
+                // save the response to log file
+                $logfile = str_replace('job', 'Amazon-List-Orders', $this->getLogFilename());
+                foreach ($api->getRawResponses() as $response) {
+                    file_put_contents($logfile, $response['body'], FILE_APPEND);
+                }
+            }
+
+            // return only unshipped orders
+            $list = $api->getList();
+
+            foreach ($list as $order) {
+                if ($order->getOrderStatus() == 'Unshipped') {
+                    $orders[] = $order;
+                }
+            }
+        } catch (Exception $ex) {
+            echo 'There was a problem with the Amazon library. Error: '.$ex->getMessage();
+        }
+
+        return $orders;
+    }
+
+    private function outputFeed($filename, $orders)
+    {
+        $feedFile = new AmazonShipmentFile($filename);
+
+        $shipmentService = $this->di->get('shipmentService');
+
+        foreach ($orders as $order) {
+            $orderId = $order->getAmazonOrderId();
+
+            $tracking = $shipmentService->getOrderTracking($orderId);
+
+            if ($tracking) {
+                // change unrecognized carriers to 'Other'
+                $carrierCode = $tracking['carrierCode'];
+                $carrierName = $tracking['carrierName'];
+
+                // Amazon Report the Error: The carrier-code field contains an invalid value: Purolator.
+                if ($carrierCode == 'Purolator') {
+                    $carrierCode = 'Other';
+                    $carrierName = 'Purolator';
+                }
+
+                // Amazon Report the Error: The carrier-code field contains an invalid value: Loomis.
+                if ($carrierCode == 'Loomis') {
+                    $carrierCode = 'Other';
+                    $carrierName = 'Loomis';
+                }
+
+                if ($carrierCode == 'TNT') {
+                    $carrierCode = 'Other';
+                    $carrierName = 'TNT';
+                }
+
+                $feedFile->write([
+                    $orderId,                     //'order-id'
+                    '',                           //'order-item-id'
+                    '',                           //'quantity'
+                    $tracking['shipDate'],        //'ship-date'
+                    $carrierCode,                 //'carrier-code'
+                    $carrierName,                 //'carrier-name'
+                    $tracking['trackingNumber'],  //'tracking-number'
+                    $tracking['shipMethod'],      //'ship-method'
+                ]);
+            }
+        }
     }
 }
--- job/tracking/upload/Amazon_Shipment.php
+++ job/tracking/upload/Amazon_Shipment.php
@@ -7,5 +7,59 @@ class Amazon_Shipment extends TrackingUploader
         // Nothing to do, it's done in AmazonShippingUploadJob
         //
         // It's might be better to move AmazonShippingUploadJob here
+        return;
+
+        $store    = 'bte-amazon-ca';
+        $orders   = $this->getUnshippedOrders($store);
+        $filename = Filenames::get('amazon.ca.shipping');
+        $this->uploadFeed($store, $filename);
+
+        $store    = 'bte-amazon-us';
+        $orders   = $this->getUnshippedOrders($store);
+        $filename = Filenames::get('amazon.us.shipping');
+        $this->uploadFeed($store, $filename);
+    }
+
+    private function uploadFeed($store, $file)
+    {
+        if (!IS_PROD) {
+            throw new Exception('This script can only run on production server.');
+        }
+
+        if (!file_exists($file)) {
+            //$this->error(__METHOD__." File not found: $file");
+            return;
+        }
+
+        $this->log("Uploading shipping: $file");
+
+        $feed = file_get_contents($file);
+
+        $api = new AmazonFeed($store);
+        $api->setFeedType('_POST_FLAT_FILE_FULFILLMENT_DATA_');
+        $api->setFeedContent($feed);
+        $api->submitFeed();
+
+        $this->markOrdersShipped($file);
+
+        File::backup($file);
+
+       #$this->log(print_r($api->getResponse(), true));
+    }
+
+    protected function markOrdersShipped($filename)
+    {
+        $fp = fopen($filename, 'r');
+
+        $columns = fgetcsv($fp, 0, "\t"); // skip first line
+
+        $shipmentService = $this->di->get('shipmentService');
+
+        while (($fields = fgetcsv($fp, 0, "\t"))) {
+            $orderId = $fields[0];
+            $shipmentService->markOrderAsShipped($orderId);
+        }
+
+        fclose($fp);
     }
 }
