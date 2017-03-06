e138e693af4d30d9f7442dc6b2f54ee8bef814b5
Hanson Li <lihsca@gmail.com>
2017-03-06 12:57:25 -0500

    Download bestbuy listing

--- job/listing/download/Bestbuy_Listing.php
+++ job/listing/download/Bestbuy_Listing.php
@@ -4,5 +4,62 @@ class Bestbuy_Listing extends ListingDownloader
 {
     public function download()
     {
+        $client = new Marketplace\Bestbuy\Client();
+
+        $offset = 0;
+        $max = 100;
+
+        $filename = Filenames::get('bestbuy.listing');
+
+        $fp = fopen($filename, 'w');
+
+        fputcsv($fp, [
+            'offer_id',
+            'active',
+            'shop_sku',
+            'upc',
+            'product_sku',
+            'price',
+            'quantity',
+            'state_code',
+            'category_code',
+            'category_label',
+            'product_title'
+        ]);
+
+        do {
+            $res = $client->listOffers($offset, $max);
+
+            foreach ($res->offers as $offer) {
+                $upc = '';
+                if (isset($offer->product_references[0])) {
+                    $upc = $offer->product_references[0]->reference;
+                }
+
+                $active = $offer->active;
+                if (!$active) {
+                    $active = 0;
+                }
+
+                fputcsv($fp, [
+                    $offer->offer_id,
+                    $active,
+                    $offer->shop_sku,
+                    $upc,
+                    $offer->product_sku,
+                    $offer->price,
+                    $offer->quantity,
+                    $offer->state_code,
+                    $offer->category_code,
+                    $offer->category_label,
+                    $offer->product_title,
+                ]);
+            }
+
+            $offset += $max;
+
+        } while ($offset < $res->total_count);
+
+        fclose($fp);
     }
 }
