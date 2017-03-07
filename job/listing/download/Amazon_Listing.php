324a5a55b7cc1842b281d3a3cfecb7dc4d22498c
Hanson Li <lihsca@gmail.com>
2017-03-07 11:27:23 -0500

    Amazon Listing download & import

--- job/listing/download/Amazon_Listing.php
+++ job/listing/download/Amazon_Listing.php
@@ -4,5 +4,12 @@ class Amazon_Listing extends ListingDownloader
 {
     public function download()
     {
+        $source = 'E:/BTE/amazon/reports/amazon_ca_listings.txt';
+        $filename = Filenames::get('amazon.ca.listing');
+        copy($source, $filename);
+
+        $source = 'E:/BTE/amazon/reports/amazon_us_listings.txt';
+        $filename = Filenames::get('amazon.us.listing');
+        copy($source, $filename);
     }
 }
--- job/listing/import/Amazon_Listing.php
+++ job/listing/import/Amazon_Listing.php
@@ -4,5 +4,63 @@ class Amazon_Listing extends ListingImporter
 {
     public function import()
     {
+        $table = 'amazon_ca_listings';
+        $file = Filenames::get('amazon.ca.listing');
+       #$file = 'w:/data/csv/amazon/amazon_ca_listings.txt';
+        $this->importAmazonListings($file, $table);
+
+        $table = 'amazon_us_listings';
+        $file = Filenames::get('amazon.us.listing');
+       #$file = 'w:/data/csv/amazon/amazon_us_listings.txt';
+        $this->importAmazonListings($file, $table);
+    }
+
+    private function importAmazonListings($file, $table)
+    {
+        $listings = $this->getListings($file);
+
+        if (!$listings) {
+            return;
+        }
+
+        $this->db->execute("TRUNCATE TABLE $table");
+
+        $columns = [ 'name', 'sku', 'price', 'asin' ];
+
+        $chunks = array_chunk($listings, 2000);
+        foreach ($chunks as $chunk) {
+            try {
+                $sql = $this->genInsertSql($table, $columns, $chunk);
+                $this->db->execute($sql);
+            } catch (\Exception $e) {
+                $this->error($e->getMessage() .' in '. __METHOD__);
+            }
+        }
+    }
+
+    private function getListings($file)
+    {
+        $listings = [];
+
+        if (!($fh = @fopen($file, 'rb'))) {
+            //$this->log("Failed to open file: $fname");
+            return false;
+        }
+
+        $title = fgetcsv($fh, 0, "\t");
+
+        while (($data = fgetcsv($fh, 0, "\t"))) {
+            $fields = array_combine($title, $data);
+            $listings[] = [
+                'name'  => $fields['item-name'],
+                'sku'   => $fields['seller-sku'],
+                'price' => $fields['price'],
+                'asin'  => $fields['asin1'],
+            ];
+        }
+
+        fclose($fh);
+
+        return $listings;
     }
 }
