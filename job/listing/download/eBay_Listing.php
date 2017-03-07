e7fbd16560f839ddb06098f56a329559c40ff8c1
Hanson Li <lihsca@gmail.com>
2017-03-07 16:30:11 -0500

    Download ebay listing

--- job/listing/download/eBay_Listing.php
+++ job/listing/download/eBay_Listing.php
@@ -4,5 +4,62 @@ class eBay_Listing extends ListingDownloader
 {
     public function download()
     {
+        // BTE
+        $client = new Marketplace\eBay\Client('bte');
+        $filename = Filenames::get('ebay.bte.listing');
+        $this->downloadListing($client, $filename);
+
+        // ODO
+        $client = new Marketplace\eBay\Client('odo');
+        $filename = Filenames::get('ebay.odo.listing');
+        $this->downloadListing($client, $filename);
+    }
+
+    private function downloadListing($client, $filename)
+    {
+        $fp = fopen($filename, 'w');
+
+        fputcsv($fp, $this->getCsvHeader());
+
+        $page = 1;
+        $totalPages = 1;
+
+        do {
+            $res = $client->getMyeBaySelling($page++);
+            if ($res->Ack == 'Success') {
+                $totalPages = $res->ActiveList->PaginationResult->TotalNumberOfPages;
+                $this->saveListing($fp, $res);
+            }
+        } while ($page <= $totalPages);
+
+        fclose($fp);
+    }
+
+    private function saveListing($fp, $res)
+    {
+        $items = $res->ActiveList->ItemArray->Item;
+
+        if ($items != null) {
+            foreach ($items as $item) {
+                fputcsv($fp, [
+                    $item->SKU,
+                    $item->ItemID,
+                    $item->Title,
+                    $item->SellingStatus->CurrentPrice,
+                    $item->QuantityAvailable,
+                ]);
+            }
+        }
+    }
+
+    private function getCsvHeader()
+    {
+        return [
+            "SKU",
+            "Item ID",
+            "Title",
+            "Fixed Price",
+            "Quantity",
+        ];
     }
 }
