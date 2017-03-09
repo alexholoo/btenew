ea871394654ed6f9743e57f001a45f06281d83b0
Hanson Li <lihsca@gmail.com>
2017-03-09 14:19:21 -0500

    Import ebay order

--- job/order/import/Ebay_Order.php
+++ job/order/import/Ebay_Order.php
@@ -4,74 +4,29 @@ class Ebay_Order extends OrderImporter
 {
     public function import()
     {
-        return; // NOT DONE YET
-
         // BTE - CAD
-        $client = new Marketplace\eBay\Client('bte');
-
-        $res = $client->getOrders();
-        //print_r($res);
-        if ($res->Ack == 'Success') {
-            $orders = $res->OrderArray->Order;
-            foreach ($orders as $order) {
-                echo $order->OrderID, PHP_EOL;
-                $this->saveOrder($order);
-            }
-        }
+        $filename = Filenames::get('ebay.bte.order');
+        $orders = $this->getOrders($filename);
+        $this->importMasterOrders($orders);
 
         // ODO - USD
-        $client = new Marketplace\eBay\Client('odo');
-
-        $res = $client->getOrders();
-        //print_r($res);
-        if ($res->Ack == 'Success') {
-            $orders = $res->OrderArray->Order;
-            foreach ($orders as $order) {
-                echo $order->OrderID, PHP_EOL;
-                $this->saveOrder($order);
-            }
-        }
+        $filename = Filenames::get('ebay.odo.order');
+        $orders = $this->getOrders($filename);
+        $this->importMasterOrders($orders);
     }
 
-    public function saveOrder($order)
+    public function getOrders($filename)
     {
-        if ($order->OrderStatus == "Cancelled") { return; }
+        $orderFile = new OrderReportFile($filename);
 
-        $OrderID             = (string)$order->OrderID;
-        $ExtOrderID          = (string)$order->ExtendedOrderID;
-        $Status              = (string)$order->OrderStatus;
-        $BuyerUsername       = (string)$order->BuyerUserID;
-        $DatePaid            = substr($order->PaidTime, 0, 10);
-        $Currency            = (string)$order->AmountPaid['currencyID'];
-        $AmountPaid          = (string)$order->AmountPaid;
-        $SalesTaxAmount      = (string)$order->ShippingDetails->SalesTax->SalesTaxAmount;
-        $ShippingService     = (string)$order->ShippingServiceSelected->ShippingService;
-        $ShippingServiceCost = (string)$order->ShippingServiceSelected->ShippingCost;
+        $orders = [];
 
-        $transactions = $order->TransactionArray;
-
-        foreach ($transactions->Transaction as $transaction) {
-            $SKU               = (string)$transaction->Item->SKU;
-            $QuantityPurchased = (string)$transaction->QuantityPurchased;
-            $TransactionID     = (string)$transaction->TransactionID;
-            $TransactionPrice  = (string)$transaction->TransactionPrice;
-            $Tracking          = (string)$transaction->ShippingDetails->ShipmentTrackingDetails->ShipmentTrackingNumber;
-            $ItemID            = (string)$transaction->Item->ItemID;
-            $Email             = (string)$transaction->Buyer->Email;
-            $RecordNumber      = (string)$transaction->ShippingDetails->SellingManagerSalesRecordNumber;
-
-            if (!$Tracking) { $Tracking = 'NA'; }
+        while ($order = $orderFile->read()) {
+            $orderId = $order['OrderID'];
+            $orders[$orderId][] = $this->toStdOrder($order);
         }
 
-        $shippingAddress = $order->ShippingAddress;
-        $Name            = (string)$shippingAddress->Name;
-        $Address         = (string)$shippingAddress->Street1;
-        $Address2        = (string)$shippingAddress->Street2;
-        $City            = (string)$shippingAddress->CityName;
-        $Province        = (string)$shippingAddress->StateOrProvince;
-        $PostalCode      = (string)$shippingAddress->PostalCode;
-        $Country         = (string)$shippingAddress->Country;
-        $Phone           = (string)$shippingAddress->Phone;
+        return $orders;
     }
 
     private function toStdOrder($order)
@@ -79,29 +34,30 @@ class Ebay_Order extends OrderImporter
         $express = $this->isExpress($order);
 
         return [
-             'orderId'      => $order[''],
-             'date'         => $order[''],
-             'orderItemId'  => $order[''],
+             'orderId'      => $order['OrderID'],
+             'date'         => $order['DatePaid'],
+             'orderItemId'  => $order['ItemID'],
              'channel'      => 'eBay',
              'express'      => $express,
-             'buyer'        => $order[''],
-             'address'      => $order[''],
-             'city'         => $order[''],
-             'province'     => $order[''],
-             'country'      => $order[''],
-             'postalcode'   => $order[''],
-             'email'        => $order[''],
-             'phone'        => $order[''],
-             'sku'          => $order[''],
-             'qty'          => $order[''],
-             'price'        => $order[''],
-             'shipping'     => $order[''],
-             'productName'  => $order[''],
+             'buyer'        => $order['Name'],
+             'address'      => $order['Address'].' '.$order['Address2'],
+             'city'         => $order['City'],
+             'province'     => $order['Province'],
+             'country'      => $order['Country'],
+             'postalcode'   => $order['PostalCode'],
+             'email'        => $order['Email'],
+             'phone'        => $order['Phone'],
+             'sku'          => $order['SKU'],
+             'qty'          => $order['QuantityPurchased'],
+             'price'        => $order['TransactionPrice'],
+             'shipping'     => $order['ShippingServiceCost'],
+             'productName'  => '',
         ];
     }
 
     private function isExpress($order)
     {
+        # 'ShippingService',
         return 0;
     }
 }
