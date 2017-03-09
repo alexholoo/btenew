e6e0a570ccb7d7d43085ec5c95084f5140af1f5f
Hanson Li <lihsca@gmail.com>
2017-03-09 13:57:06 -0500

    Download ebay order

--- job/order/download/Ebay_Order.php
+++ job/order/download/Ebay_Order.php
@@ -1,14 +1,15 @@
 <?php
 
+use Marketplace\eBay\OrderReportFile;
+
 class Ebay_Order extends OrderDownloader
 {
     public function download()
     {
-        return;
-
         // BTE - CAD
         $client = new Marketplace\eBay\Client('bte');
-        $table = 'ebay_order_report_bte';
+        $filename = Filenames::get('ebay.bte.order');
+        $orderFile = new OrderReportFile($filename);
 
         $res = $client->getOrders();
         //print_r($res);
@@ -16,13 +17,14 @@ class Ebay_Order extends OrderDownloader
             $orders = $res->OrderArray->Order;
             foreach ($orders as $order) {
                 echo $order->OrderID, PHP_EOL;
-                $this->saveOrder($order, $table);
+                $this->saveOrder($order, $orderFile);
             }
         }
 
         // ODO - USD
         $client = new Marketplace\eBay\Client('odo');
-        $table = 'ebay_order_report_odo';
+        $filename = Filenames::get('ebay.odo.order');
+        $orderFile = new OrderReportFile($filename);
 
         $res = $client->getOrders();
         //print_r($res);
@@ -30,17 +32,18 @@ class Ebay_Order extends OrderDownloader
             $orders = $res->OrderArray->Order;
             foreach ($orders as $order) {
                 echo $order->OrderID, PHP_EOL;
-                $this->saveOrder($order, $table);
+                $this->saveOrder($order, $orderFile);
             }
         }
     }
 
-    public function saveOrder($order, $table)
+    public function saveOrder($order, $orderFile)
     {
         if ($order->OrderStatus == "Cancelled") {
             return;
         }
 
+        // Order General
         $OrderID             = (string)$order->OrderID;
        #$ExtOrderID          = (string)$order->ExtendedOrderID;
         $Status              = (string)$order->OrderStatus;
@@ -52,33 +55,18 @@ class Ebay_Order extends OrderDownloader
         $ShippingService     = (string)$order->ShippingServiceSelected->ShippingService;
         $ShippingServiceCost = (string)$order->ShippingServiceSelected->ShippingCost;
 
-        try {
-            $success = $this->db->insertAsDict($table,
-                array(
-                    'OrderID'             => $OrderID,
-                    'Status'              => $Status,
-                    'BuyerUsername'       => $BuyerUsername,
-                    'DatePaid'            => $DatePaid,
-                    'Currency'            => $Currency,
-                    'AmountPaid'          => $AmountPaid,
-                    'SalesTaxAmount'      => $SalesTaxAmount,
-                    'ShippingService'     => $ShippingService,
-                    'ShippingServiceCost' => $ShippingServiceCost,
-                )
-            );
+        // Shipping Address
+        $shippingAddress     = $order->ShippingAddress;
+        $Name                = (string)$shippingAddress->Name;
+        $Address             = (string)$shippingAddress->Street1;
+        $Address2            = (string)$shippingAddress->Street2;
+        $City                = (string)$shippingAddress->CityName;
+        $Province            = (string)$shippingAddress->StateOrProvince;
+        $PostalCode          = (string)$shippingAddress->PostalCode;
+        $Country             = (string)$shippingAddress->Country;
+        $Phone               = (string)$shippingAddress->Phone;
 
-            $this->saveOrderItem($order);
-            $this->saveShippingAddress($order);
-
-        } catch (Exception $e) {
-            // echo $e->getMessage(), EOL;
-        }
-    }
-
-    private function saveOrderItem($order)
-    {
-        $OrderID = (string)$order->OrderID;
-
+        // Order Items
         $transactions = $order->TransactionArray;
 
         foreach ($transactions->Transaction as $transaction) {
@@ -95,55 +83,36 @@ class Ebay_Order extends OrderDownloader
                 $Tracking = 'NA';
             }
 
-            try {
-                $success = $this->db->insertAsDict('ebay_order_item',
-                    array(
-                        'OrderID'           => $OrderID,
-                        'SKU'               => $SKU,
-                        'QuantityPurchased' => $QuantityPurchased,
-                        'TransactionID'     => $TransactionID,
-                        'TransactionPrice'  => $TransactionPrice,
-                        'Tracking'          => $Tracking,
-                        'ItemID'            => $ItemID,
-                        'Email'             => $Email,
-                        'RecordNumber'      => $RecordNumber,
-                    )
-                );
-            } catch (Exception $e) {
-                // echo $e->getMessage(), EOL;
-            }
-        }
-    }
-
-    private function saveShippingAddress($order)
-    {
-        $OrderID         = (string)$order->OrderID;
-        $shippingAddress = $order->ShippingAddress;
-        $Name            = (string)$shippingAddress->Name;
-        $Address         = (string)$shippingAddress->Street1;
-        $Address2        = (string)$shippingAddress->Street2;
-        $City            = (string)$shippingAddress->CityName;
-        $Province        = (string)$shippingAddress->StateOrProvince;
-        $PostalCode      = (string)$shippingAddress->PostalCode;
-        $Country         = (string)$shippingAddress->Country;
-        $Phone           = (string)$shippingAddress->Phone;
-
-        try {
-            $success = $this->db->insertAsDict('ebay_order_shipping_address',
-                array(
-                    'OrderID'    => $OrderID,
-                    'Name'       => $Name,
-                    'Address'    => $Address,
-                    'Address2'   => $Address2,
-                    'City'       => $City,
-                    'Province'   => $Province,
-                    'PostalCode' => $PostalCode,
-                    'Country'    => $Country,
-                    'Phone'      => $Phone,
-                )
-            );
-        } catch (Exception $e) {
-            // echo $e->getMessage(), EOL;
+            $orderFile->write(compact(
+                // Order
+                'OrderID',
+                'Status',
+                'BuyerUsername',
+                'DatePaid',
+                'Currency',
+                'AmountPaid',
+                'SalesTaxAmount',
+                'ShippingService',
+                'ShippingServiceCost',
+                // Item
+                'SKU',
+                'QuantityPurchased',
+                'TransactionID',
+                'TransactionPrice',
+                'Tracking',
+                'ItemID',
+                'Email',
+                'RecordNumber',
+                // Address
+                'Name',
+                'Address',
+                'Address2',
+                'City',
+                'Province',
+                'PostalCode',
+                'Country',
+                'Phone'
+            ));
         }
     }
 }
---
+++ src/Marketplace/eBay/OrderReportFile.php
@@ -0,0 +1,87 @@
+<?php
+
+class OrderReportFile
+{
+    protected $handle;
+    protected $filename;
+
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function __destruct()
+    {
+        if (is_resource($this->handle)) {
+            fclose($this->handle);
+        }
+    }
+
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+
+    public function read()
+    {
+        if (!$this->handle) {
+            $this->handle = fopen($this->filename, 'r');
+            fgetcsv($this->handle); // skip the header
+        }
+
+        $fields = fgetcsv($this->handle);
+        if ($fields) {
+            return array_combine($this->getHeader(), $fields);
+        }
+
+        return $fields;
+    }
+
+    public function write($order)
+    {
+        if (!$this->handle) {
+            $this->handle = fopen($this->filename, 'w');
+            fputcsv($this->handle, $this->getHeader());
+        }
+
+        if (count($order) != count($this->getHeader())) {
+            throw new \Exception(__METHOD__. ' Wrong number of elements: '. var_export($order, true));
+        }
+
+        return fputcsv($this->handle, $order);
+    }
+
+    public function getHeader()
+    {
+        return [
+            // Order
+            'OrderID',
+            'Status',
+            'BuyerUsername',
+            'DatePaid',
+            'Currency',
+            'AmountPaid',
+            'SalesTaxAmount',
+            'ShippingService',
+            'ShippingServiceCost',
+            // Item
+            'SKU',
+            'QuantityPurchased',
+            'TransactionID',
+            'TransactionPrice',
+            'Tracking',
+            'ItemID',
+            'Email',
+            'RecordNumber',
+            // Address
+            'Name',
+            'Address',
+            'Address2',
+            'City',
+            'Province',
+            'PostalCode',
+            'Country',
+            'Phone'
+        ];
+    }
+}
