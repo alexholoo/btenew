fd4308513761a8911f02a78d8796df7bed41bb03
Hanson Li <lihsca@gmail.com>
2017-03-09 11:57:17 -0500

    File::archive

--- src/Toolkit/File.php
+++ src/Toolkit/File.php
@@ -26,6 +26,11 @@ class File // FileUtils
          return false;
     }
 
+    /**
+     * $ttl can be in two formats
+     * - number: for example, 3600, the interval in seconds
+     * - string: '3 hours', '1 day', etc
+     */
     public static function expired($filename, $ttl)
     {
         if (ctype_digit($ttl)) {
@@ -34,6 +39,9 @@ class File // FileUtils
         return (!file_exists($filename) || time() > strtotime($ttl, filemtime($filename)));
     }
 
+    /**
+     * @see File::expired
+     */
     public static function notExpired($filename, $ttl)
     {
         if (ctype_digit($ttl)) {
@@ -43,6 +51,8 @@ class File // FileUtils
     }
 
     /**
+     * Rename the file by suffixing filename with timestamp
+     *
      * Before:
      *   $filename = 'E:/BTE/purchase/shopping-cart.csv';
      *
@@ -61,6 +71,33 @@ class File // FileUtils
         $fname = $path['filename'].'-'.date('Ymd-His', filemtime($filename));
         $ext   = isset($path['extension']) ? $path['extension'] : '';
        #$ext   = $path['extension'] ?? ''; // php7+ only
+
+        $newfile = "$dir/$fname.$ext";
+
+        rename($filename, $newfile);
+    }
+
+    /**
+     * Rename the filen by suffixing filename with timestamp, then move the file
+     * to archive/ folder
+     *
+     * Before:
+     *   $filename = 'E:/BTE/purchase/shopping-cart.csv';
+     *
+     * After:
+     *   $filename = 'E:/BTE/purchase/archive/shopping-cart-20170106-141936.csv';
+     */
+    public static function archive($filename)
+    {
+        if (!file_exists($filename)) {
+            return;
+        }
+
+        $path = pathinfo($filename);
+
+        $dir   = $path['dirname'] . '/archive';
+        $fname = $path['filename'].'-'.date('Ymd-His', filemtime($filename));
+        $ext   = $path['extension'] ?? ''; // php7+ only
 
         $newfile = "$dir/$fname.$ext";
 
