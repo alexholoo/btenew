2d33d0d69e5e5f19541cb7a2c5530c3885b2ed10
Hanson Li <lihsca@gmail.com>
2017-03-09 16:17:30 -0500

    Download/import amazon orders

--- job/order/download/Amazon_Order.php
+++ job/order/download/Amazon_Order.php
@@ -1,181 +1,130 @@
 <?php
 
+use Marketplace\Amazon\OrderReportFile;
+
 class Amazon_Order extends OrderDownloader
 {
     public function download()
     {
-        return;
-
         // Amazon CA
         $client = new Marketplace\Amazon\Client('bte-amazon-ca');
-        $channel = 'Amazon-ACA';
+        $filename = Filenames::get('amazon.ca.order');
+        $orderFile = new OrderReportFile($filename);
 
         $orders = $client->getOrderList();
         foreach ($orders as $order) {
-            $this->saveOrder($order, $channel);
+            $this->saveOrder($order, $orderFile);
         }
 
         // Amazon US
         $client = new Marketplace\Amazon\Client('bte-amazon-us');
-        $channel = 'Amazon-US';
+        $filename = Filenames::get('amazon.us.order');
+        $orderFile = new OrderReportFile($filename);
 
         $orders = $client->getOrderList();
         foreach ($orders as $order) {
-            $this->saveOrder($order, $channel);
+            $this->saveOrder($order, $orderFile);
         }
     }
 
-    public function saveOrder($order, $channel)
+    public function saveOrder($order, $orderFile)
     {
         $data = $order->getData();
-
-        if ($this->orderExists($data['AmazonOrderId'])) {
-            return;
-        }
 
         if ($data['OrderStatus'] == 'Canceled') {
             // TODO: delete order if exists?
             return;
         }
 
-        if (!isset($data['OrderTotal'])) {
-            $data['OrderTotal']['Amount'] = '0.00';
-            $data['OrderTotal']['CurrencyCode'] = 'CAD';
-        }
-        if (!isset($data['PaymentMethod'])) {
-            $data['PaymentMethod'] = '';
-        }
-        if (!isset($data['BuyerName'])) {
-            $data['BuyerName'] = '';
-        }
+       #if (!isset($data['OrderTotal'])) {
+       #    $data['OrderTotal']['Amount'] = '0.00';
+       #    $data['OrderTotal']['CurrencyCode'] = 'CAD';
+       #}
+       #if (!isset($data['PaymentMethod'])) {
+       #    $data['PaymentMethod'] = '';
+       #}
+       #if (!isset($data['BuyerName'])) {
+       #    $data['BuyerName'] = '';
+       #}
         if (!isset($data['BuyerEmail'])) {
             $data['BuyerEmail'] = '';
         }
-        if (!isset($data['ShippedByAmazonTFM'])) {
-            $data['ShippedByAmazonTFM'] = 'false';
-        }
-        if (!isset($data['EarliestDeliveryDate'])) {
-            $data['EarliestDeliveryDate'] = null;
-        }
-        if (!isset($data['LatestDeliveryDate'])) {
-            $data['LatestDeliveryDate'] = null;
-        }
+       #if (!isset($data['ShippedByAmazonTFM'])) {
+       #    $data['ShippedByAmazonTFM'] = 'false';
+       #}
+       #if (!isset($data['EarliestDeliveryDate'])) {
+       #    $data['EarliestDeliveryDate'] = null;
+       #}
+       #if (!isset($data['LatestDeliveryDate'])) {
+       #    $data['LatestDeliveryDate'] = null;
+       #}
 
-        try {
-            $this->db->insertAsDict('amazon_order', [
-                'Channel'                      => $channel,
-                'OrderId'                      => $data['AmazonOrderId'],
-                'PurchaseDate'                 => $this->dtime($data['PurchaseDate']),
-                'LastUpdateDate'               => $this->dtime($data['LastUpdateDate']),
-                'OrderStatus'                  => $data['OrderStatus'],
-                'FulfillmentChannel'           => $data['FulfillmentChannel'],
-                'SalesChannel'                 => $data['SalesChannel'],
-                'ShipServiceLevel'             => $data['ShipServiceLevel'],
-                'CurrencyCode'                 => $data['OrderTotal']['CurrencyCode'],
-                'OrderTotalAmount'             => $data['OrderTotal']['Amount'],
-                'NumberOfItemsShipped'         => $data['NumberOfItemsShipped'],
-                'NumberOfItemsUnshipped'       => $data['NumberOfItemsUnshipped'],
-                'PaymentMethod'                => $data['PaymentMethod'],
-                'BuyerName'                    => $data['BuyerName'],
-                'BuyerEmail'                   => $data['BuyerEmail'],
-                'ShipmentServiceLevelCategory' => $data['ShipmentServiceLevelCategory'],
-                'ShippedByAmazonTFM'           => $this->yesNo($data['ShippedByAmazonTFM']),
-                'OrderType'                    => $data['OrderType'],
-                'EarliestShipDate'             => $this->dtime($data['EarliestShipDate']),
-                'LatestShipDate'               => $this->dtime($data['LatestShipDate']),
-                'EarliestDeliveryDate'         => $this->dtime($data['EarliestDeliveryDate']),
-                'LatestDeliveryDate'           => $this->dtime($data['LatestDeliveryDate']),
-                'IsBusinessOrder'              => $this->yesNo($data['IsBusinessOrder']),
-                'IsPrime'                      => $this->yesNo($data['IsPrime']),
-                'IsPremiumOrder'               => $this->yesNo($data['IsPremiumOrder']),
-            ]);
+        $address = $order->getShippingAddress();
 
-            echo $data['AmazonOrderId'], ' ', $channel, EOL;
-
-            $this->saveOrderItem($order);
-            $this->saveShippingAddress($order);
-
-        } catch (\Exception $e) {
-            //echo $e->getMessage(), EOL;
-        }
-    }
-
-    private function saveOrderItem($order)
-    {
         $items = $order->fetchItems();
 
         foreach ($items->getItems() as $item) {
-            if (!isset($item['ConditionNote'])) {
-                $item['ConditionNote'] = '';
-            }
+           #if (!isset($item['ConditionNote'])) {
+           #    $item['ConditionNote'] = '';
+           #}
 
-            try {
-                $this->db->insertAsDict('amazon_order_item', [
-                    'OrderId'            => $order->getAmazonOrderId(),
-                    'ASIN'               => $item['ASIN'],
-                    'SellerSKU'          => $item['SellerSKU'],
-                    'OrderItemId'        => $item['OrderItemId'],
-                    'Title'              => $item['Title'],
-                    'QuantityOrdered'    => $item['QuantityOrdered'],
-                    'QuantityShipped'    => $item['QuantityShipped'],
-                    'CurrencyCode'       => $item['ItemPrice']['CurrencyCode'],
-                    'ItemPrice'          => $item['ItemPrice']['Amount'],
-                    'ShippingPrice'      => $item['ShippingPrice']['Amount'],
-                    'GiftWrapPrice'      => $item['GiftWrapPrice']['Amount'],
-                    'ItemTax'            => $item['ItemTax']['Amount'],
-                    'ShippingTax'        => $item['ShippingTax']['Amount'],
-                    'GiftWrapTax'        => $item['GiftWrapTax']['Amount'],
-                    'ShippingDiscount'   => $item['ShippingDiscount']['Amount'],
-                    'PromotionDiscount'  => $item['PromotionDiscount']['Amount'],
-                    'ConditionId'        => $item['ConditionId'],
-                    'ConditionSubtypeId' => $item['ConditionSubtypeId'],
-                    'ConditionNote'      => $item['ConditionNote'],
-                ]);
-            } catch (\Exception $e) {
-                //echo $e->getMessage(), EOL;
-            }
-        }
-    }
-
-    private function saveShippingAddress($order)
-    {
-        $address = $order->getShippingAddress();
-
-        try {
-            $this->db->insertAsDict('amazon_order_shipping_address', [
-                'OrderId'       => $order->getAmazonOrderId(),
-                'Name'          => $address['Name'],
-                'AddressLine1'  => $address['AddressLine1'],
-                'AddressLine2'  => $address['AddressLine2'],
-                'AddressLine3'  => $address['AddressLine3'],
-                'City'          => $address['City'],
-                'County'        => $address['County'],
-                'District'      => $address['District'],
-                'StateOrRegion' => $address['StateOrRegion'],
-                'PostalCode'    => $address['PostalCode'],
-                'CountryCode'   => $address['CountryCode'],
-                'Phone'         => $address['Phone'],
+            $orderFile->write([
+                $data['AmazonOrderId'],
+                $data['PurchaseDate'],
+               #$data['LastUpdateDate'],
+                $data['OrderStatus'],
+                $data['FulfillmentChannel'],
+                $data['SalesChannel'],
+                $data['ShipServiceLevel'],
+               #$data['OrderTotal']['CurrencyCode'],
+               #$data['OrderTotal']['Amount'],
+               #$data['NumberOfItemsShipped'],
+               #$data['NumberOfItemsUnshipped'],
+               #$data['PaymentMethod'],
+               #$data['BuyerName'],
+                $data['BuyerEmail'],
+               #$data['ShipmentServiceLevelCategory'],
+               #$data['ShippedByAmazonTFM'],
+               #$data['OrderType'],
+               #$data['EarliestShipDate'],
+               #$data['LatestShipDate'],
+               #$data['EarliestDeliveryDate'],
+               #$data['LatestDeliveryDate'],
+               #$data['IsBusinessOrder'],
+               #$data['IsPrime'],
+               #$data['IsPremiumOrder'],
+                // item
+                $item['ASIN'],
+                $item['SellerSKU'],
+                $item['OrderItemId'],
+                $item['Title'],
+                $item['QuantityOrdered'],
+               #$item['QuantityShipped'],
+                $item['ItemPrice']['CurrencyCode'],
+                $item['ItemPrice']['Amount'],
+                $item['ShippingPrice']['Amount'],
+               #$item['GiftWrapPrice']['Amount'],
+               #$item['ItemTax']['Amount'],
+               #$item['ShippingTax']['Amount'],
+               #$item['GiftWrapTax']['Amount'],
+               #$item['ShippingDiscount']['Amount'],
+               #$item['PromotionDiscount']['Amount'],
+               #$item['ConditionId'],
+               #$item['ConditionSubtypeId'],
+               #$item['ConditionNote'],
+                // address
+                $address['Name'],
+                $address['AddressLine1'],
+                $address['AddressLine2'],
+                $address['AddressLine3'],
+                $address['City'],
+                $address['County'],
+                $address['District'],
+                $address['StateOrRegion'],
+                $address['PostalCode'],
+                $address['CountryCode'],
+                $address['Phone'],
             ]);
-        } catch (\Exception $e) {
-            //echo $e->getMessage(), EOL;
         }
-    }
-
-    private function orderExists($orderId)
-    {
-        $sql = "SELECT OrderId FROM amazon_order WHERE OrderId='$orderId'";
-        return $this->db->fetchOne($sql);
-    }
-
-    private function yesNo($value)
-    {
-        if ($value == 'false') return 'N';
-        return 'Y';
-    }
-
-    private function dtime($value)
-    {
-        # "2016-09-22T06:59:59Z" => "2016-09-22 06:59:59"
-        return str_replace(['T', 'Z'], [' ', ''], $value);
     }
 }
--- job/order/import/Amazon_Order.php
+++ job/order/import/Amazon_Order.php
@@ -1,164 +1,68 @@
 <?php
 
+use Marketplace\Amazon\OrderReportFile;
+
 class Amazon_Order extends OrderImporter
 {
     public function import()
     {
-        return; // NOT DONE YET
-
         // Amazon CA
-        $this->channel = 'Amazon-ACA';
-        $client = new Marketplace\Amazon\Client('bte-amazon-ca');
-        $orders = $client->getOrderList();
-        $orders = $this->reindexOrders($orders);
+        $channel = 'Amazon-ACA';
+        $filename = Filenames::get('amazon.ca.order');
+        $orders = $this->getOrders($filename, $channel);
         $this->importMasterOrders($orders);
 
         // Amazon US
-        $this->channel = 'Amazon-US';
-        $client = new Marketplace\Amazon\Client('bte-amazon-us');
-        $orders = $client->getOrderList();
-        $orders = $this->reindexOrders($orders);
+        $channel = 'Amazon-US';
+        $filename = Filenames::get('amazon.us.order');
+        $orders = $this->getOrders($filename, $channel);
         $this->importMasterOrders($orders);
     }
 
-    public function reindexOrders($orders)
+    public function getOrders($filename, $channel)
     {
-        $data = $order->getData();
+        $orderFile = new OrderReportFile($filename);
 
-        if ($this->orderExists($data['AmazonOrderId'])) { return; }
-        if ($data['OrderStatus'] == 'Canceled') { return; }
+        $orders = [];
 
-        if (!isset($data['OrderTotal'])) {
-            $data['OrderTotal']['Amount'] = '0.00';
-            $data['OrderTotal']['CurrencyCode'] = 'CAD';
-        }
-        if (!isset($data['PaymentMethod'])) {
-            $data['PaymentMethod'] = '';
-        }
-        if (!isset($data['BuyerName'])) {
-            $data['BuyerName'] = '';
-        }
-        if (!isset($data['BuyerEmail'])) {
-            $data['BuyerEmail'] = '';
-        }
-        if (!isset($data['ShippedByAmazonTFM'])) {
-            $data['ShippedByAmazonTFM'] = 'false';
-        }
-        if (!isset($data['EarliestDeliveryDate'])) {
-            $data['EarliestDeliveryDate'] = null;
-        }
-        if (!isset($data['LatestDeliveryDate'])) {
-            $data['LatestDeliveryDate'] = null;
+        while ($order = $orderFile->read()) {
+            $orderId = $order['OrderId'];
+            $orders[$orderId][] = $this->toStdOrder($order, $channel);
         }
 
-        $this->db->insertAsDict('amazon_order', [
-            $this->channel,
-            $data['AmazonOrderId'],
-            $this->dtime($data['PurchaseDate']),
-            $this->dtime($data['LastUpdateDate']),
-            $data['OrderStatus'],
-            $data['FulfillmentChannel'],
-            $data['SalesChannel'],
-            $data['ShipServiceLevel'],
-            $data['OrderTotal']['CurrencyCode'],
-            $data['OrderTotal']['Amount'],
-            $data['NumberOfItemsShipped'],
-            $data['NumberOfItemsUnshipped'],
-            $data['PaymentMethod'],
-            $data['BuyerName'],
-            $data['BuyerEmail'],
-            $data['ShipmentServiceLevelCategory'],
-            $this->yesNo($data['ShippedByAmazonTFM']),
-            $data['OrderType'],
-            $this->dtime($data['EarliestShipDate']),
-            $this->dtime($data['LatestShipDate']),
-            $this->dtime($data['EarliestDeliveryDate']),
-            $this->dtime($data['LatestDeliveryDate']),
-            $this->yesNo($data['IsBusinessOrder']),
-            $this->yesNo($data['IsPrime']),
-            $this->yesNo($data['IsPremiumOrder']),
-        ]);
-
-        $items = $order->fetchItems();
-
-        foreach ($items->getItems() as $item) {
-            if (!isset($item['ConditionNote'])) {
-                $item['ConditionNote'] = '';
-            }
-
-            $this->db->insertAsDict('amazon_order_item', [
-                $order->getAmazonOrderId(),
-                $item['ASIN'],
-                $item['SellerSKU'],
-                $item['OrderItemId'],
-                $item['Title'],
-                $item['QuantityOrdered'],
-                $item['QuantityShipped'],
-                $item['ItemPrice']['CurrencyCode'],
-                $item['ItemPrice']['Amount'],
-                $item['ShippingPrice']['Amount'],
-                $item['GiftWrapPrice']['Amount'],
-                $item['ItemTax']['Amount'],
-                $item['ShippingTax']['Amount'],
-                $item['GiftWrapTax']['Amount'],
-                $item['ShippingDiscount']['Amount'],
-                $item['PromotionDiscount']['Amount'],
-                $item['ConditionId'],
-                $item['ConditionSubtypeId'],
-                $item['ConditionNote'],
-            ]);
-        }
-
-        $address = $order->getShippingAddress();
-        $this->db->insertAsDict('amazon_order_shipping_address', [
-            $order->getAmazonOrderId(),
-            $address['Name'],
-            $address['AddressLine1'],
-            $address['AddressLine2'],
-            $address['AddressLine3'],
-            $address['City'],
-            $address['County'],
-            $address['District'],
-            $address['StateOrRegion'],
-            $address['PostalCode'],
-            $address['CountryCode'],
-            $address['Phone'],
-        ]);
+        return $orders;
     }
 
-    private function toStdOrder($order)
+    private function toStdOrder($order, $channel)
     {
+        $express = $this->isExpress($order);
+
         return [
-             'orderId'      => $order[''],
-             'date'         => $order[''],
-             'orderItemId'  => $order[''],
-             'channel'      => $this->channel,
-             'express'      => $order[''],
-             'buyer'        => $order[''],
-             'address'      => $order[''],
-             'city'         => $order[''],
-             'province'     => $order[''],
-             'country'      => $order[''],
-             'postalcode'   => $order[''],
-             'email'        => '',
-             'phone'        => $order[''],
-             'sku'          => $order[''],
-             'qty'          => $order[''],
-             'price'        => $order[''],
-             'shipping'     => $order[''],
-             'productName'  => $order[''],
+             'orderId'      => $order['OrderId'],
+             'date'         => substr($order['Date'], 0, 10),
+             'orderItemId'  => $order['OrderItemId'],
+             'channel'      => $channel,
+             'express'      => $express,
+             'buyer'        => $order['Name'],
+             'address'      => $order['Address1'].' '.$order['Address1'].' '.$order['Address1'],
+             'city'         => $order['City'],
+             'province'     => $order['StateOrRegion'],
+             'country'      => $order['CountryCode'],
+             'postalcode'   => $order['PostalCode'],
+             'email'        => $order['BuyerEmail'],
+             'phone'        => $order['Phone'],
+             'sku'          => $order['SellerSKU'],
+             'qty'          => $order['Quantity'],
+             'price'        => $order['ItemPrice'],
+             'shipping'     => $order['ShippingPrice'],
+             'productName'  => $order['Title'],
         ];
     }
 
-    private function yesNo($value)
-    {
-        if ($value == 'false') return 'N';
-        return 'Y';
-    }
-
-    private function dtime($value)
+    private function isExpress($order)
     {
         # "2016-09-22T06:59:59Z" => "2016-09-22 06:59:59"
-        return str_replace(['T', 'Z'], [' ', ''], $value);
+        // $order['ShipServiceLevel']
+        return 0;
     }
 }
