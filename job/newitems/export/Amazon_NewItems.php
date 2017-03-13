1f6ed6dbb785601661e735171301d17d0b48fd92
Hanson Li <lihsca@gmail.com>
2017-03-13 14:14:31 -0400

    NewItems for Amazon

--- job/newitems/export/Amazon_NewItems.php
+++ job/newitems/export/Amazon_NewItems.php
@@ -4,5 +4,19 @@ class Amazon_NewItems extends NewItemsExporter
 {
     public function export()
     {
+        $this->exportNewItemsCA();
+        $this->exportNewItemsUS();
+    }
+
+    protected function exportNewItemsCA()
+    {
+        $store = 'bte-amazon-ca';
+        $filename = Filenames::get('amazon.ca.newitems');
+    }
+
+    protected function exportNewItemsUS()
+    {
+        $store = 'bte-amazon-us';
+        $filename = Filenames::get('amazon.us.newitems');
     }
 }
