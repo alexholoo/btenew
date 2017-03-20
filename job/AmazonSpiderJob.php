c01ebc5928e09a8cf705cb2ff927ef472010d586
Hanson Li <lihsca@gmail.com>
2017-03-20 17:52:51 -0400

    curl download

--- job/AmazonSpiderJob.php
+++ job/AmazonSpiderJob.php
@@ -5,9 +5,17 @@ include 'classes/simple_html_dom.php';
 
 class AmazonSpiderJob extends Job
 {
-    protected $proxyList;
-    protected $headerList;
-    protected $userAgentList;
+    protected $failed;
+
+    protected $proxy;
+    protected $proxyAuth;
+
+    protected $cookieFile = 'cookies.txt';
+
+    protected $headers;
+    protected $userAgent;
+    protected $referer;
+    protected $encoding;
 
     public function run($argv = [])
     {
@@ -15,7 +23,9 @@ class AmazonSpiderJob extends Job
 
         $asinList = $this->getAsinList();
 
-        $failed = 0;
+        $this->failed = 0;
+
+        $this->changeWhoAmI();
 
         foreach ($asinList as $asin) {
             $fname = "E:/BTE/amazon/item-desc/html/$asin.html";
@@ -24,19 +34,17 @@ class AmazonSpiderJob extends Job
             echo $asin, PHP_EOL;
 
             $url = "https://www.amazon.com/dp/$asin";
-
-            // TODO: use php curl to download the page
-            exec("wget $url -O $fname");
+            // exec("wget $url -O $fname");
+            $this->downloadPage($url, $fname);
 
             if (filesize($fname) < 10000) { // 10K
                 echo chr(7);
 
-                // TODO: change proxy/header/userAgent
-                // changeWhoAmI
+                $this->changeWhoAmI();
 
-                if (++$failed == 3) break;
+                if (++$this->failed == 3) break;
             } else {
-                $failed = 0;
+                $this->failed = 0;
 
                 // The file might be gzipped, need to gunzip first.
                 // $this->processFile($fname, $asin);
@@ -45,9 +53,144 @@ class AmazonSpiderJob extends Job
             if (date('H') < 11 || date('H') >= 18) {
                 sleep(30); // not in office hours
             } else {
-                sleep(rand(90, 150)); // office hours
+               #sleep(rand(90, 150)); // office hours
+                sleep(5);
             }
         }
+    }
+
+    protected function downloadPage($url, $fname)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+       #curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+       #curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookieFile);
+       #curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookieFile);
+
+       #curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
+       #curl_setopt($ch, CURLOPT_PROXYUSERPWD, $this->proxyAuth);
+       #curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+
+       #curl_setopt($ch, CURLOPT_REFERER, $this->referer);
+        curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+       #curl_setopt($ch, CURLOPT_ENCODING, $this->encoding);
+
+        $output = curl_exec($ch);
+
+        $error = curl_error($ch);
+        $info = curl_getinfo($ch);
+        $errno = curl_errno($ch);
+
+        // print_r($info);
+        /*Array
+        (
+            [http_code] => 200
+            [header_size] => 1199
+            [request_size] => 308
+            [size_download] => 184052
+            [speed_download] => 76465
+        )*/
+
+        curl_close($ch);
+
+        file_put_contents($fname, $output);
+    }
+
+    protected function changeWhoAmI()
+    {
+        $proxies = [
+            'http://138.197.23.220:8080',
+            'http://207.141.59.221:8080',
+            'http://35.185.42.72:80',
+            'http://96.239.193.243:8080',
+            'http://164.58.168.2:8080',
+            'http://35.185.12.179:80',
+            'http://207.99.118.74:8080',
+            'http://47.52.33.214:3128',
+            'http://35.167.66.19:3128',
+            'http://45.55.209.249:3128',
+            'http://104.131.30.42:8080',
+            'http://35.185.199.138:80',
+            'http://35.185.207.5:80',
+            'http://47.52.33.136:3128',
+            'http://45.79.219.25:3128',
+            'http://104.196.248.56:80',
+            'http://198.23.161.126:3128',
+            'http://192.129.198.21:9001',
+            'http://74.84.131.34:80',
+            'http://94.78.199.45:8081',
+            'http://110.50.84.162:8080',
+        ];
+
+        $headers = [
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Encoding: gzip, deflate',
+            'Accept-Language: en-US,en;q=0.5',
+        ];
+
+        $referers = [
+            '',
+            'http://cn.dealmoon.com',
+            'http://www.amazon.com/',
+            'http://www.baidu.com/',
+            'http://www.google.com/',
+            '',
+        ];
+
+        $userAgents = [
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A',
+            'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; da-dk) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1',
+            'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1',
+            'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0',
+            'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11',
+            'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+            'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0',
+            'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
+            'Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36',
+            'Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0',
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36',
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0',
+            'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Macintosh; Intel Mac OS X 10_7_3; Trident/6.0)',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/4.0; InfoPath.2; SV1; .NET CLR 2.0.50727; WOW64)',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/5.0)',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 7.0; InfoPath.3; .NET CLR 3.1.40767; Trident/6.0; en-IN)',
+            'Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0',
+        ];
+
+        $this->proxy = $proxies[array_rand($proxies)];
+        $this->headers = $headers;
+        $this->userAgent = $userAgents[array_rand($userAgents)];
+        $this->referer = $referers[array_rand($referers)];
+        $this->encoding = 'en-US';
+
+       #print_r($this->proxy); echo PHP_EOL;
+       #print_r($this->headers); echo PHP_EOL;
+       #print_r($this->userAgent); echo PHP_EOL;
+       #print_r($this->referer); echo PHP_EOL;
     }
 
     protected function getAsinList()
