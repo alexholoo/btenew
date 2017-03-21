24f7aa92b3c44d63ebe8e87766caa0e3de61710b
Hanson Li <lihsca@gmail.com>
2017-03-21 14:37:12 -0400

    Newegg US newitems upload

--- job/newitems/upload/Newegg_NewItems.php
+++ job/newitems/upload/Newegg_NewItems.php
@@ -6,10 +6,20 @@ class Newegg_NewItems extends NewItemsUploader
 {
     public function upload()
     {
+        // CA
         $filename = Filenames::get('newegg.ca.newitems');
 
         if (file_exists($filename)) {
             $client = new Marketplace\Newegg\Client('CA');
+            $client->uploadNewItems($filename);
+            File::backup($filename);
+        }
+
+        // US
+        $filename = Filenames::get('newegg.us.newitems');
+
+        if (file_exists($filename)) {
+            $client = new Marketplace\Newegg\Client('US');
             $client->uploadNewItems($filename);
             File::backup($filename);
         }
