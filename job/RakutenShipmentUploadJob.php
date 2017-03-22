0bd5333253673a5284271f2750dc3b6d9e7a4f23
Hanson Li <lihsca@gmail.com>
2017-03-22 17:01:45 -0400

    Rakuten jobs

---
+++ job/RakutenShipmentUploadJob.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'classes/Job.php';
+
+class RakutenShipmentUploadJob extends Job
+{
+    public function run($argv = [])
+    {
+        $this->log('>> '. __CLASS__);
+
+        include_once('tracking/upload/Base.php');
+        include_once('tracking/Filenames.php');
+
+        $job = $this->getJob("tracking/upload/Rakuten_Shipment.php");
+
+        $job->upload();
+    }
+
+    protected function getJob($filename)
+    {
+        include_once($filename);
+
+        $path = pathinfo($filename);
+        $class = $path['filename'];
+
+        $job = false;
+
+        if (class_exists($class)) {
+            $job = new $class;
+        }
+
+        return $job;
+    }
+}
+
+include __DIR__ . '/../public/init.php';
+
+$job = new RakutenShipmentUploadJob();
+$job->run($argv);
