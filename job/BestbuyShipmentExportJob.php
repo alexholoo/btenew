80b9c1f69cd32ec801bf4c0b8f582bc5303e0596
Hanson Li <lihsca@gmail.com>
2017-03-22 16:37:05 -0400

    Bestbuy Jobs

---
+++ job/BestbuyShipmentExportJob.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'classes/Job.php';
+
+class BestbuyShipmentExportJob extends Job
+{
+    public function run($argv = [])
+    {
+        $this->log('>> '. __CLASS__);
+
+        include_once('tracking/export/Base.php');
+        include_once('tracking/Filenames.php');
+
+        $job = $this->getJob("tracking/export/Bestbuy_Tracking.php");
+
+        $job->export();
+    }
+
+    protected function getJob($filename)
+    {
+        include_once($filename);
+
+        $path = pathinfo($filename);
+        $class = $path['filename'];
+
+        $job = false;
+
+        if (class_exists($class)) {
+            $job = new $class;
+        }
+
+        return $job;
+    }
+}
+
+include __DIR__ . '/../public/init.php';
+
+$job = new BestbuyShipmentExportJob();
+$job->run($argv);
---
+++ job/BestbuyShipmentUploadJob.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'classes/Job.php';
+
+class BestbuyShipmentUploadJob extends Job
+{
+    public function run($argv = [])
+    {
+        $this->log('>> '. __CLASS__);
+
+        include_once('tracking/upload/Base.php');
+        include_once('tracking/Filenames.php');
+
+        $job = $this->getJob("tracking/upload/Bestbuy_Shipment.php");
+
+        $job->upload();
+    }
+
+    protected function getJob($filename)
+    {
+        include_once($filename);
+
+        $path = pathinfo($filename);
+        $class = $path['filename'];
+
+        $job = false;
+
+        if (class_exists($class)) {
+            $job = new $class;
+        }
+
+        return $job;
+    }
+}
+
+include __DIR__ . '/../public/init.php';
+
+$job = new BestbuyShipmentUploadJob();
+$job->run($argv);
