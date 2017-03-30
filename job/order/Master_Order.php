e2f4ac9cce2afeb31402ccbf47de854e9d4af693
Hanson Li <lihsca@gmail.com>
2017-03-30 13:30:28 -0400

    order/Master_Order needs more testing

--- job/order/Master_Order.php
+++ job/order/Master_Order.php
@@ -12,8 +12,8 @@ class Master_Order extends Job
 
         $this->importBestbuyOrders($masterFile);
 
-        $this->importEbayOrders($masterFile, 'BTE');
-        $this->importEbayOrders($masterFile, 'ODO');
+#       $this->importEbayOrders($masterFile, 'BTE');
+#       $this->importEbayOrders($masterFile, 'ODO');
 
         $this->importNeweggOrders($masterFile, 'CA');
         $this->importNeweggOrders($masterFile, 'US');
@@ -33,26 +33,30 @@ class Master_Order extends Job
             $filename = Filenames::get('amazon.us.order');
         }
 
-        $orderFile = new Marketplace\Amazon\StdOrderReportFile($filename);
+        $orderFile = new Marketplace\Amazon\StdOrderReportFile($filename, $site);
+
         while ($order = $orderFile->read()) {
+            $express = ($order['ship-service-level'] == 'Expedited') ? 1 : 0;
+            $address = $order['ship-address-1'].' '.$order['ship-address-1'].' '.$order['ship-address-1'];
+
             $masterFile->write([
-                $order[''],     // 'channel',
-                $order[''],     // 'date',
-                $order[''],     // 'channel_order_id',
-                $order[''],     // 'order_item_id',
-                $order[''],     // 'express',
-                $order[''],     // 'buyer',
-                $order[''],     // 'address',
-                $order[''],     // 'city',
-                $order[''],     // 'province',
-                $order[''],     // 'postalcode',
-                $order[''],     // 'country',
-                $order[''],     // 'phone',
-                $order[''],     // 'email',
-                $order[''],     // 'sku',
-                $order[''],     // 'price',
-                $order[''],     // 'qty',
-                $order[''],     // 'shipping',
+                $channel,
+                $order['purchase-date'],
+                $order['order-id'],
+                $order['order-item-id'],
+                $express,
+                $order['buyer-name'],
+                $address,
+                $order['ship-city'],
+                $order['ship-state'],
+                $order['ship-postal-code'],
+                $order['ship-country'],
+                $order['ship-phone-number'],
+                $order['buyer-email'],
+                $order['sku'],
+                $order['item-price'],
+                $order['quantity-purchased'],
+                $order['shipping-price'],
             ]);
         }
     }
@@ -63,25 +67,29 @@ class Master_Order extends Job
         $filename = Filenames::get('bestbuy.order');
 
         $orderFile = new Marketplace\Bestbuy\OrderReportFile($filename);
+
         while ($order = $orderFile->read()) {
+            if ($order['status'] != 'RECEIVED') {
+                continue;
+            }
             $masterFile->write([
-                $channel,       // 'channel',
-                $order[''],     // 'date',
-                $order[''],     // 'channel_order_id',
-                $order[''],     // 'order_item_id',
-                $order[''],     // 'express',
-                $order[''],     // 'buyer',
-                $order[''],     // 'address',
-                $order[''],     // 'city',
-                $order[''],     // 'province',
-                $order[''],     // 'postalcode',
-                $order[''],     // 'country',
-                $order[''],     // 'phone',
-                $order[''],     // 'email',
-                $order[''],     // 'sku',
-                $order[''],     // 'price',
-                $order[''],     // 'qty',
-                $order[''],     // 'shipping',
+                $channel,
+                $order['date'],
+                $order['orderId'],
+                $order['orderItemId'],
+                $order['express'],
+                $order['buyer'],
+                $order['address'],
+                $order['city'],
+                $order['state'],
+                $order['zipcode'],
+                $order['country'],
+                $order['phone'],
+                '', // 'email',
+                $order['sku'],
+                $order['price'],
+                $order['qty'],
+                $order['shipping'],
             ]);
         }
     }
@@ -99,25 +107,27 @@ class Master_Order extends Job
         }
 
         $orderFile = new Marketplace\eBay\OrderReportFile($filename);
+
         while ($order = $orderFile->read()) {
+            $express = $order['ShippingService'];
             $masterFile->write([
-                $channel,       // 'channel',
-                $order[''],     // 'date',
-                $order[''],     // 'channel_order_id',
-                $order[''],     // 'order_item_id',
-                $order[''],     // 'express',
-                $order[''],     // 'buyer',
-                $order[''],     // 'address',
-                $order[''],     // 'city',
-                $order[''],     // 'province',
-                $order[''],     // 'postalcode',
-                $order[''],     // 'country',
-                $order[''],     // 'phone',
-                $order[''],     // 'email',
-                $order[''],     // 'sku',
-                $order[''],     // 'price',
-                $order[''],     // 'qty',
-                $order[''],     // 'shipping',
+                $channel,
+                $order['DatePaid'],
+                $order['OrderID'],
+                $order['ItemID'],
+                $express,
+                $order['Name'],
+                $order['Address'].' '.$order['Address2'],
+                $order['City'],
+                $order['Province'],
+                $order['PostalCode'],
+                $order['Country'],
+                $order['Phone'],
+                $order['Email'],
+                $order['SKU'],
+                $order['TransactionPrice'],
+                $order['QuantityPurchased'],
+                $order['ShippingServiceCost'],
             ]);
         }
     }
@@ -134,26 +144,31 @@ class Master_Order extends Job
             $filename = Filenames::get('newegg.us.master.order');
         }
 
-        $orderFile = new Marketplace\Newegg\StdOrderListFile($filename);
+        $orderFile = new Marketplace\Newegg\StdOrderListFile($filename, $site);
+
         while ($order = $orderFile->read()) {
+            $address = $order['Ship To Address Line 1'].' '.$order['Ship To Address Line 2'];
+            $express = preg_match('/Standard|Economy/', $order['Order Shipping Method']) ? 0 : 1;
+            $buyer = $order['Ship To First Name'].' '.$order['Ship To LastName'];
+
             $masterFile->write([
-                $channel,       // 'channel',
-                $order[''],     // 'date',
-                $order[''],     // 'channel_order_id',
-                $order[''],     // 'order_item_id',
-                $order[''],     // 'express',
-                $order[''],     // 'buyer',
-                $order[''],     // 'address',
-                $order[''],     // 'city',
-                $order[''],     // 'province',
-                $order[''],     // 'postalcode',
-                $order[''],     // 'country',
-                $order[''],     // 'phone',
-                $order[''],     // 'email',
-                $order[''],     // 'sku',
-                $order[''],     // 'price',
-                $order[''],     // 'qty',
-                $order[''],     // 'shipping',
+                $channel,
+                date('Y-m-d', strtotime($order['Order Date & Time'])),
+                $order['Order Number'],
+                $order['Item Newegg #'],
+                $express,
+                $buyer,
+                $address,
+                $order['Ship To City'],
+                $order['Ship To State'],
+                $order['Ship To ZipCode'],
+                $order['Ship To Country'],
+                $order['Ship To Phone Number'],
+                $order['Order Customer Email'],
+                $order['Item Seller Part #'],
+                $order['Item Unit Price'],
+                $order['Quantity Ordered'],
+                $order['Item Unit Shipping Charge'],
             ]);
         }
     }
@@ -163,26 +178,29 @@ class Master_Order extends Job
         $channel = 'Rakuten-BUY';
         $filename = Filenames::get('rakuten.us.master.order');
 
-        $orderFile = new Marketplace\Newegg\StdOrderListFile($filename);
+        $orderFile = new Marketplace\Rakuten\StdOrderListFile($filename, 'US');
+
         while ($order = $orderFile->read()) {
+            $address = $order['Ship_To_Street1'].' '.$order['Ship_To_Street2'];
+            $express = $order['ShippingMethodId'];
             $masterFile->write([
-                $channel,       // 'channel',
-                $order[''],     // 'date',
-                $order[''],     // 'channel_order_id',
-                $order[''],     // 'order_item_id',
-                $order[''],     // 'express',
-                $order[''],     // 'buyer',
-                $order[''],     // 'address',
-                $order[''],     // 'city',
-                $order[''],     // 'province',
-                $order[''],     // 'postalcode',
-                $order[''],     // 'country',
-                $order[''],     // 'phone',
-                $order[''],     // 'email',
-                $order[''],     // 'sku',
-                $order[''],     // 'price',
-                $order[''],     // 'qty',
-                $order[''],     // 'shipping',
+                $channel,
+                date('Y-m-d', strtotime($order['Date_Entered'])),
+                $order['Receipt_ID'],
+                $order['Receipt_Item_ID'],
+                $express,
+                $order['Ship_To_Name'],
+                $address,
+                $order['Ship_To_City'],
+                $order['Ship_To_State'],
+                $order['Ship_To_Zip'],
+                'US',     // 'country',
+                $order['Bill_To_Phone'],
+                $order['Email'],
+                $order['ReferenceId'],
+                $order['Price'],
+                $order['Quantity'],
+                $order['ShippingFee'],
             ]);
         }
     }
