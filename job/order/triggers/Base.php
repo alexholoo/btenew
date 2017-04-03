3cff89b132a60f73c93a345a4ce0401855ae798e
Hanson Li <lihsca@gmail.com>
2017-04-03 15:58:55 -0400

    add comment

--- job/order/triggers/Base.php
+++ job/order/triggers/Base.php
@@ -3,7 +3,8 @@
 class OrderTrigger extends Job
 {
     protected $orders;
-    protected $priority = 0;  // 0 to disable
+    protected $priority = 0; // Trigger with smaller priority runs first
+                             // 0 to disable the trigger
 
     public function setOrders($orders)
     {
