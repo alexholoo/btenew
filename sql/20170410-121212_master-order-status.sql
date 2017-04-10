47534a0c80bfb4d92f67643e7734b3e5a9677f09
Hanson Li <lihsca@gmail.com>
2017-04-10 12:42:18 -0400

    drop primary key on master_order_status

--- sql/20170410-121212_master-order-status.sql
+++ sql/20170410-121212_master-order-status.sql
@@ -4,7 +4,8 @@ ALTER TABLE `master_order_status`
 	ALTER `order_item_id` DROP DEFAULT;
 ALTER TABLE `master_order_status`
 	CHANGE COLUMN `order_item_id` `order_item_id` VARCHAR(40) NOT NULL COMMENT 'master_order_item.id' FIRST;
-
+ALTER TABLE `master_order_status`
+	DROP PRIMARY KEY;
 
 ALTER TABLE `master_order_item`
 	ADD COLUMN `order_item_id` VARCHAR(40) NOT NULL AFTER `order_id`;
