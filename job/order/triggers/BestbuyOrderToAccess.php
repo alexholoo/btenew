48474ef9c4e7c5943e636a3dfddcdc61f215295a
Hanson Li <lihsca@gmail.com>
2017-04-10 17:52:54 -0400

    bestbuy/newegg order triggers

--- job/order/triggers/BestbuyOrderToAccess.php
+++ job/order/triggers/BestbuyOrderToAccess.php
@@ -8,7 +8,85 @@ class BestbuyOrderToAccess extends OrderTrigger
     {
         $this->log('=> '. __CLASS__);
 
-        // if ($order['channel'] != 'Bestbuy') continue;
-        // if (isset($order['overstock-changed']))
+        $this->skuService = $this->di->get('skuService');
+
+        $this->importBestbuyOrders();
+    }
+
+    protected function importBestbuyOrders()
+    {
+        $accdb = $this->openAccessDB();
+
+        $orders = $this->getBestbuyOrders();
+
+        foreach ($this->orders as $order) {
+            if ($order['channel'] != 'Bestbuy') {
+                continue;
+            }
+
+            $workDate    = $order['date'];
+            $orderId     = $order['reference'];
+            $sku         = $order['sku'];
+            $channel     = 'BestbuyCA';
+            $xpress      = $order['express'];
+            $qty         = $order['qty'];
+            $supplier    = $this->skuService->getSupplier($sku);
+            $ponum       = ' ';
+            $mfrpn       = $this->skuService->getMpn($sku);
+            $stockStatus = ' ';
+
+            if (isset($order['overstock-changed'])) {
+                $stockStatus = 'In Stock';
+            }
+
+            // TODO: fix multi-items-order issue
+            $sql = "SELECT * FROM BestbuyCA WHERE [Order #]='$orderId'";
+            $result = $accdb->query($sql)->fetch();
+            if ($result) {
+                continue;
+            }
+
+            $data = [
+                'Work Date'     => $workDate,
+                'Channel'       => $channel,
+                'Order #'       => $orderId,
+                'Express'       => $xpress,
+                'Stock Status'  => $stockStatus,
+                'Qty'           => $qty,
+                'Supplier'      => $supplier,
+                'Supplier SKU'  => $sku,
+                'Mfr #'         => $mfrpn,
+                'Supplier #'    => ' ',
+                'Remarks'       => '',
+                'RelatedSKU'    => '',
+                'Dimension'     => '',
+            ];
+
+            $sql = $this->insertMssql('BestbuyCA', $data);
+
+            $ret = $accdb->exec($sql);
+
+            if (!$ret) {
+                $this->error(__METHOD__);
+                $this->error(print_r($accdb->errorInfo(), true));
+                $this->error($sql);
+            }
+
+            $this->log($orderId);
+        }
+    }
+
+    protected function openAccessDB()
+    {
+        $dbname = "Z:/Purchasing/General Purchase.accdb";
+
+        if (!IS_PROD) {
+            $dbname = "C:/Users/BTE/Desktop/General Purchase.accdb";
+        }
+
+        $dsn = "odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=$dbname;";
+        $db = new PDO($dsn);
+
+        return $db;
     }
 }
--- job/order/triggers/NeweggOrderToAccess.php
+++ job/order/triggers/NeweggOrderToAccess.php
@@ -8,7 +8,95 @@ class NeweggOrderToAccess extends OrderTrigger
     {
         $this->log('=> '. __CLASS__);
 
-        // if ($order['channel'] != 'NeweggCA|NeweggUSA') continue;
-        // if (isset($order['overstock-changed']))
+        $this->importNeweggOrders();
+    }
+
+    protected function importNeweggOrders()
+    {
+        $lastOrderNo = '';
+
+        $accdb = $this->openAccessDB();
+
+        foreach ($this->orders as $order) {
+
+            $orderNo = $order['order_id'];
+            $date    = $order['date'];
+            $sku     = $order['sku'];
+            $qty     = $order['qty'];
+            $express = $order['express'];
+
+            if ($order['channel'] == 'NeweggCA') {
+                $channel = 'NeweggCA';
+                $table   = 'Newegg';
+            } else if ($order['channel'] == 'NeweggUSA') {
+                $channel = 'NeweggUS';
+                $table   = 'NeweggUS';
+            } else {
+                continue;
+            }
+
+            $stockStatus = ' ';
+            if (isset($order['overstock-changed'])) {
+                $stockStatus = 'In Stock';
+            }
+
+            // check if the order already imported
+            $sql = "SELECT * FROM $table WHERE [PO #]='$orderNo'";
+            $result = $accdb->query($sql)->fetch();
+            if ($result && $orderNo != $lastOrderNo) {
+                //echo "Skip $orderNo $channel\n";
+                continue;
+            }
+
+            $supplier = $this->getSupplier($sku);
+            $ponum    = $this->getSku($sku);
+            $mfrpn    = $this->getMfrPartNum($sku);
+
+            echo "Importing $orderNo $channel\n";
+
+            $sql = $this->insertMssql($table, [
+                'Work Date'    => $date,
+                'Channel'      => $channel,
+                'PO #'         => $orderNo,
+                'ShipMethod'   => $express ? 'Express' : 'Standard',
+                'Stock Status' => $stockStatus,
+                'Qty'          => $qty,
+                'Supplier'     => $supplier,
+                'Supplier SKU' => $ponum,
+                'Mfr #'        => $mfrpn,
+                'Supplier #'   => '',
+                'Remarks'      => '',
+                'RelatedSKU'   => '',
+                'Dimension'    => '',
+            ]);
+
+            $ret = $accdb->exec($sql);
+
+            if (!$ret) {
+                $this->error(__METHOD__);
+                $this->error(print_r($accdb->errorInfo(), true));
+                $this->error($sql);
+            }
+
+            $lastOrderNo = $orderNo;
+        }
+    }
+
+    protected function openAccessDB()
+    {
+        $dbname = "Z:/Purchasing/General Purchase.accdb";
+        if (!IS_PROD) {
+            $dbname = "C:/Users/BTE/Desktop/General Purchase.accdb";
+        }
+
+        $dsn = "odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=$dbname;";
+        $db = new PDO($dsn);
+
+        return $db;
+    }
+
+    protected function getMfrPartNum($sku)
+    {
+        return $this->di->get('skuService')->getMpn($sku);
     }
 }
