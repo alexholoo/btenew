734a82818eaf6c962b057ad13ad4b8bd18d50d03
Hanson Li <lihsca@gmail.com>
2017-04-12 12:31:53 -0400

    ignore extra fields in inventory.csv

--- bin/scripts/inventory.php
+++ bin/scripts/inventory.php
@@ -24,7 +24,16 @@ $count = 0;
 $db->execute('TRUNCATE TABLE inventory_location');
 while(($fields = fgetcsv($fh))) {
     try {
-        $data = array_combine($columns, $fields);
+        if (count($columns) == count($fields)) {
+            $data = array_combine($columns, $fields);
+        } else {
+            if (count($fields) > count($columns)) {
+                $data = array_combine($columns, array_slice($fields, 0, count($columns)));
+            } else {
+                print_r($fields);
+                continue;
+            }
+        }
         $db->insertAsDict('inventory_location', $data);
         $count++;
     } catch (Exception $e) {
