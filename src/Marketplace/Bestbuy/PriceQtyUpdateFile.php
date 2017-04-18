be94e9f677b21db6720fe0cb744a1fff6b4f29a4
Hanson Li <lihsca@gmail.com>
2017-04-18 16:41:52 -0400

    PriceQtyUpdateFile for each marketplace

---
+++ src/Marketplace/Bestbuy/PriceQtyUpdateFile.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Marketplace\Bestbuy;
+
+class PriceQtyUpdateFile
+{
+    protected $filename;
+    protected $delimiter = ";";
+    protected $handle;
+    protected $columns  = ['sku', 'price', 'quantity'];
+
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function __destruct()
+    {
+        $this->close();
+    }
+
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+
+    public function setDelimiter($delimiter)
+    {
+        $this->delimiter = $delimiter;
+        return $this;
+    }
+
+    public function setColumns($columns)
+    {
+        $this->columns = $columns;
+        return $this;
+    }
+
+    public function close()
+    {
+        if (is_resource($this->handle)) {
+            fclose($this->handle);
+        }
+    }
+
+    public function write($data)
+    {
+        if (!$this->handle) {
+            if (file_exists($this->filename)) {
+                $this->handle = fopen($this->filename, 'a');
+            } else {
+                $this->handle = fopen($this->filename, 'w');
+                if ($this->columns) {
+                    fputcsv($this->handle, $this->columns, $this->delimiter);
+                }
+            }
+        }
+
+        if (count($data) != count($this->columns)) {
+            throw new \Exception(get_called_class().': Wrong number of elements: '. var_export($data, true));
+        }
+
+        return fputcsv($this->handle, $data, $this->delimiter);
+    }
+}
