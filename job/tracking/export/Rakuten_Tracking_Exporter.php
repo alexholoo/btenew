65895efbd246119c4b0e9440f8b94c255e41d3d0
Hanson Li <lihsca@gmail.com>
2017-04-21 11:36:05 -0400

    use Rakuten::StdOrderListFile to getUnshippedOrders

--- job/tracking/export/Rakuten_Tracking_Exporter.php
+++ job/tracking/export/Rakuten_Tracking_Exporter.php
@@ -28,70 +28,20 @@ class Rakuten_Tracking_Exporter extends Tracking_Exporter
         }
     }
 
+    // TODO: get unshipped orders from database, instead of file
     protected function getUnshippedOrders($channel)
     {
         $shipmentService = $this->di->get('shipmentService');
 
-        // TODO: need a class: Marketplace\Rakuten\MasterOrderFile
-#       $orderFile = Filenames::get('rakuten.us.master.order');
-        $orderFile = 'w:/data/csv/rakuten/orders/rakuten_master_orders.csv';
-
-        if (!file_exists($orderFile)) {
-            $this->error(__METHOD__." File not found: $orderFile");
-            return [];
-        }
-
-        $fp = fopen($orderFile, 'r');
-
-        $columns = fgetcsv($fp);
-        /*
-         0   SellerShopperNumber,
-         1   Receipt_ID,
-         2   Receipt_Item_ID,
-         3   ListingID,
-         4   Date_Entered,
-         5   Sku,
-         6   ReferenceId,
-         7   Quantity,
-         8   Qty_Shipped,
-         9   Qty_Cancelled,
-         10  Title,
-         11  Price,
-         12  Product_Rev,
-         13  Shipping_Cost,
-         14  ProductOwed,
-         15  ShippingOwed,
-         16  Commission,
-         17  ShippingFee,
-         18  PerItemFee,
-         19  Tax_Cost,
-         20  Bill_To_Company,
-         21  Bill_To_Phone,
-         22  Bill_To_Fname,
-         23  Bill_To_Lname,
-         24  Email,
-         25  Ship_To_Name,
-         26  Ship_To_Company,
-         27  Ship_To_Street1,
-         28  Ship_To_Street2,
-         29  Ship_To_City,
-         30  Ship_To_State,
-         31  Ship_To_Zip,
-         32  ShippingMethodId
-        */
+        $filename = Filenames::get('rakuten.us.master.order');
+        $orderFile = new Marketplace\Rakuten\StdOrderListFile($filename, 'US');
 
         $orders = [];
 
-        while (($values = fgetcsv($fp)) !== FALSE) {
-            if (count($columns) != count($values)) {
-                $this->error(__METHOD__. print_r($values, true));
-                continue;
-            }
-            $fields = array_combine($columns, $values);
-
-            $receiptId     = $fields['Receipt_ID']; // rakuten order id
-            $receiptItemId = $fields['Receipt_Item_ID'];
-            $qty           = $fields['Quantity'];
+        while ($order = $orderFile->read()) {
+            $receiptId     = $order['Receipt_ID']; // rakuten order id
+            $receiptItemId = $order['Receipt_Item_ID'];
+            $qty           = $order['Quantity'];
 
             if ($shipmentService->isOrderShipped($receiptId)) {
                 continue;
@@ -120,8 +70,6 @@ class Rakuten_Tracking_Exporter extends Tracking_Exporter
                 ];
             }
         }
-
-        fclose($fp);
 
         return $orders;
     }
