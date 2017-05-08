795cc6244e14c115440f48391a3ab6c0773c72b6
Hanson Li <lihsca@gmail.com>
2017-05-08 11:46:05 -0400

    sprintf.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -483,6 +483,21 @@ bte.utils.newGuid = function () {
     });
 }
 
+bte.utils.sprintf = function (str) {
+    var args = arguments,
+        flag = true,
+        i = 1;
+    str = str.replace(/%s/g, function () {
+        var arg = args[i++];
+        if (typeof arg === 'undefined') {
+            flag = false;
+            return '';
+        }
+        return arg;
+    });
+    return flag ? str : '';
+}
+
 bte.utils.showToast = function (msg, autoHide=false) {
     $.toast({
         heading: 'Success',
