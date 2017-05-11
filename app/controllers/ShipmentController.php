9346eb60dfdb1180777bb309baad66fbe422a5c8
Hongshuan Li <lihsca@gmail.com>
2017-05-10 20:46:17 -0400

    move chitchat to shipment controller

--- app/controllers/ShipmentController.php
+++ app/controllers/ShipmentController.php
@@ -19,4 +19,30 @@ class ShipmentController extends ControllerBase
             $this->view->data = $tracking;
         }
     }
+
+    public function chitchatAction()
+    {
+        $this->view->pageTitle = 'Chitchat';
+
+        $saved = 0;
+
+        if ($this->request->isPost()) {
+            if (($info = $this->request->getPost('Tracking'))) {
+                $saved = $this->chitchatService->save($info);
+            }
+            else if ($this->request->getPost('Delete')) {
+                $items = $this->request->getPost('Items');
+                $this->chitchatService->delete($items);
+            }
+            else if ($this->request->getPost('Delete_All')) {
+                $this->chitchatService->deleteAll();
+            }
+            else if ($this->request->getPost('Export')) {
+                $this->chitchatService->export();
+            }
+        }
+
+        $this->view->saved = $saved;
+        $this->view->list  = $this->chitchatService->list();
+    }
 }
