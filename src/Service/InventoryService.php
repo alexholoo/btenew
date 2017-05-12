1a0a42319212f273cad337893036664ccac87ea9
Hanson Li <lihsca@gmail.com>
2017-05-12 12:53:38 -0400

    add methods to InventoryService

--- src/Service/InventoryService.php
+++ src/Service/InventoryService.php
@@ -6,4 +6,45 @@ use Phalcon\Di\Injectable;
 
 class InventoryService extends Injectable
 {
+    public function load()
+    {
+        $sql = "SELECT * FROM bte_inventory ORDER BY id DESC";
+        $result = $this->db->fetchAll($sql);
+        return $result;
+    }
+
+    public function get($partnum)
+    {
+        $sql = "SELECT * FROM bte_inventory WHERE partnum='$partnum'";
+        $result = $this->db->fetchOne($sql);
+        return $result;
+    }
+
+    public function getAvail($partnum)
+    {
+        $result = $this->get($partnum);
+
+        $qty = 0;
+
+        if ($result) {
+            $qty = $result['qty'];
+        }
+
+        return $qty;
+    }
+
+    public function add($info)
+    {
+    }
+
+    public function deduct($sku, $order)
+    {
+    }
+
+    public function loadChanges()
+    {
+        $sql = "SELECT * FROM bte_inventory_change ORDER BY id DESC";
+        $result = $this->db->fetchAll($sql);
+        return $result;
+    }
 }
