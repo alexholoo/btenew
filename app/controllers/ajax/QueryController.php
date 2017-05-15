6884c70d008f5da262864023fb972e1512a9d863
Hanson Li <lihsca@gmail.com>
2017-05-15 16:20:34 -0400

    little change to /ajax/QueryController

--- app/controllers/ajax/QueryController.php
+++ app/controllers/ajax/QueryController.php
@@ -7,6 +7,33 @@ use Phalcon\Mvc\Controller;
 class QueryController extends ControllerBase
 {
     /**
+     * /ajax/query/sku?id=SKU
+     */
+    public function skuAction()
+    {
+        $sku = $this->request->getQuery('id');
+
+        $info = $this->skuService->getMasterSku($sku);
+
+        if ($info) {
+            $this->response->setJsonContent(['status' => 'OK', 'data' => $info]);
+        } else {
+            $this->response->setJsonContent(['status' => 'ERROR', 'message' => 'SKU not found']);
+        }
+
+        return $this->response;
+    }
+
+    /**
+     * /ajax/query/mastersku?id=SKU
+     */
+    public function masterSkuAction()
+    {
+        // Forward to another action in the current controller
+        $this->dispatcher->forward(['action' => 'sku']);
+    }
+
+    /**
      * return sku list with same UPC
      *
      * /ajax/query/upc/xxxx
@@ -108,24 +135,6 @@ class QueryController extends ControllerBase
 
             return $this->response;
         }
-    }
-
-    /**
-     * /ajax/query/mastersku?id=SKU
-     */
-    public function masterSkuAction()
-    {
-        $sku = $this->request->getQuery('id');
-
-        $info = $this->skuService->getMasterSku($sku);
-
-        if ($info) {
-            $this->response->setJsonContent(['status' => 'OK', 'data' => $info]);
-        } else {
-            $this->response->setJsonContent(['status' => 'ERROR', 'message' => 'SKU not found']);
-        }
-
-        return $this->response;
     }
 
     /**
