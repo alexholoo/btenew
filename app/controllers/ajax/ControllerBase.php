39d37d7f30389d12e56ba8cd9f559f1b444caeab
Hanson Li <lihsca@gmail.com>
2017-05-17 16:51:47 -0400

    restrict /ajax/* to AJAX request only

--- app/controllers/ajax/ControllerBase.php
+++ app/controllers/ajax/ControllerBase.php
@@ -3,12 +3,22 @@
 namespace Ajax\Controllers;
 
 use Phalcon\Mvc\Controller;
+use Phalcon\Mvc\Dispatcher;
 
 class ControllerBase extends Controller
 {
     public function initialize()
     {
-        // TODO: restrict /ajax/* to AJAX request only
         $this->view->disable();
+    }
+
+    // TODO: remove __ to restrict /ajax/* to AJAX request only
+    public function __beforeExecuteRoute(Dispatcher $dispatcher)
+    {
+        if (!$this->request->isAjax()) {
+            return false;
+        }
+
+        return true;
     }
 }
