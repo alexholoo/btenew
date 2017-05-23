6c61267598ed61ac575d3e40b3cd49d8c9173345
Hanson Li <lihsca@gmail.com>
2017-05-23 14:03:53 -0400

    remove unuseful code

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -4,11 +4,4 @@ namespace App\Controllers;
 
 class OrderController extends ControllerBase
 {
-    public function getAction()
-    {
-        $this->dispatcher->forward([
-            'controller' => 'query',
-            'action'     => 'order',
-        ]);
-    }
 }
