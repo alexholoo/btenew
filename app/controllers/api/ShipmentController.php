c084ed7b55ea66e7f9361c3c2a8304961d5e8567
Hanson Li <lihsca@gmail.com>
2017-06-06 11:07:49 -0400

    use POST for /api/shipment/new

--- app/controllers/api/ShipmentController.php
+++ app/controllers/api/ShipmentController.php
@@ -13,16 +13,18 @@ class ShipmentController extends ControllerBase
 
     public function newAction()
     {
-        $trackingNum = $this->request->getQuery('trackingnum');
-        $user = $this->request->getQuery('user');
+        if ($this->request->isPost()) {
+            $trackingNum = $this->request->getPost('trackingnum');
+            $user = $this->request->getPost('user');
 
-        if ($trackingNum && $user) {
-            $info = $this->shipmentService->addShipment($trackingNum, $user);
-            $this->response->setJsonContent(['status' => 'OK', 'data' => $info]);
-        } else {
-            $this->response->setJsonContent(['status' => 'ERROR', 'message' => 'Bad Request']);
+            if ($trackingNum && $user) {
+                $info = $this->shipmentService->addShipment($trackingNum, $user);
+                $this->response->setJsonContent(['status' => 'OK', 'data' => $info]);
+            } else {
+                $this->response->setJsonContent(['status' => 'ERROR', 'message' => 'Bad Request']);
+            }
+
+            return $this->response;
         }
-
-        return $this->response;
     }
 }
