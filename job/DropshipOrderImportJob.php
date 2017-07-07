3c56175a2893058df14487a23e0a301f708b1d42
Hanson Li <lihsca@gmail.com>
2017-07-07 12:43:21 -0400

    dropship orders include rakuten/newegg

--- job/DropshipOrderImportJob.php
+++ job/DropshipOrderImportJob.php
@@ -60,10 +60,10 @@ class DropshipOrderImportJob extends Job
 
         fclose($fh);
 
-        if (!$this->haveNewOrders($data)) {
-            echo "No new orders, nothing imported.\n";
-            return;
-        }
+        #if (!$this->haveNewOrders($data)) {
+        #    echo "No new orders, nothing imported.\n";
+        #    return;
+        #}
 
         try {
             $sql = $this->genInsertSql($table, $columns, $data);
@@ -78,6 +78,13 @@ class DropshipOrderImportJob extends Job
     }
 
     public function importDropshipOrders()
+    {
+        $this->db->execute("TRUNCATE TABLE ca_order_notes");
+        $this->importAmazonCaOrders();
+        $this->importRakutenNeweggOrders();
+    }
+
+    public function importAmazonCaOrders()
     {
         $file  = 'ca_order_notes.csv';
         $table = 'ca_order_notes';
@@ -114,21 +121,87 @@ class DropshipOrderImportJob extends Job
 
         fclose($fh);
 
-        if (!$this->haveNewDropshipOrders($data)) {
-            echo "No new dropship orders, nothing imported.\n";
-            return;
-        }
+        #if (!$this->haveNewDropshipOrders($data)) {
+        #    echo "No new dropship orders, nothing imported.\n";
+        #    return;
+        #}
 
         try {
             $sql = $this->genInsertSql($table, $columns, $data);
-            $this->db->execute("TRUNCATE TABLE $table");
             $this->db->execute($sql);
         } catch (Exception $e) {
             echo $e->getMessage(), EOL;
         }
 
         $count = count($data);
-        echo "$count dropship orders imported\n";
+        echo "$count orders imported\n";
+    }
+
+    public function importRakutenNeweggOrders()
+    {
+        $file  = 'rakuten_newegg_order_note.csv';
+        $table = 'ca_order_notes';
+
+        if (($fh = @fopen("E:/BTE/import/$file", 'rb')) === false) {
+            echo "Failed to open file: $file\n";
+            return;
+        }
+
+        echo "Importing $file\n";
+
+        $title = fgetcsv($fh); // skip the first line
+
+        // Table columns
+        $columns = [
+            'date',
+            'order_id',
+            'stock_status',
+            'express',
+            'qty',
+            'supplier',
+            'supplier_sku',
+            'mpn',
+            'supplier_no',
+            'notes',
+            'related_sku',
+            'dimension',
+        ];
+
+        $data = [];
+        while(($values = fgetcsv($fh))) {
+            $fields = array_combine($title, $values);
+            $data[] = [
+                $fields['date'],
+                $fields['order_id'],
+                $fields['Stock Staus'],
+                $fields['Xpress'],
+                $fields['qty'],
+                $fields['supplier'],
+                $fields['supplier_sku'],
+                $fields['MPN'],
+                $fields['Supplier#'],
+                $fields['notes'],
+                $fields['related_sku'],
+                $fields['dimension'],
+            ];
+        }
+
+        fclose($fh);
+
+        #if (!$this->haveNewDropshipOrders($data)) {
+        #    echo "No new dropship orders, nothing imported.\n";
+        #    return;
+        #}
+
+        try {
+            $sql = $this->genInsertSql($table, $columns, $data);
+            $this->db->execute($sql);
+        } catch (Exception $e) {
+            echo $e->getMessage(), EOL;
+        }
+
+        $count = count($data);
+        echo "$count orders imported\n";
     }
 
     public function haveNewOrders($data)
